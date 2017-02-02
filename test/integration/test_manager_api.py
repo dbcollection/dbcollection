2b3cd772dd1594308691e8b3743d4ab44099cb04
farrajota <mlgfarrajota@outlook.com>
2017-02-02 11:56:35 +0000

    renamed folder from system to integration test

---
+++ test/integration/test_manager_api.py
@@ -0,0 +1,21 @@
+"""
+Test manager API with the cifar10 dataset
+"""
+
+
+# 0. remove all files + data dir
+
+# 1. Load data (+download), list
+# 2. Download data
+
+#
+- load
+- add
+- delete
+- config
+- download
+- reset
+- query
+- list
+
+
