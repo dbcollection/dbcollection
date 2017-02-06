e62313d92f3ddb968d69538c27be275161c3e3ae
farrajota <mlgfarrajota@outlook.com>
2017-02-06 16:05:25 +0000

    added docstring to dbcollection import

--- dbcollection/__init__.py
+++ dbcollection/__init__.py
@@ -1,3 +1,19 @@
+"""
+Dataset collection package.
+
+This package allows to easily manage and load pre-processed datasets in an easy
+way by using hdf5 files as metadata storage. By storing all the necessary metadata
+on disk, memory RAM can be allocated to other functionalities without noticable
+performance lost, and allows for huge datasets to be used in systems with limited
+memory capacity. 
+
+This package enables the user to set and configure a dataset once and reuse it as
+many times it for multiple tasks without manually having te need to setup a 
+dataset every time.
+
+<TODO: finish the header file explanation>
+"""
+
 __all__ = ['cache', 'loader', 'manager', 'storage', 'utils']
 
 from . import *
