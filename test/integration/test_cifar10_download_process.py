1ab57e2ebef07204119500a5b9cb0e77ee092a11
farrajota <mlgfarrajota@outlook.com>
2017-02-07 17:30:49 +0000

    added two tests

---
+++ test/integration/test_cifar10_download_process.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+"""
+Test downloading + preprocessing the cifar10 dataset.
+"""
+
+
+from __future__ import print_function
+import os
+import sys
+import numpy as np
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+lib_path = os.path.abspath(os.path.join(dir_path, '..', '..'))
+sys.path.append(lib_path)
+import dbcollection.manager as dbclt
+
+# download dataset
+print('==> dbcollection.download:')
+#dbclt.download(name='cifar10', data_dir='/home/mf/tmp/download_data', verbose=True)
+
+# load dataset (process metadata file)
+print('==> dbcollection.load:')
+loader = dbclt.load(name='cifar10', data_dir='/home/mf/tmp/download_data/cifar10', download=True)
+
+# print data from the loader
+print('Dataset: ' + loader.name)
+print('Category: ' + loader.category)
+print('Task: ' + loader.task)
+print('Data path: ' + loader.data_dir)
+print('Metadata cache path: ' + loader.cache_path)
