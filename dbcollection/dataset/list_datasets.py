2959f7d97402ec86cae2e638951e97205ec60d72
farrajota <mlgfarrajota@outlook.com>
2017-02-20 19:01:55 +0000

    simplified the list of datasets

--- dbcollection/dataset/list_datasets.py
+++ dbcollection/dataset/list_datasets.py
@@ -5,23 +5,9 @@ All available datasets must be added in this file.
 """
 
 
-from . import image_processing
+from . import cifar
 
-
-#---------------------------------------------------------
-# Category lists
-#---------------------------------------------------------
-
-image_processing_list = {
-    "cifar10": image_processing.cifar.cifar10.Cifar10,
-    #"cifar100": image_processing.cifar.cifar100.Cifar100,
-}
-
-
-#---------------------------------------------------------
-# MAIN list
-#---------------------------------------------------------
 
 datasets = {
-    "image_processing": image_processing_list,
+    "cifar10" : cifar.cifar10.Cifar10,
 }
