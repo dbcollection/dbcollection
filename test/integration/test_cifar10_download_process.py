5ce02c1b27ccdcdb356859c12a1e344337d314bf
farrajota <mlgfarrajota@outlook.com>
2017-02-20 19:14:41 +0000

    removed the category field from the test

--- test/integration/test_cifar10_download_process.py
+++ test/integration/test_cifar10_download_process.py
@@ -16,17 +16,17 @@ lib_path = os.path.abspath(os.path.join(dir_path, '..', '..'))
 sys.path.append(lib_path)
 import dbcollection.manager as dbclt
 
+
 # download dataset
 print('==> dbcollection.download:')
-#dbclt.download(name='cifar10', data_dir='/home/mf/tmp/download_data', verbose=True)
+dbclt.download(name='cifar10', data_dir='/home/mf/tmp/download_data', verbose=True)
 
 # load dataset (process metadata file)
 print('==> dbcollection.load:')
-loader = dbclt.load(name='cifar10', data_dir='/home/mf/tmp/download_data/cifar10', download=True)
+loader = dbclt.load(name='cifar10', verbose=True)
 
 # print data from the loader
 print('Dataset: ' + loader.name)
-print('Category: ' + loader.category)
 print('Task: ' + loader.task)
 print('Data path: ' + loader.data_dir)
 print('Metadata cache path: ' + loader.cache_path)
