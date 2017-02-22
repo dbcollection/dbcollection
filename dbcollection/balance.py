7cb7417240c628e88ff3843dd74c6becfd21411b
farrajota <mlgfarrajota@outlook.com>
2017-02-22 12:33:47 +0000

    removed whitespace

--- dbcollection/balance.py
+++ dbcollection/balance.py
@@ -3,10 +3,10 @@ Balances the data between sets of the dataset.
 
 Allocating/moving data between two or more sets can be done using the balance
 property in the load() API. To do this, simply select the name of the sets you
-want to balance, and then provide the total amount per set. 
+want to balance, and then provide the total amount per set.
 
-This allows the user to move data around, even depleting a set of data if needed. 
-Moving the entire data of a dataset into one set is not only possible but also 
+This allows the user to move data around, even depleting a set of data if needed.
+Moving the entire data of a dataset into one set is not only possible but also
 doable and easy.
 """
 
@@ -34,7 +34,7 @@ def balance(handler, sets, split_distribution):
     parse_inputs(sets, split_distribution)
 
     # concat data
-    
+
 
     # shuffle if required
 
@@ -42,6 +42,6 @@ def balance(handler, sets, split_distribution):
 
     # split the data for each set
 
-    
+
 
 
