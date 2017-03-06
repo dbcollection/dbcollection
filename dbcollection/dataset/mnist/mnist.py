52d7f965fe2148d250bfbe8824025d741f2eaa67
farrajota <mlgfarrajota@outlook.com>
2017-03-06 22:51:38 +0000

    small fix to the mnist fields

--- dbcollection/dataset/mnist/mnist.py
+++ dbcollection/dataset/mnist/mnist.py
@@ -108,23 +108,23 @@ class MNIST:
 
         # add data to the **default** group
         fileh5.add_data('train/source', 'data', data["train"]["images"], np.uint8)
-        fileh5.add_data('train/source', 'labels', data["train"]["labels"], np.uint8)
+        fileh5.add_data('train/source', 'label', data["train"]["labels"], np.uint8)
 
         fileh5.add_data('test/source', 'data', data["test"]["images"], np.uint8)
-        fileh5.add_data('test/source', 'labels', data["test"]["labels"], np.uint8)
+        fileh5.add_data('test/source', 'label', data["test"]["labels"], np.uint8)
 
         # add data to the **list** group
         # write data to the metadata file
         fileh5.add_data('train/default', 'class', str2ascii(self.classes), np.uint8)
         fileh5.add_data('train/default', 'data',  data["train"]["images"], np.uint8)
-        fileh5.add_data('train/default', 'labels', data["train"]["labels"], np.uint8)
+        fileh5.add_data('train/default', 'label', data["train"]["labels"], np.uint8)
         fileh5.add_data('train/default', 'object_id', data["train"]["object_ids"], np.int32)
         # object fields is necessary to identify which fields compose 'object_id'
         fileh5.add_data('train/default', 'object_fields', str2ascii(data['train']["object_fields"]), np.uint8)
 
         fileh5.add_data('test/default', 'class', str2ascii(self.classes), np.uint8)
         fileh5.add_data('test/default', 'data', data["test"]["images"], np.uint8)
-        fileh5.add_data('test/default', 'labels', data["test"]["labels"], np.uint8)
+        fileh5.add_data('test/default', 'label', data["test"]["labels"], np.uint8)
         fileh5.add_data('test/default', 'object_id', data["test"]["object_ids"], np.int32)
         # object fields is necessary to identify which fields compose 'object_id'
         fileh5.add_data('test/default', 'object_fields', str2ascii(data['test']["object_fields"]), np.uint8)
