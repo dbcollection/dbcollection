aef9c03202f5c09a0ce0353b76a679292f9bb75f
farrajota <mlgfarrajota@outlook.com>
2017-03-30 15:20:00 +0100

    refactored the imagenet classification task

--- dbcollection/datasets/imagenet/classification.py
+++ dbcollection/datasets/imagenet/classification.py
@@ -16,9 +16,9 @@ from dbcollection.utils.pad import pad_list
 
 
 class Classification:
-    """ Imagenet ILSVRC 2012 Classification preprocessing functions """
+    """ ImageNet ILSVRC 2012 Classification preprocessing functions """
 
-    def __init__(self, data_path, cache_path, verbose=True):
+    def __init__(self, data_path, cache_path, verbose=True, paths=None):
         """
         Initialize class.
         """
@@ -28,8 +28,14 @@ class Classification:
 
         self.fname_val_groundtruth_idx = 'ILSVRC2012_validation_ground_truth.txt'
         self.fname_metadata = 'meta.mat'
-        self.dirnames_train = ['ILSVRC2012_img_train', 'train']
-        self.dirnames_val = ['ILSVRC2012_img_val', 'val']
+        if paths:
+            assert os.path.isdir(paths[0]), 'Train directory does not exist: {}'.format(paths[0])
+            assert os.path.isdir(paths[0]), 'Validation directory does not exist: {}'.format(paths[1])
+            self.dirnames_train = [paths[0]]
+            self.dirnames_val = [paths[1]]
+        else:
+            self.dirnames_train = ['ILSVRC2012_img_train', 'train']
+            self.dirnames_val = ['ILSVRC2012_img_val', 'val']
 
 
     def get_file_path(self, fname):
@@ -131,60 +137,6 @@ class Classification:
         return set_data
 
 
-    def process_set(self, data):
-        """
-        Process the data into arrays for storage.
-        """
-        # load annotations
-        annotations = self.get_annotations()
-
-        # combine class name, label and description elems into lists
-        class_list = list(data.keys())
-        label_list = [annotations[classname]['label'] for classname in class_list]
-        description_list = [annotations[classname]['description'] for classname in class_list]
-
-        # progress bar
-        if self.verbose:
-            counter = 0
-            prgbar = progressbar.ProgressBar(max_value=len(class_list))
-
-
-        # group all filenames into a list
-        filenames = []
-        class_id = []
-        object_id = []
-        list_class_fname = []
-        for cname in data:
-            range_ini = len(filenames)
-
-            for filename in data[cname]:
-                filenames.append(filename)
-                class_id.append(class_list.index(cname))
-                object_id.append([len(filenames), class_id[-1]])
-
-            # organize filenames by class id
-            list_class_fname.append(list(range(range_ini, len(filenames))))
-
-            # update progress bar
-            if self.verbose:
-                counter += 1
-                prgbar.update(counter)
-
-        # pad list with zeros in order to have all lists of the same size
-        list_class_fname = pad_list(list_class_fname, -1)
-
-        return {
-            "object_fields": str2ascii(['image_filename', 'class_name']),
-            "class_names": str2ascii(class_list),
-            "labels": str2ascii(label_list),
-            "descriptions": str2ascii(description_list),
-            "image_filenames": str2ascii(filenames),
-            "class_ids": np.array(class_id, dtype=np.int32),
-            "object_ids": np.array(object_id, dtype=np.int32),
-            "list_classes_filenames": np.array(list_class_fname, dtype=np.int32)
-        }
-
-
     def load_data(self):
         """
         Load the data from the files.
@@ -195,104 +147,143 @@ class Classification:
         annotations = self.get_annotations()
 
         # get correct set paths
-        train_dir = self.get_dir_path(self.dirnames_train)
-        val_dir = self.get_dir_path(self.dirnames_val)
-
-        #============
-        # Train set
-        #============
-        if self.verbose:
-            print('\n==> Fetch train set data from dir: {}'.format(train_dir))
-        train_data = construct_set_from_dir(train_dir, self.verbose)
-        if self.verbose:
-            print('\n==> Process train set data... ')
-        train_processed = self.process_set(train_data)
-
-        #==========
-        # Val set
-        #==========
-        # check if the val dir is in the original format or in a converted format
-        if self.verbose:
-            print('\n==> Fetch val set data from dir: {}'.format(val_dir))
-        _, folders = dir_get_size(val_dir)
-        if folders > 0:
-            val_data = construct_set_from_dir(val_dir, self.verbose)
-        else:
-            val_data = self.fetch_val_dir_data(val_dir)
-        if self.verbose:
-            print('==> Process train set data... ')
-        val_processed = self.process_set(val_data)
-
-        return {
-            "train": {
-                "nested": train_data,
-                "flattened": train_processed
-            },
-            "val": {
-                "nested": val_data,
-                "flattened": val_processed
-            }
+        dir_paths = {
+            "train" : self.get_dir_path(self.dirnames_train),
+            "val" : self.get_dir_path(self.dirnames_val)
         }
 
+        # cycle the train and val data
+        for set_name in dir_paths:
+            data_dir = dir_paths[set_name]
+            if self.verbose:
+                print('\n==> Fetch set {} data from dir: {}'.format(set_name, data_dir))
 
-    def hdf5_add_data_source(self, handler, data, set_name):
-        """
-        Add data to the HDF5 file in a nested format (folder-like).
-        """
-        # set pointer
-        data_ = data[set_name]["nested"]
+            if set_name is 'train':
+                data = construct_set_from_dir(data_dir, self.verbose)
+            else:
+                _, folders = dir_get_size(data_dir)
+                if folders > 0:
+                    data = construct_set_from_dir(data_dir, self.verbose)
+                else:
+                    data = self.fetch_val_dir_data(data_dir)
 
+            yield {set_name : data}
+
+
+    def store_data_source(self, handler, data, set_name):
+        """
+        Store classes + filenames as a nested tree.
+        """
         # progress bar
         if self.verbose:
+            prgbar = progressbar.ProgressBar(max_value=len(data)).start()
             counter = 0
-            prgbar = progressbar.ProgressBar(max_value=len(data_))
+
+        sourceg = handler.create_group('source/' + set_name)
 
         # cycle all classes from the data with the original annotations
-        for cname in data_:
-            for i, filename in enumerate(data_[cname]):
-                handler.add_data('source/' + set_name +'/images/' + cname, str(i), str2ascii(filename), np.uint8)
+        for cname in data[set_name]:
+
+            images_filenames = [os.path.join(set_name, cname, fname) for fname in data[set_name][cname]]
+            images_filenames.sort()
+            sourceg.create_dataset('image_filenames', data=str2ascii(images_filenames), dtype=np.uint8)
 
             # update progress bar
             if self.verbose:
                 counter += 1
                 prgbar.update(counter)
 
-        handler.add_data('source/' + set_name, "classes", data[set_name]["flattened"]["class_names"])
-        handler.add_data('source/' + set_name, "labels", data[set_name]["flattened"]["labels"])
-        handler.add_data('source/' + set_name, "descriptions", data[set_name]["flattened"]["descriptions"])
+        # force progressbar to 100%
+        prgbar.finish()
 
 
-    def process_metadata(self):
+    def convert_data_to_arrays(self, data):
+        """
+        Convert folders/filenames to arrays.
+        """
+        # load annotations
+        annotations = self.get_annotations()
+
+        # intialize lists
+        classes = list(data.keys())
+        classes.sort()
+        label_list = [annotations[cname]['label'] for cname in classes]
+        description_list = [annotations[cname]['description'] for cname in classes]
+        object_ids = []
+        filenames = []
+        list_image_filenames_per_class = []
+
+        # progress bar
+        if self.verbose:
+            progbar = progressbar.ProgressBar(max_value=len(classes)).start()
+            counter = 0
+
+        # cycle all classes
+        count_fname = 0
+        for class_id, cname in enumerate(classes):
+            range_ini = len(filenames)
+
+            for filename in data[cname]:
+                filenames.append(filename)
+                object_ids.append([count_fname, class_id])
+                count_fname += 1
+
+            # organize filenames by class id
+            list_image_filenames_per_class.append(list(range(range_ini, len(filenames))))
+
+            # update progress bar
+            if self.verbose:
+                counter += 1
+                progbar.update(counter)
+
+        # force progressbar to 100%
+        progbar.finish()
+
+        # pad list with zeros in order to have all lists of the same size
+        list_image_filenames_per_video = pad_list(list_image_filenames_per_class, -1)
+
+        return {
+            "object_fields": str2ascii(['image_filename', 'class_name']),
+            "classes": str2ascii(classes),
+            "labels": str2ascii(label_list),
+            "descriptions": str2ascii(description_list),
+            "image_filenames": str2ascii(filenames),
+            "object_ids": np.array(object_ids, dtype=np.int32),
+            "list_image_filenames_per_class": np.array(list_image_filenames_per_class, dtype=np.int32)
+        }
+
+
+    def process_metadata(self, save_name):
         """
         Process metadata and store it in a hdf5 file.
         """
-        if self.verbose:
-            print('\nLoading data...')
-        # load data to memory
-        data = self.load_data()
-
         # create/open hdf5 file with subgroups for train/val/test
-        file_name = os.path.join(self.cache_path, 'classification.h5')
+        if save_name:
+            file_name = os.path.join(self.cache_path, save_name)
+        else:
+            file_name = os.path.join(self.cache_path, 'classification.h5')
         fileh5 = h5py.File(file_name, 'w', version='latest')
+
         if self.verbose:
-            print('\nWritting data to file: {}'.format(file_name))
+            print('\n==> Storing metadata to file: {}'.format(file_name))
 
-        # add data to the **list** group
-        for set_name in ['train', 'val']:
-            if self.verbose:
-                print('==> Writing ::{}:: set data...'.format(set_name))
+        # setup data generator
+        data_gen = self.load_data()
 
-            defaultg = fileh5.create_group('default/' + set_name)
-            defaultg.create_dataset( 'classes', data=data[set_name]["flattened"]["class_names"], dtype=np.uint8)
-            defaultg.create_dataset( 'labels', data=data[set_name]["flattened"]["labels"], dtype=np.uint8)
-            defaultg.create_dataset( 'descriptions', data=data[set_name]["flattened"]["descriptions"], dtype=np.uint8)
-            defaultg.create_dataset( 'image_filenames', data=data[set_name]["flattened"]["image_filenames"], dtype=np.uint8)
-            defaultg.create_dataset( 'class_ids', data=data[set_name]["flattened"]["class_ids"], dtype=np.uint8)
-            defaultg.create_dataset( 'object_ids', data=data[set_name]["flattened"]["object_ids"], dtype=np.int32)
-            defaultg.create_dataset( 'object_fields', data=data[set_name]["flattened"]["object_fields"], dtype=np.uint8)
+        for data in data_gen:
+            for set_name in data:
 
-            # add data to the **source** group
-            self.hdf5_add_data_source(fileh5, data, set_name)
+                if self.verbose:
+                    print('Saving set metadata: {}'.format(set_name))
+
+                # add data to the **source** group
+                self.store_data_source(fileh5, data, set_name)
+
+                 # add data to the **default** group
+                data_array = self.convert_data_to_arrays(data[set_name])
+                defaultg = fileh5.create_group('default/' + set_name)
+                for field_name in data_array:
+                    defaultg.create_dataset(field_name, data=data_array[field_name])
 
         # close file
         fileh5.close()
@@ -301,8 +292,8 @@ class Classification:
         return file_name
 
 
-    def run(self):
+    def run(self, save_name=None):
         """
         Run task processing.
         """
-        return self.process_metadata()
+        return self.process_metadata(save_name)
