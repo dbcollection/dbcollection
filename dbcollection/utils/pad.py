c53d959010829531bc58f1fab0f6414109185743
farrajota <mlgfarrajota@outlook.com>
2017-05-03 16:42:07 +0100

    added pad_list2 method

--- dbcollection/utils/pad.py
+++ dbcollection/utils/pad.py
@@ -2,6 +2,7 @@
 Padding functions.
 """
 
+
 def pad_list(listA, val=-1):
     """Pad list of lists with 'val' such that all lists have the same length.
 
@@ -30,6 +31,38 @@ def pad_list(listA, val=-1):
 
     # pad all lists with the a padding value
     return [l + [val]*int(max_size-len(l)) for l in listA]
+
+
+def pad_list2(listA, val=-1):
+    """Pad list of lists of lists with 'val' such that all lists have the same length.
+
+    Parameters
+    ----------
+    listA : list of lists
+        List of lists of different sizes.
+    val : number
+        Value to pad the lists.
+
+    Returns
+    -------
+    list of lists
+        A list of lists with the same same.
+
+    Raises
+    ------
+        None
+    """
+    # pad list with zeros in order to have all lists of the same size
+    assert isinstance(listA, list), 'Input must be a list. Got {}, espected {}' \
+                                    .format(type(listA), type(list))
+
+    # get size of the biggest list
+    max_nrows = max([len(l) for l in listA])
+    max_size = max([len(li) for l in listA for li in l])
+
+    # pad all lists with the a padding value
+    out = [[] for i in range(len(listA))]
+    return [out[i].append(li + [val]*int(max_size-len(li))) for i, l in enumerate(listA) for li in l]
 
 
 def unpad_list(listA, val=-1):
