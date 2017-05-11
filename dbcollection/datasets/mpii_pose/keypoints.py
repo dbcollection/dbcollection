625b533422fed3baa73965458c5bd9f0bee649b3
farrajota <mlgfarrajota@outlook.com>
2017-05-11 12:17:03 +0100

    fixed mpii dataset

--- dbcollection/datasets/mpii_pose/keypoints.py
+++ dbcollection/datasets/mpii_pose/keypoints.py
@@ -70,63 +70,18 @@ class Keypoints:
             prgbar = progressbar.ProgressBar(max_value=nfiles)
 
         data = {
-            "train" : {},
-            "test" : {}
+            "train" : [],
+            "test" : []
         }
 
 
+        # cycle all files
         for ifile in range(nfiles):
             if annotations['RELEASE'][0][0][1][0][ifile] == 0:
                 set_name = 'test'
             else:
                 set_name = 'train'
 
-            # image annots
-            image_filename = os.path.join('images', str(annotations['RELEASE'][0][0][0][0][ifile][0][0][0][0][0]))
-
-            # parse keypoints
-            if any(annotations['RELEASE'][0][0][0][0][ifile][3][0]):
-                frame_sec = int(annotations['RELEASE'][0][0][0][0][ifile][2][0][0])
-                video_idx = int(annotations['RELEASE'][0][0][0][0][ifile][3][0][0])
-
-                poses_annots = []
-                for i in range(annotations['RELEASE'][0][0][0][0][ifile][1][0]):
-                    keypoints = [[0, 0, 0]] * 16  # [x, y, is_visible]
-                    for j in range(len(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0])):
-                        idx = annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0][j][0][0][0]
-                        x = annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0][j][1][0][0]
-                        y = annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0][j][2][0][0]
-
-                        if any(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0][j][3]):
-                            is_visible = int(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0][j][3][0])
-                        else:
-                            is_visible = -1
-                        keypoints[idx] = [x, y, is_visible]
-
-                    poses_annots.append({
-                        "x1" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][0][0][0],
-                        "y1" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][1][0][0],
-                        "x2" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][2][0][0],
-                        "y2" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][3][0][0],
-                        "keypoints" : keypoints,
-                        "scale" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][5][0][0],
-                        "objpos" : {
-                            "x" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][6][0][0][0][0][0],
-                            "y" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][6][0][0][1][0][0],
-                        }
-                    })
-            else:
-                frame_sec, video_idx = -1, -1
-
-                poses_annots = []
-                for i in range(annotations['RELEASE'][0][0][0][0][ifile][1][0]):
-                    poses_annots.append({
-                        "scale" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][0][0][0],
-                        "objpos" : {
-                            "x" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][1][0][0][0][0][0],
-                            "y" : annotations['RELEASE'][0][0][0][0][ifile][1][0][i][1][0][0][1][0][0],
-                        }
-                    })
 
             # single person
             single_person = [0]
@@ -136,27 +91,125 @@ class Keypoints:
 
             # activity/action id
             act = {"cat_name" : '', "act_name" : '', "act_id" : -1}
-            if any(annotations['RELEASE'][0][0][4][ifile][0]):
+            if any(annotations['RELEASE'][0][0][4][ifile][0][0]):
                 act = {
                     "cat_name" : str(annotations['RELEASE'][0][0][4][ifile][0][0][0]),
                     "act_name" : str(annotations['RELEASE'][0][0][4][ifile][0][1][0]),
                     "act_id" : int(annotations['RELEASE'][0][0][4][ifile][0][2][0][0])
                 }
 
+            # image annots
+            image_filename = os.path.join('images', str(annotations['RELEASE'][0][0][0][0][ifile][0][0][0][0][0]))
+
+            if any(annotations['RELEASE'][0][0][0][0][ifile][3][0]):
+                frame_sec = int(annotations['RELEASE'][0][0][0][0][ifile][2][0][0])
+                video_idx = int(annotations['RELEASE'][0][0][0][0][ifile][3][0][0])
+            else:
+                frame_sec, video_idx = -1, -1
+
+            # properties field names ('scale', 'objpos', ...)
+            try:
+                pnames = annotations['RELEASE'][0][0][0][0][ifile][1][0].dtype.names
+            except IndexError:
+                data[set_name].append({
+                    "image_filename" : image_filename,
+                    "frame_sec" : frame_sec,
+                    "video_idx" : video_idx,
+                    "poses_annotations" : [],
+                    "activity" : act,
+                    "single_person" : single_person
+                })
+                continue # skip rest
+
+            # parse keypoints
+            poses_annots = []
+            if any(annotations['RELEASE'][0][0][0][0][ifile][3][0]):
+                for i in range(len(annotations['RELEASE'][0][0][0][0][ifile][1][0])):
+                    try:
+                        keypoints = [[0, 0, 0]] * 16  # [x, y, is_visible]
+                        vnames = annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0].dtype.names
+                        for j in range(len(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0])):
+                            x = float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0][j][vnames.index('x')][0][0])
+                            y = float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0][j][vnames.index('y')][0][0])
+                            idx = int(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0][j][vnames.index('id')][0][0])
+
+                            try:
+                                is_visible = int(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][4][0][0][0][0][j][vnames.index('is_visible')][0])
+                            except (ValueError):
+                                is_visible = -1
+
+                            keypoints[idx] = [x, y, is_visible]
+                    except (AttributeError, IndexError):
+                         keypoints = [[0, 0, 0]] * 16 # [x, y, is_visible]
+
+                    try:
+                        x1 = float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('x1')][0][0])
+                        y1 = float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('y1')][0][0])
+                        x2 = float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('x2')][0][0])
+                        y2 = float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('y2')][0][0])
+                    except ValueError:
+                        x1, y1, x2, y2 = -1, -1, -1, -1
+
+                    try:
+                        objnames = annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('objpos')][0].dtype.names
+                        scale = float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('scale')][0][0])
+                        objpos = {
+                            "x" : float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('objpos')][0][0][objnames.index('x')][0][0]),
+                            "y" : float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('objpos')][0][0][objnames.index('y')][0][0])
+                        }
+                    except (ValueError, IndexError):
+                        scale = -1
+                        objpos = {"x" : -1, "y" : -1}
+
+                    poses_annots.append({
+                        "x1" : x1,
+                        "y1" : y1,
+                        "x2" : x2,
+                        "y2" : y2,
+                        "keypoints" : keypoints,
+                        "scale" : scale,
+                        "objpos" : objpos
+                    })
+            else:
+                for i in range(len(annotations['RELEASE'][0][0][0][0][ifile][1][0])):
+                    try:
+                        objnames = annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('objpos')][0].dtype.names
+                        scale = float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('scale')][0][0])
+                        objpos = {
+                            "x" : float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('objpos')][0][0][objnames.index('x')][0][0]),
+                            "y" : float(annotations['RELEASE'][0][0][0][0][ifile][1][0][i][pnames.index('objpos')][0][0][objnames.index('y')][0][0])
+                        }
+                    except (IndexError, ValueError, AttributeError):
+                        scale = -1
+                        objpos = {"x" : -1, "y" : -1}
+
+                    poses_annots.append({
+                        "scale" : scale,
+                        "objpos" : objpos
+                    })
+
             # add fields to data
-            data[set_name] = {
+            data[set_name].append({
                 "image_filename" : image_filename,
                 "frame_sec" : frame_sec,
                 "video_idx" : video_idx,
                 "poses_annotations" : poses_annots,
                 "activity" : act,
                 "single_person" : single_person
-            }
+            })
+
+            # update progressbar
+            if self.verbose:
+                prgbar.update(ifile)
+
+        # update progressbar
+        if self.verbose:
+            prgbar.finish()
 
         # fetch video ids
         videonames = []
         for ivideo in range(len(annotations['RELEASE'][0][0][5][0])):
-            videonames.append(annotations['RELEASE'][0][0][5][0][ivideo][0])
+            videonames.append(str(annotations['RELEASE'][0][0][5][0][ivideo][0]))
 
         return data, videonames
 
@@ -201,19 +254,20 @@ class Keypoints:
             activity_grp['activity_name'] = str2ascii(annot["activity"]["act_name"])
             activity_grp['activity_id'] = np.array(annot["activity"]["act_id"], dtype=np.int32)
 
-            pose_annot_grp = file_grp.create_group("pose_annotations")
-            for j in range(len(annot["poses_annotations"])):
-                pose_grp = pose_annot_grp.create_group(str(j))
-                pose_grp["scale"] = np.array(annot["poses_annotations"][j]["scale"], dtype=np.float)
-                pose_grp["objpos/x"] = np.array(annot["poses_annotations"][j]["objpos"]["x"], dtype=np.float)
-                pose_grp["objpos/y"] = np.array(annot["poses_annotations"][j]["objpos"]["y"], dtype=np.float)
+            if any(annot["poses_annotations"]):
+                pose_annot_grp = file_grp.create_group("pose_annotations")
+                for j in range(len(annot["poses_annotations"])):
+                    pose_grp = pose_annot_grp.create_group(str(j))
+                    pose_grp["scale"] = np.array(annot["poses_annotations"][j]["scale"], dtype=np.float)
+                    pose_grp["objpos/x"] = np.array(annot["poses_annotations"][j]["objpos"]["x"], dtype=np.float)
+                    pose_grp["objpos/y"] = np.array(annot["poses_annotations"][j]["objpos"]["y"], dtype=np.float)
 
-                if "x1" in annot["poses_annotations"][j]:
-                    pose_grp["x1"] = np.array(annot["poses_annotations"][j]["x1"], dtype=np.float)
-                    pose_grp["y1"] = np.array(annot["poses_annotations"][j]["y1"], dtype=np.float)
-                    pose_grp["x2"] = np.array(annot["poses_annotations"][j]["x2"], dtype=np.float)
-                    pose_grp["y2"] = np.array(annot["poses_annotations"][j]["y2"], dtype=np.float)
-                    pose_grp["keypoints"] = np.array(annot["poses_annotations"][j]["keypoints"], dtype=np.float)
+                    if "x1" in annot["poses_annotations"][j]:
+                        pose_grp["x1"] = np.array(annot["poses_annotations"][j]["x1"], dtype=np.float)
+                        pose_grp["y1"] = np.array(annot["poses_annotations"][j]["y1"], dtype=np.float)
+                        pose_grp["x2"] = np.array(annot["poses_annotations"][j]["x2"], dtype=np.float)
+                        pose_grp["y2"] = np.array(annot["poses_annotations"][j]["y2"], dtype=np.float)
+                        pose_grp["keypoints"] = np.array(annot["poses_annotations"][j]["keypoints"], dtype=np.float)
 
 
             # update progressbar
@@ -240,19 +294,19 @@ class Keypoints:
         image_filenames = []
         frame_sec = []
         video_idx = []
-        single_person_id_list = []
+        #single_person_id_list = []
         category_name, activity_name, activity_id = [], [], []
-        scale = []
-        objpos = [] # [x, y]
-        head_bbox = []
-        keypoints = []
+        scale = [-1]
+        objpos = [[-1, -1]] # [x, y]
+        head_bbox = [[-1, -1, -1, -1]]
+        keypoints = [[[-1, -1, -1]] * 16]
 
         object_id = []
 
         if is_train:
-            object_fields = ["image_filenames", "head_bbox",
-                             "keypoints", "scale", "objpos",
-                             "frame_sec", "video_idx"]
+            object_fields = ["image_filenames", "scale", "objpos",
+                             "head_bbox", "keypoints", "frame_sec",
+                             "video_idx"]
         else:
             object_fields = ["image_filenames", "scale", "objpos"]
 
@@ -280,33 +334,47 @@ class Keypoints:
             objs_per_image = []
             keypoints_per_image = []
             single_person_per_image = []
-            for j, pose_annot in enumerate(annot["poses_annotations"]):
-                scale.append(pose_annot["scale"])
-                objpos.append([pose_annot["objpos"]["x"],
-                               pose_annot["objpos"]["y"]])
-
-                if j+1 in annot["single_person"]:
-                    single_person_per_image.append(obj_per_img_counter)
-
+            if not any(annot["poses_annotations"]):
                 if is_train:
-                    head_bbox.append([pose_annot["x1"],
-                                      pose_annot["y1"],
-                                      pose_annot["x2"],
-                                      pose_annot["y2"]])
-                    keypoints.append(pose_annot["keypoints"])
-
-                    object_id.append([i, j, j, j, j, i, i])
-
-                    # add object_id to the keypoint list
-                    keypoints_per_image.append(obj_per_img_counter)
+                    object_id.append([i, 0, 0, 0, 0, i, i])
                 else:
-                    object_id.append([i, j, j])
+                    object_id.append([i, 0, 0])
 
                 # add object_id to the list
                 objs_per_image.append(obj_per_img_counter)
 
                 # update counter
                 obj_per_img_counter += 1
+            else:
+                for j, pose_annot in enumerate(annot["poses_annotations"]):
+                    scale.append(pose_annot["scale"])
+                    objpos.append([pose_annot["objpos"]["x"],
+                                   pose_annot["objpos"]["y"]])
+
+                    if j+1 in annot["single_person"]:
+                        single_person_per_image.append(obj_per_img_counter)
+
+                    if is_train:
+                        head_bbox.append([pose_annot["x1"],
+                                          pose_annot["y1"],
+                                          pose_annot["x2"],
+                                          pose_annot["y2"]])
+
+                        keypoints.append(pose_annot["keypoints"])
+
+                        object_id.append([i, obj_per_img_counter, obj_per_img_counter,
+                                          obj_per_img_counter, obj_per_img_counter, i, i])
+
+                        # add object_id to the keypoint list
+                        keypoints_per_image.append(obj_per_img_counter)
+                    else:
+                        object_id.append([i, obj_per_img_counter, obj_per_img_counter])
+
+                    # add object_id to the list
+                    objs_per_image.append(obj_per_img_counter)
+
+                    # update counter
+                    obj_per_img_counter += 1
 
             list_object_ids_per_image.append(objs_per_image)
             list_single_person_per_image.append(single_person_per_image)
