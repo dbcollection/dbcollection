445be77c9ad4c2f23f1eb8843a7aeb46d276c9f3
farrajota <mlgfarrajota@outlook.com>
2017-05-14 18:36:26 +0100

    Refactor mpii tasks using BaseTask

--- dbcollection/datasets/mpii_pose/keypoints.py
+++ dbcollection/datasets/mpii_pose/keypoints.py
@@ -6,15 +6,16 @@ MPII Keypoints process functions.
 from __future__ import print_function, division
 import os
 import numpy as np
-import h5py
 import progressbar
+
+from dbcollection.datasets.dbclass import BaseTask
 
 from dbcollection.utils.string_ascii import convert_str_to_ascii as str2ascii
 from dbcollection.utils.pad import pad_list
 from dbcollection.utils.file_load import load_matlab
 
 
-class Keypoints:
+class Keypoints(BaseTask):
     """ MPII Keypoints preprocessing functions """
 
     # metadata filename
@@ -38,15 +39,6 @@ class Keypoints:
         'left elbow',       #-- 15
         'left wrist'        #-- 16
     ]
-
-
-    def __init__(self, data_path, cache_path, verbose=True):
-        """
-        Initialize class.
-        """
-        self.cache_path = cache_path
-        self.data_path = data_path
-        self.verbose = verbose
 
 
     def load_annotations(self):
@@ -410,49 +402,6 @@ class Keypoints:
             handler['keypoints'] = np.array(keypoints, dtype=np.float)
 
             handler['list_keypoints_per_image'] = np.array(pad_list(list_keypoints_per_image, -1), dtype=np.int32)
-
-
-    def process_metadata(self):
-        """
-        Process metadata and store it in a hdf5 file.
-        """
-
-        # create/open hdf5 file with subgroups for train/val/test
-        file_name = os.path.join(self.cache_path, self.filename_h5 + '.h5')
-        fileh5 = h5py.File(file_name, 'w', version='latest')
-
-        if self.verbose:
-            print('\n==> Storing metadata to file: {}'.format(file_name))
-
-        # setup data generator
-        data_gen = self.load_data()
-
-        for data in data_gen:
-            for set_name in data:
-
-                if self.verbose:
-                    print('\nSaving set metadata: {}'.format(set_name))
-
-                # add data to the **source** group
-                sourceg = fileh5.create_group('source/' + set_name)
-                self.add_data_to_source(sourceg, data[set_name])
-
-                # add data to the **default** group
-                defaultg = fileh5.create_group('default/' + set_name)
-                self.add_data_to_default(defaultg, data[set_name], set_name)
-
-        # close file
-        fileh5.close()
-
-        # return information of the task + cache file
-        return file_name
-
-
-    def run(self):
-        """
-        Run task processing.
-        """
-        return self.process_metadata()
 
 
 class KeypointsNoSourceGrp(Keypoints):
