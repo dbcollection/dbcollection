87b00eed1f8daf14abcea4ff620f28f27b50c731
farrajota <mlgfarrajota@outlook.com>
2017-05-30 15:58:57 +0100

    Bump package version

--- setup.py
+++ setup.py
@@ -19,10 +19,10 @@ with open('requirements.txt') as f:
 
 setup(
     name='dbcollection',
-    version='0.1.0',
+    version='0.1.1',
     author='Miguel Farrajota',
     url='https://github.com/farrajota/dbcollection',
-    download_url='https://github.com/farrajota/dbcollection/archive/0.1.0.tar.gz',
+    download_url='https://github.com/farrajota/dbcollection/archive/0.1.1.tar.gz',
     description='Cross-platform, cross-language dataset metadata manager for machine learning',
     long_description=long_description,
     license=license_mit,
