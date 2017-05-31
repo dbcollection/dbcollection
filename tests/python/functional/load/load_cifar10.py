a0345adbfd58f4e5dc2281f70f64a314fb157d7b
farrajota <mglfarrajota@outlook.com>
2017-05-31 17:19:48 +0100

    Improve functional test comments for cifar10

--- tests/python/functional/load/load_cifar10.py
+++ tests/python/functional/load/load_cifar10.py
@@ -13,13 +13,17 @@ import dbcollection.manager as dbc
 data_dir = os.path.join(os.path.expanduser("~"), 'tmp', 'download_data')
 
 
-# delete all cache data + dir
+# delete any existing cache data + dir on disk
 print('\n==> dbcollection: config_cache()')
 dbc.config_cache(delete_cache=True, is_test=True)
 
 # download/setup dataset
 print('\n==> dbcollection: load()')
-cifar10 = dbc.load(name='cifar10', task='classification', data_dir=data_dir, verbose=True, is_test=True)
+cifar10 = dbc.load(name='cifar10',
+                   task='classification',
+                   data_dir=data_dir,
+                   verbose=True,
+                   is_test=True)
 
 # print data from the loader
 print('\n==> dbcollection: info()')
@@ -32,6 +36,6 @@ print('Task: ' + cifar10.task)
 print('Data path: ' + cifar10.data_dir)
 print('Metadata cache path: ' + cifar10.cache_path)
 
-# delete all cache data + dir
+# wipe the generated cache data + dir from the disk
 print('\n==> dbcollection: config_cache()')
 dbc.config_cache(delete_cache=True, is_test=True)
