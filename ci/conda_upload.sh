cd736f227b7d9864efd83a3f62d351a7777ad155
farrajota <mglfarrajota@outlook.com>
2017-06-05 17:47:24 +0100

    Enable pypi deploy and osx tests

--- ci/conda_upload.sh
+++ ci/conda_upload.sh
@@ -6,7 +6,7 @@ OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-conda build .
+conda build conda-recipe --quit
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME*.tar.bz2 --force
 
 # Note:
