c9d3339653fdf55db73ca2e6638b6f0fbe5b94a7
farrajota <mglfarrajota@outlook.com>
2017-06-26 16:59:39 +0100

    test: Refactor process tests

---
+++ tests/python/functional/process/ucf101.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Test loading ucf101.
+"""
+
+import os
+from dbcollection.utils.test import TestBaseDB
+
+
+# setup
+name = 'ucf101'
+task = 'recognition'
+data_dir = os.path.join(os.path.expanduser("~"), 'tmp', 'download_data')
+verbose = True
+
+# Run tester
+tester = TestBaseDB(name, task, data_dir, verbose)
+tester.run('process')
