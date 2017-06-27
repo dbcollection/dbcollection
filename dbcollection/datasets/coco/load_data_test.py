109b9e42a5ffcdfb30d5a0e7fb435e7f34ba1aa4
farrajota <mglfarrajota@outlook.com>
2017-06-27 09:56:25 +0100

    db: Remove whitespaces in coco test

--- dbcollection/datasets/coco/load_data_test.py
+++ dbcollection/datasets/coco/load_data_test.py
@@ -18,14 +18,14 @@ def load_data_test(set_name, image_dir, annotation_path, verbose=True):
     # parse annotations
     # images
     if verbose:
-        print('> Processing image annotations... ')
+        print('> Processing image annotations...')
     for i, annot in enumerate(annotations['images']):
         data[annot['file_name']] = {
-            "file_name" : os.path.join(image_dir, annot['file_name']),
-            "width" : annot['width'],
-            "height" : annot['height'],
-            "id" : annot['id'],
-            "coco_url" : annot['coco_url'],
+            "file_name": os.path.join(image_dir, annot['file_name']),
+            "width": annot['width'],
+            "height": annot['height'],
+            "id": annot['id'],
+            "coco_url": annot['coco_url'],
         }
 
     filename_ids = {}
@@ -34,23 +34,23 @@ def load_data_test(set_name, image_dir, annotation_path, verbose=True):
 
     # categories
     if verbose:
-        print('> Processing category annotations... ')
+        print('> Processing category annotations...')
     categories = {}
     category_list, supercategory_list, category_id = [], [], []
     for i, annot in enumerate(annotations['categories']):
         categories[annot['id']] = {
-            "name" : annot['name'],
-            "supercategory" : annot['supercategory'],
-            "id" : annot['id']
+            "name": annot['name'],
+            "supercategory": annot['supercategory'],
+            "id": annot['id']
         }
         category_id.append(annot['id'])
         category_list.append(annot['name'])
         supercategory_list.append(annot['supercategory'])
     supercategory_list = list(set(supercategory_list))
 
-    return {set_name : [OrderedDict(sorted(data.items())),
-                        filename_ids,
-                        annotations,
-                        category_list,
-                        supercategory_list,
-                        category_id]}
+    return {set_name: [OrderedDict(sorted(data.items())),
+                       filename_ids,
+                       annotations,
+                       category_list,
+                       supercategory_list,
+                       category_id]}
