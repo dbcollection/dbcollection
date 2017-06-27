8410e271ead4581cc7c9750c3f7e5030f21742a8
farrajota <mglfarrajota@outlook.com>
2017-06-27 16:25:39 +0100

    db: Add list to coco keypoint script for better visualization

--- dbcollection/datasets/coco/keypoints.py
+++ dbcollection/datasets/coco/keypoints.py
@@ -38,6 +38,28 @@ class Keypoints2016(BaseTask):
         "test-dev": os.path.join('annotations', 'image_info_test-dev2015.json')
     }
 
+    keypoints_list = {
+        'nose',             #-- 1
+        'left_eye',         #-- 2
+        'right_eye',        #-- 3
+        'left_ear',         #-- 4
+        'right_ear',        #-- 5
+
+        'left_shoulder',    #-- 6
+        'right_shoulder',   #-- 7
+        'left_elbow',       #-- 8
+        'right_elbow',      #-- 9
+        'left_wrist',       #-- 10
+        'right_wrist',      #-- 11
+
+        'left_hip',         #-- 12
+        'right_hip',        #-- 13
+        'left_knee',        #-- 14
+        'right_knee',       #-- 15
+        'left_ankle',       #-- 16
+        'right_ankle'       #-- 17
+    }
+
 
     def parse_image_annotations(self, image_dir, annotations):
         """
