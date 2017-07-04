ee0a6b36a8dade563fba4bcb6a2ac62a1004d0ae
farrajota <mglfarrajota@outlook.com>
2017-07-04 15:25:04 +0100

    matlab: Add tests using MOxUnit framework

--- APIs/matlab/tests/test_utils.m
+++ APIs/matlab/tests/test_utils.m
@@ -1,11 +1,50 @@
-% Test dbcollection util functions
+function test_suite=test_utils
+    % Test dbcollection util functions
 
-%% pad_list
-function test_utils_pad_list
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions=localfunctions();
+    catch % no problem; early Matlab versions can use initTestSuite fine
+    end
+    initTestSuite;
 
 
-%% unpad_list
+function test_pad_list
+    assertTrue(true);
 
-%% convert_str_to_ascii
+function test_unpad_list
+    assertTrue(true)
 
-%% convert_ascii_to_str
+function test_convert_str_to_ascii_single
+    sample_str = 'test_string';
+    sample_res = padarray(double(sample_str), [0 1], 0, 'post');
+    utils = dbcollection_utils();
+    res = utils.string_ascii.convert_str_to_ascii(sample_str);
+    assertEqual(sample_res, res)
+
+function test_convert_str_to_ascii_matrix
+    sample_str = ['test_string'; 'string_test'];
+    sample_res = padarray(double(sample_str), [0 1], 0, 'post');
+    utils = dbcollection_utils();
+    res = utils.string_ascii.convert_str_to_ascii(sample_str);
+    assertEqual(sample_res, res)
+
+function test_convert_str_to_ascii_cell
+    sample_str = {'test_string'; 'string_test'};
+    sample_res = padarray(double(char(sample_str)), [0 1], 0, 'post');
+    utils = dbcollection_utils();
+    res = utils.string_ascii.convert_str_to_ascii(sample_str);
+    assertEqual(sample_res, res)
+
+function test_convert_ascii_to_str_single
+    sample_str = 'test_string';
+    sample_ascii = padarray(double(char(sample_str)), [0 1], 0, 'post');
+    utils = dbcollection_utils();
+    res = utils.string_ascii.convert_ascii_to_str(sample_ascii);
+    assertEqual(sample_str, res)
+
+function test_convert_ascii_to_str_matrix
+    sample_str = ['test_string'; 'string_test'];
+    sample_ascii = padarray(double(char(sample_str)), [0 1], 0, 'post');
+    utils = dbcollection_utils();
+    res = utils.string_ascii.convert_ascii_to_str(sample_ascii);
+    assertEqual(sample_str, res)
