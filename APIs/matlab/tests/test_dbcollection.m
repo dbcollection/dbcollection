8f44f54a36a5ca8e624e2521f4df07fe5a0b8232
farrajota <mglfarrajota@outlook.com>
2017-07-04 16:59:00 +0100

    matlab Add tests scafold for the dbcollection API

--- APIs/matlab/tests/test_dbcollection.m
+++ APIs/matlab/tests/test_dbcollection.m
@@ -1,21 +1,58 @@
-% Test dbcollection API
+function test_suite=test_dbcollection
+    % Test dbcollection API
 
-%% Load dbcollection
-dbc = dbcollection();
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions=localfunctions();
+    catch % no problem; early Matlab versions can use initTestSuite fine
+    end
+    initTestSuite;
 
-%% Load dataset
+function info = fetch_minst_info()
+    info = struct();
+    info.name = 'mnist';
+    info.task = 'classification';
+    info.data_dir = [getenv('HOME') '/tmp/download_data'];
+    info.is_test = true;
 
-%% Download dataset
+function test_load_mnist
+    dbc = dbcollection();
+    info = fetch_minst_info();
+    loader = dbc.load(info);
+    assertEqual(info.name, loader.name)
+    assertEqual(info.task, loader.task)
+    assertEqual(info.data_dir, loader.data_dir)
 
-%% process dataset
+function test_download_mnist
+    % TODO
+    dbc = dbcollection();
+    assertTrue(true);
 
-%% add dataset to list
+function test_process_mnist
+    % TODO
+    dbc = dbcollection();
+    assertTrue(true);
 
-%% remove dataset to list
+function test_add_dataset
+    % TODO
+    dbc = dbcollection();
+    assertTrue(true);
 
-%% config cache
+function test_remove_dataset
+    % TODO
+    dbc = dbcollection();
+    assertTrue(true);
 
-%% query cache
+function test_config_cache
+    % TODO
+    dbc = dbcollection();
+    assertTrue(true);
 
-%% Test info display
-dbc.info(struct('is_test', true))
+function test_query_cache
+    % TODO
+    dbc = dbcollection();
+    assertTrue(true);
+
+function test_info_print_cache_contents
+    dbc = dbcollection();
+    dbc.info(struct('is_test', true))
+    assertTrue(true);
