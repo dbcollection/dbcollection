f34653eace4a9122f6f21d8d973c9a95872008f6
farrajota <mglfarrajota@outlook.com>
2017-07-04 16:20:31 +0100

    matlab: Add tests to the data loader class

--- APIs/matlab/tests/test_DatasetLoader.m
+++ APIs/matlab/tests/test_DatasetLoader.m
@@ -1,27 +1,102 @@
-% Test dbcollection's dataset loader API
+function test_suite=test_DatasetLoader
+    % Test dbcollection's dataset loader API
 
-%% Load file
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions=localfunctions();
+    catch % no problem; early Matlab versions can use initTestSuite fine
+    end
+    initTestSuite;
 
-name = 'cifar10';
-task = 'classification';
-data_dir = '/home/mf/tmp/download_data/cifar10';
-cache_path = '/home/mf/dbcollection/cifar10/classification.h5';
+function [loader, utils] = setup_ini()
+    name = 'mnist';
+    task = 'classification';
+    data_dir = [getenv('HOME') '/dbcollection/mnist/data'];
+    cache_path = [getenv('HOME') '/dbcollection/mnist/classification.h5'];
 
-%% initialize object
+    % initialize object
+    loader = dbcollection_DatasetLoader(name, task, data_dir, cache_path);
+    utils = dbcollection_utils();
+    
+function test_get
+    sample_classes = ['0'; '1'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9'];
+    
+    % setup data loader + utils class
+    [loader, utils] = setup_ini();
 
-loader = dbcollection_DatasetLoader(name, task, data_dir, cache_path);
-utils = dbcollection_utils();
+    % call get() method
+    data = loader.get('train', 'classes');
+    % convert double to char
+    classes = utils.string_ascii.convert_ascii_to_str(data);
 
-%% get()
+    assertEqual(sample_classes, classes);
 
-data = loader.get('train', 'classes');
-classes = utils.string_ascii.convert_ascii_to_str(data);
-disp(classes)
+function test_object
+    sample_ids = [0, 5];
+    
+    % setup data loader + utils class
+    [loader, ~] = setup_ini();
 
-%% object()
+    % call object() method
+    ids = loader.object('train', 1); 
+    
+    assertEqual(sample_ids, double(ids));  % int32 -> double
 
-%% size()
+function test_size_1
+    sample_ids = [10, 2];
+    
+    % setup data loader + utils class
+    [loader, ~] = setup_ini();
 
-%% list()
+    % call object() method
+    cls_size = loader.size('train', 'classes'); 
+    
+    assertEqual(sample_ids, cls_size);
 
-%% object_field_id()
+function test_size_2
+    sample_ids = [60000, 2];
+    
+    % setup data loader + utils class
+    [loader, ~] = setup_ini();
+
+    % call object() method
+    cls_size = loader.size('train'); 
+    
+    assertEqual(sample_ids, cls_size);     
+
+function test_list
+    sample_field_names = {'classes', ...
+                          'images', ...
+                          'labels', ...
+                          'list_images_per_class', ...
+                          'object_fields', ...
+                          'object_ids'};
+    
+    % setup data loader + utils class
+    [loader, ~] = setup_ini();
+
+    % call object() method
+    field_names = loader.list('train'); 
+    
+    assertEqual(sample_field_names, field_names);
+    
+function test_object_field_id_1
+    sample_idx = 1;
+    
+    % setup data loader + utils class
+    [loader, ~] = setup_ini();
+
+    % call object() method
+    res = loader.object_field_id('train', 'images'); 
+    
+    assertEqual(sample_idx, res);
+    
+function test_object_field_id_2
+    sample_idx = 2;
+    
+    % setup data loader + utils class
+    [loader, ~] = setup_ini();
+
+    % call object() method
+    res = loader.object_field_id('train', 'labels'); 
+    
+    assertEqual(sample_idx, res);    
