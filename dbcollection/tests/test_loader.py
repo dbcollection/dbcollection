7dfe27942029de7eca2a5eb298e04ad8190e6ff6
farrajota <mglfarrajota@outlook.com>
2017-07-17 17:59:05 +0100

    python: Add tests to loader.py

--- dbcollection/tests/test_loader.py
+++ dbcollection/tests/test_loader.py
@@ -4,31 +4,88 @@ Test dbcollection/utils/loader.py.
 
 
 import os
-import sys
 import pytest
 from dbcollection.loader import DatasetLoader
+from dbcollection.utils.string_ascii import convert_ascii_to_str as tostr_
 
 
-def test_get():
-    pass
+home_dir = os.path.expanduser("~")
+name = 'mnist'
+task = 'classification'
+data_dir = os.path.join(home_dir, 'dbcollection', 'mnist', 'data')
+cache_path = os.path.join(home_dir, 'dbcollection','mnist','classification.h5')
+loader = DatasetLoader(name, task, data_dir, cache_path)
 
-def test_object__get_idx():
-    pass
 
-def test_object__get_values():
-    pass
+@pytest.mark.parametrize("output", [
+    (['0', '1','2', '3', '4', '5', '6', '7', '8', '9']),
+])
+def test_get_all(output):
+    assert(output == tostr_(loader.get('train', 'classes')))
 
-def test_size__default():
-    pass
 
-def test_size__full():
-    pass
+@pytest.mark.parametrize("output", [
+    (['0', '1','2', '3', '4', '5', '6', '7', '8', '9']),
+    (['0', '1','2', '3', '4']),
+])
+def test_get_range(output):
+    assert(output == tostr_(loader.get('train', 'classes', list(range(len(output))))))
+
+
+@pytest.mark.parametrize("output", [
+    (['5', '6', '7', '8', '9']),
+    (['1','2', '4']),
+])
+def test_get_range2(output):
+    idx_list = [int(l) for l in output]
+    assert(output == tostr_(loader.get('train', 'classes', idx_list)))
+
+
+@pytest.mark.parametrize("output, index", [
+    ([0, 5], 0),
+    ([1, 0], 1),
+    ([2, 4], 2),
+    ([3, 1], 3),
+    ([4, 9], 4),
+])
+def test_object_single(output, index):
+    assert(output == loader.object('train', index).tolist())
+
+
+@pytest.mark.parametrize("output, index", [
+    ([[0, 5], [1, 0]], [0, 1]),
+    ([[2, 4], [3, 1]], [2, 3]),
+    ([[4, 9], [5, 2]], [4, 5]),
+])
+def test_object_two(output, index):
+    assert(output == loader.object('train', index).tolist())
+
+
+@pytest.mark.parametrize("field_name, output", [
+    ('classes', [10, 2]),
+    ('images', [60000, 28, 28]),
+    ('object_ids', [60000, 2]),
+])
+def test_size_1(field_name, output):
+    assert(output == loader.size('train', field_name))
+
+
+def test_size_2():
+    assert([60000, 2] == loader.size('train'))
 
 def test_list():
-    pass
+    sample_field_names = ['classes',
+                          'images',
+                          'labels',
+                          'list_images_per_class',
+                          'object_fields',
+                          'object_ids']
+    assert(sample_field_names == loader.list('train'))
 
-def test_object_field_id__succeed():
-    pass
 
-def test_object_field_id__fail():
-    pass
+@pytest.mark.parametrize("field_name, output", [
+    ('images', 0),
+    ('labels', 1),
+])
+def test_object_field_id(field_name, output):
+    assert(output == loader.object_field_id('train', field_name))
