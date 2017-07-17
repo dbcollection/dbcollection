909770c8e1ec7b8989850411cd7bd59ce761333f
farrajota <mglfarrajota@outlook.com>
2017-07-17 16:23:24 +0100

    python: Refactor pad tests

--- dbcollection/tests/test_pad.py
+++ dbcollection/tests/test_pad.py
@@ -4,7 +4,7 @@ Test dbcollection/utils/pad.py.
 
 
 import pytest
-from dbcollection.utils.pad import pad_list, pad_list2, unpad_list, unpad_list2
+from dbcollection.utils.pad import pad_list, unpad_list, squeeze_list, unsqueeze_list
 
 
 @pytest.mark.parametrize("sample, output, fill_value", [
@@ -20,25 +20,6 @@ def test_pad_list(sample, output, fill_value):
 
 
 @pytest.mark.parametrize("sample, output, fill_value", [
-    ([[[1, 2]],
-     [[1], [2]],
-     [[1], [2, 3], [4, 5, 6], [7, 8, 9, 10, 11, 12]]],
-     [[[1, 2, -1, -1, -1, -1],  [-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1]],
-      [[1, -1, -1, -1, -1, -1], [2, -1, -1, -1, -1, -1],  [-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1]],
-      [[1, -1, -1, -1, -1, -1], [2, 3, -1, -1, -1, -1],   [4, 5, 6, -1, -1, -1],    [7, 8, 9, 10, 11, 12]]],
-     -1),
-
-     ([[[1, 2]],
-      [[1], [2], [3]]],
-     [[[1, 2], [9999, 9999], [9999, 9999]],
-      [[1, 9999], [2, 9999], [3, 9999]]],
-     9999),
-])
-def test_pad_list2(sample, output, fill_value):
-    assert(output == pad_list2(sample, fill_value))
-
-
-@pytest.mark.parametrize("sample, output, fill_value", [
     ([[1, 2, 3, -1, -1], [5, 6, -1, -1, -1]], [[1, 2, 3], [5, 6]], -1),
     ([[5, 0, -1], [1, 2, 3, 4, 5]], [[0, -1], [1, 2, 3, 4]], 5),
 ])
@@ -47,8 +28,16 @@ def test_unpad_list(sample, output, fill_value):
 
 
 @pytest.mark.parametrize("sample, output, fill_value", [
-    ([[[1,2,3,-1,-1],[5,6,-1,-1,-1]]], [[[1, 2, 3], [5, 6]]], -1),
-    ([[[1,2,3,-1,-1],[5,6,-1,-1,-1], [-1,-1,-1,-1,-1,-1]]], [[[1, 2, 3], [5, 6]]], -1),
+    ([[1,2], [3], [4,5,6]], [1, 2, -1, 3, -1, 4, 5, 6], -1),
+    ([[1,1,1,1,1], [-1,-1,-1,-1,-1]], [1,1,1,1,1,9999, -1,-1,-1,-1,-1], 9999)
 ])
-def test_unpad_list2(sample, output, fill_value):
-    assert(output == unpad_list2(sample, fill_value))
+def test_squeeze_list(sample, output, fill_value):
+    assert(output == squeeze_list(sample, fill_value))
+
+
+@pytest.mark.parametrize("sample, output, fill_value", [
+    ([1, 2, -1, 3, -1, 4, 5, 6], [[1,2], [3], [4,5,6]], -1),
+    ([1,1,1,1,1,9999, -1,-1,-1,-1,-1], [[1,1,1,1,1], [-1,-1,-1,-1,-1]], 9999)
+])
+def test_unsqueeze_list(sample, output, fill_value):
+    assert(output == unsqueeze_list(sample, fill_value))
