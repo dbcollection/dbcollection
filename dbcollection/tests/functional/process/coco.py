9cfbfe33fa79b8f12e3659ae4f8665dbfd2ddf45
farrajota <mglfarrajota@outlook.com>
2017-07-18 15:18:31 +0100

    python: Add test

---
+++ dbcollection/tests/functional/process/coco.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Test processing coco.
+"""
+
+import os
+from dbcollection.utils.test import TestBaseDB
+
+
+# setup
+name = 'coco'
+task = 'detection_d'
+data_dir = os.path.join(os.path.expanduser("~"), 'tmp', 'download_data')
+verbose = True
+
+# Run tester
+tester = TestBaseDB(name, task, data_dir, verbose)
+tester.run('process')
