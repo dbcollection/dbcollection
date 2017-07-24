08467c5771298db273bcf0f0e1d880ea5954d1cf
farrajota <mglfarrajota@outlook.com>
2017-07-24 17:30:01 +0100

    Add a test for empty indexes for the n.object() method

--- dbcollection/tests/test_loader.py
+++ dbcollection/tests/test_loader.py
@@ -59,6 +59,9 @@ def test_object_two(output, index):
 def test_object_no_index():
     assert((60000, 2) == loader.object('train').shape)
 
+def test_object_empty_index():
+    assert((60000, 2) == loader.object('train', []).shape)
+
 @pytest.mark.parametrize("field_name, output", [
     ('classes', [10, 2]),
     ('images', [60000, 28, 28]),
