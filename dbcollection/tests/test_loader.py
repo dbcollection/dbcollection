74ca231d77707fdf605fc41c3e293a31ebac3bb4
farrajota <mglfarrajota@outlook.com>
2017-09-30 11:48:03 +0100

    Replace mnist with cifar10

--- dbcollection/tests/test_loader.py
+++ dbcollection/tests/test_loader.py
@@ -9,7 +9,7 @@ import dbcollection as dbc
 from dbcollection.utils.string_ascii import convert_ascii_to_str as tostr_
 
 
-loader = dbc.load(name='mnist')
+loader = dbc.load(name='cifar10')
 
 
 @pytest.mark.parametrize("output", [
