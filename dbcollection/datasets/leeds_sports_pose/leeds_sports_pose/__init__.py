e299d85433bb2d54c788f65e1fad7239d61f1d35
farrajota <mglfarrajota@outlook.com>
2017-09-30 15:49:47 +0100

    Refactor lsp

---
+++ dbcollection/datasets/leeds_sports_pose/leeds_sports_pose/__init__.py
@@ -0,0 +1,28 @@
+"""
+Leeds Sports Pose (LSP) Dataset download/process functions.
+"""
+
+
+from dbcollection.datasets import BaseDataset
+from .keypoints import Keypoints, KeypointsOriginal
+
+urls = (
+    'http://sam.johnson.io/research/lsp_dataset_original.zip',
+    {
+        'url': 'http://sam.johnson.io/research/lsp_dataset.zip',
+        'extract_dir': 'lsp_dataset'
+    }
+)
+keywords = ('image_processing', 'detection', 'human pose', 'keypoints')
+tasks = {
+    "keypoints": Keypoints,
+    "keypoints_original": KeypointsOriginal
+}
+default_task = 'keypoints'
+
+class Dataset(BaseDataset):
+    """ Leeds Sports Pose (LSP) Dataset preprocessing/downloading functions """
+    urls = urls
+    keywords = keywords
+    tasks = tasks
+    default_task = default_task
