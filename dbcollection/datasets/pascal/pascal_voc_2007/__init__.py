468b03de9406b74f7c779dca035dd46ee43bc822
farrajota <mglfarrajota@outlook.com>
2017-09-30 15:53:51 +0100

    Refactor pascal voc 2007

---
+++ dbcollection/datasets/pascal/pascal_voc_2007/__init__.py
@@ -0,0 +1,22 @@
+"""
+Pascal VOC 2007 download/process functions.
+"""
+
+
+from dbcollection.datasets import BaseDataset
+from .detection import Detection
+
+urls = (
+    'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar',
+    'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar'
+)
+keywords = ('image_processing', 'object_detection')
+tasks = {"detection": Detection}
+default_task = 'detection'
+
+class Dataset(BaseDataset):
+    """ Pascal VOC 2007 preprocessing/downloading class """
+    urls = urls
+    keywords = keywords
+    tasks = tasks
+    default_task = default_task
