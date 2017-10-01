e91953d3da9d56ecb497756af5719047e82efbc5
farrajota <mglfarrajota@outlook.com>
2017-10-01 18:04:14 +0100

    Add test for db methods/classes

---
+++ dbcollection/tests/core/test_db.py
@@ -0,0 +1,10 @@
+"""
+Test dbcollection dataset methods/classes
+"""
+
+import pytest
+from dbcollection.core.db import fetch_list_datasets
+
+def test_fetch_list_datasets():
+    db_list = fetch_list_datasets()
+    assert 'mnist' in db_list
