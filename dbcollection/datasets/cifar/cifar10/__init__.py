ead98a8fa85d4fa489306dd89ba3e8b883f68907
farrajota <mglfarrajota@outlook.com>
2017-10-04 12:31:30 +0100

    Modify the hdf5 data write scheme for cifar10

--- dbcollection/datasets/cifar/cifar10/__init__.py
+++ dbcollection/datasets/cifar/cifar10/__init__.py
@@ -6,11 +6,13 @@ Cifar10 download/process functions.
 from dbcollection.core.db import BaseDataset
 from .classification import Classification
 
+
 urls = ({'url': 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
          'md5hash': 'c58f30108f718f92721af3b95e74349a'})
 keywords = ('image_processing', 'classification')
 tasks = {"classification": Classification}
 default_task = 'classification'
+
 
 class Dataset(BaseDataset):
     """ Cifar10 preprocessing/downloading functions """
