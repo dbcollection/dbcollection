76b419b6a39facbc12655571ae7a797003b4ba99
farrajota <mglfarrajota@outlook.com>
2017-10-04 12:22:48 +0100

    Rename method

--- dbcollection/utils/hdf5.py
+++ dbcollection/utils/hdf5.py
@@ -2,11 +2,13 @@
 hdf5 utility functions
 """
 
+
 import numpy as np
 
-def hdf5_store_data(h5_handler, field_name, data, dtype=None, chunks=True,
+
+def hdf5_write_data(h5_handler, field_name, data, dtype=None, chunks=True,
                     compression="gzip", compression_opts=4, fillvalue=-1):
-    """Store data of a variable in a hdf5 file."""
+    """Write/store data into a hdf5 file."""
     assert h5_handler, "Must input a hdf5 file handler"
     assert field_name, 'Must input a field name.'
     assert data, 'Must input a data field.'
