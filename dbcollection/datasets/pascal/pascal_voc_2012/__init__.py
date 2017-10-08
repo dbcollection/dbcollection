9d82114d2442adc1896d4ae3e9a97b991429e0e4
farrajota <mglfarrajota@outlook.com>
2017-10-08 22:07:27 +0100

    Fix fullpaths in pascal_voc_2012

--- dbcollection/datasets/pascal/pascal_voc_2012/__init__.py
+++ dbcollection/datasets/pascal/pascal_voc_2012/__init__.py
@@ -6,14 +6,14 @@ Pascal VOC 2012 download/process functions.
 from dbcollection.core.db import BaseDataset
 from .detection import Detection
 
-urls = ('http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar')
+urls = ('http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar',)
 keywords = ('image_processing', 'object_detection')
 tasks = {"detection" : Detection}
 default_task = 'detection'
 
 
 class Dataset(BaseDataset):
-    """Pascal VOC 2012 preprocessing/downloading class """
+    """Pascal VOC 2012 preprocessing/downloading class."""
     urls = urls
     keywords = keywords
     tasks = tasks
