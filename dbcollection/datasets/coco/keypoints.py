f949959c8b5f6f13a5e4a3d508e35848dd1192c0
farrajota <mglfarrajota@outlook.com>
2017-10-08 21:26:17 +0100

    Fix fullpaths in coco and the source save data method

--- dbcollection/datasets/coco/keypoints.py
+++ dbcollection/datasets/coco/keypoints.py
@@ -212,7 +212,7 @@ class Keypoints2016(BaseTask):
                 print('\n> Loading data files for the set: ' + set_name)
 
             # image dir
-            image_dir = self.image_dir_path[set_name]
+            image_dir = os.path.join(self.data_path, self.image_dir_path[set_name])
 
             # annotation file path
             annot_filepath = os.path.join(self.data_path, self.annotation_path[set_name])
@@ -227,7 +227,7 @@ class Keypoints2016(BaseTask):
         """
         Store classes + filenames as a nested tree.
         """
-        image_dir = self.image_dir_path[set_name]
+        image_dir = os.path.join(self.data_path, self.image_dir_path[set_name])
         if 'test' in set_name:
             is_test = True
             data_ = data[0]
@@ -265,11 +265,11 @@ class Keypoints2016(BaseTask):
         image_grp = hdf5_handler.create_group('images')
         for i, annot in enumerate(annotations['images']):
             file_grp = image_grp.create_group(str(i))
-            hdf5_write_data(hdf5_handler, 'file_name', str2ascii(os.path.join(image_dir, annot["file_name"])), dtype=np.uint8, fillvalue=0)
-            hdf5_write_data(hdf5_handler, 'coco_url', str2ascii(annot["coco_url"]), dtype=np.uint8, fillvalue=0)
-            hdf5_write_data(hdf5_handler, 'width', np.array(annot["width"], dtype=np.int32), fillvalue=-1)
-            hdf5_write_data(hdf5_handler, 'height', np.array(annot["height"], dtype=np.int32), fillvalue=-1)
-            hdf5_write_data(hdf5_handler, 'id', np.array(annot["id"], dtype=np.int32), fillvalue=-1)
+            file_grp['file_name'] = str2ascii(os.path.join(image_dir, annot["file_name"]))
+            file_grp['coco_url'] = str2ascii(annot["coco_url"])
+            file_grp['width'] = np.array(annot["width"], dtype=np.int32)
+            file_grp['height'] = np.array(annot["height"], dtype=np.int32)
+            file_grp['id'] = np.array(annot["id"], dtype=np.int32)
 
             # update progressbar
             if self.verbose:
@@ -284,12 +284,12 @@ class Keypoints2016(BaseTask):
         cat_grp = hdf5_handler.create_group('categories')
         for i, annot in enumerate(annotations['categories']):
             file_grp = cat_grp.create_group(str(i))
-            hdf5_write_data(hdf5_handler, 'supercategory', str2ascii(annot["supercategory"]), dtype=np.uint8, fillvalue=0)
-            hdf5_write_data(hdf5_handler, 'name', str2ascii(annot["name"]), dtype=np.uint8, fillvalue=0)
-            hdf5_write_data(hdf5_handler, 'id', np.array(annot["id"], dtype=np.int32), fillvalue=-1)
+            file_grp['supercategory'] = str2ascii(annot["supercategory"])
+            file_grp['name'] = str2ascii(annot["name"])
+            file_grp['id'] = np.array(annot["id"], dtype=np.int32)
             if not is_test:
-                hdf5_write_data(hdf5_handler, 'keypoints', str2ascii(annot["keypoints"]), dtype=np.uint8, fillvalue=-1)
-                hdf5_write_data(hdf5_handler, 'skeleton', np.array(annot["skeleton"], dtype=np.uint8), fillvalue=0)
+                file_grp['keypoints'] = str2ascii(annot["keypoints"])
+                file_grp['skeleton'] = np.array(annot["skeleton"], dtype=np.uint8)
 
             # update progressbar
             if self.verbose:
@@ -307,15 +307,15 @@ class Keypoints2016(BaseTask):
             annot_grp = hdf5_handler.create_group('annotations')
             for i, annot in enumerate(annotations['annotations']):
                 file_grp = annot_grp.create_group(str(i))
-                hdf5_write_data(hdf5_handler, 'iscrowd', np.array(annot["iscrowd"], dtype=np.int32), fillvalue=-1)
-                hdf5_write_data(hdf5_handler, 'area', np.array(annot["area"], dtype=np.float), fillvalue=-1)
-                hdf5_write_data(hdf5_handler, 'id', np.array(annot["id"], dtype=np.int32), fillvalue=-1)
-                hdf5_write_data(hdf5_handler, 'category_id', np.array(annot["category_id"], dtype=np.int32), fillvalue=-1)
-                hdf5_write_data(hdf5_handler, 'image_id', np.array(annot["image_id"], dtype=np.int32), fillvalue=-1)
-                hdf5_write_data(hdf5_handler, 'bbox', np.array(annot["bbox"], dtype=np.float), fillvalue=-1)
-                hdf5_write_data(hdf5_handler, 'segmentation', np.array(annot["segmentation"], dtype=np.float), fillvalue=-1)
-                hdf5_write_data(hdf5_handler, 'keypoints', np.array(annot["keypoints"], dtype=np.int32), fillvalue=-1)
-                hdf5_write_data(hdf5_handler, 'num_keypoints', np.array(annot["num_keypoints"], dtype=np.uint8), fillvalue=-1)
+                file_grp['iscrowd'] = np.array(annot["iscrowd"], dtype=np.int32)
+                file_grp['area'] = np.array(annot["area"], dtype=np.float)
+                file_grp['id'] = np.array(annot["id"], dtype=np.int32)
+                file_grp['category_id'] = np.array(annot["category_id"], dtype=np.int32)
+                file_grp['image_id'] = np.array(annot["image_id"], dtype=np.int32)
+                file_grp['bbox'] = np.array(annot["bbox"], dtype=np.float)
+                file_grp['segmentation'] = np.array(annot["segmentation"], dtype=np.float)
+                file_grp['keypoints'] = np.array(annot["keypoints"], dtype=np.int32)
+                file_grp['num_keypoints'] = np.array(annot["num_keypoints"], dtype=np.uint8)
 
                 # update progressbar
                 if self.verbose:
@@ -330,23 +330,23 @@ class Keypoints2016(BaseTask):
         grouped_grp = hdf5_handler.create_group('grouped')
         for i, key in enumerate(data_):
             file_grp = grouped_grp.create_group(str(i))
-            hdf5_write_data(hdf5_handler, 'image_filename', str2ascii(data_[key]["file_name"]), dtype=np.uint8, fillvalue=0)
-            hdf5_write_data(hdf5_handler, 'width', np.array(data_[key]["width"], dtype=np.int32), fillvalue=-1)
-            hdf5_write_data(hdf5_handler, 'height', np.array(data_[key]["height"], dtype=np.int32), fillvalue=-1)
+            file_grp['image_filename'] = str2ascii(data_[key]["file_name"])
+            file_grp['width'] = np.array(data_[key]["width"], dtype=np.int32)
+            file_grp['height'] = np.array(data_[key]["height"], dtype=np.int32)
 
             if 'object' in data_[key]:
                 file_grp['keypoint_names'] = keypoints_
                 file_grp['skeleton'] = skeleton_
                 for j, obj in enumerate(data_[key]["object"]):
                     obj_grp = file_grp.create_group(str(j))
-                    hdf5_write_data(hdf5_handler, 'category', category_, dtype=np.uint8, fillvalue=0)
-                    hdf5_write_data(hdf5_handler, 'supercategory', supercategory_, dtype=np.uint8, fillvalue=0)
-                    hdf5_write_data(hdf5_handler, 'area', np.array(obj["area"], dtype=np.int32), fillvalue=-1)
-                    hdf5_write_data(hdf5_handler, 'iscrowd', np.array(obj["iscrowd"], dtype=np.int32), fillvalue=-1)
-                    hdf5_write_data(hdf5_handler, 'bbox', np.array(obj["bbox"], dtype=np.float), fillvalue=-1)
-                    hdf5_write_data(hdf5_handler, 'segmentation', np.array(obj["segmentation"], dtype=np.float), fillvalue=-1)
-                    hdf5_write_data(hdf5_handler, 'num_keypoints', np.array(obj["num_keypoints"], dtype=np.uint8), fillvalue=-1)
-                    hdf5_write_data(hdf5_handler, 'keypoints', np.array(obj["keypoints"], dtype=np.int32), fillvalue=-1)
+                    obj_grp['category'] = category_
+                    obj_grp['supercategory'] = supercategory_
+                    obj_grp['area'] = np.array(obj["area"], dtype=np.int32)
+                    obj_grp['iscrowd'] = np.array(obj["iscrowd"], dtype=np.int32)
+                    obj_grp['bbox'] = np.array(obj["bbox"], dtype=np.float)
+                    obj_grp['segmentation'] = np.array(obj["segmentation"], dtype=np.float)
+                    obj_grp['num_keypoints'] = np.array(obj["num_keypoints"], dtype=np.uint8)
+                    obj_grp['keypoints'] = np.array(obj["keypoints"], dtype=np.int32)
 
             # update progressbar
             if self.verbose:
@@ -361,7 +361,7 @@ class Keypoints2016(BaseTask):
         """
         Add data of a set to the default group.
         """
-        image_dir = self.image_dir_path[set_name]
+        image_dir = os.path.join(self.data_path, self.image_dir_path[set_name])
         if 'test' in  set_name:
             is_test = True
             data_ = data[0]
