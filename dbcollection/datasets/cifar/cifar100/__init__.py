0363c03b94e6d44a5ec46cc8b5ecd7c822362ab3
farrajota <mglfarrajota@outlook.com>
2017-10-08 21:04:09 +0100

    Fix urls in cifar100

--- dbcollection/datasets/cifar/cifar100/__init__.py
+++ dbcollection/datasets/cifar/cifar100/__init__.py
@@ -8,7 +8,7 @@ from .classification import Classification
 
 
 urls = ({'url': 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz',
-         'md5hash': 'eb9058c3a382ffc7106e4002c42a8d85'})
+         'md5hash': 'eb9058c3a382ffc7106e4002c42a8d85'},)
 keywords = ('image_processing', 'classification')
 tasks = {"classification": Classification}
 default_task = 'classification'
