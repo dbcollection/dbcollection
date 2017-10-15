74e678ab04ad5b242c32a7ce7d4a7c80ecc99cf3
farrajota <mglfarrajota@outlook.com>
2017-10-15 11:23:02 +0100

    Update dbcollection docstring

--- dbcollection/__init__.py
+++ dbcollection/__init__.py
@@ -1,17 +1,36 @@
 """
 Dataset collection package.
 
-This package allows to easily manage and load datasets in an easy
+dbcollection is a library for downloading/parsing/managing datasets via simple methods.
+It was built from the ground up to be cross-platform (Windows, Linux, MacOS) and
+cross-language (Python, Lua, Matlab, etc.). This is achieved by using the popular HDF5
+file format to store (meta)data of manually parsed datasets and Python for scripting.
+By doing so, this library can target any platform that supports Python and any language
+that has bindings for HDF5.
+
+This package allows to easily manage and load datasets in an easy and simple
 way by using hdf5 files as metadata storage. By storing all the necessary metadata
-on disk, memory RAM can be allocated to other functionalities without noticable
-performance lost, and allows for huge datasets to be used in systems with limited
-memory capacity.
+to disk, it allows for huge datasets to be used in systems with reduced
+memory usage. Also, once a dataset is setup, it is setup forever! Users can reuse it
+as many times as they want/need for a myriad of tasks without having to setup a
+dataset each time they hack some code. This lets users focus on more important tasks
+fast prototyping without having to spend time managing datasets or creating/modyfing
+scripts to load/fetch data from disk.
 
-This package enables the user to set and configure a dataset once and reuse it as
-many times it for multiple tasks without manually having te need to setup a
-dataset every time.
 
-<TODO: finish the header file explanation>
+Main features
+
+- Simple API to load/download/setup/manage datasets
+- Simple API to fetch data of a dataset
+- All data is stored in disk, resulting in reduced RAM usage (useful for large datasets)
+- Datasets only need to be setup once
+- Cross-platform (Windows, Linux, MacOs).
+- Easily extensible to other languages that have support for HDF5 files
+- Concurrent/parallel data access is possible thanks to the HDF5 file format
+- Diverse list of popular datasets are available for use
+- All datasets were manually parsed by someone, meaning that some of the quirks were
+  already solved for you
+
 """
 
 
