a8f4c8f223a06a8e3a5d809ea8d3e4145597e076
farrajota <mglfarrajota@outlook.com>
2017-10-17 22:38:28 +0100

    Fix linting for caltech pedestrian

--- dbcollection/datasets/caltech/caltech_pedestrian/detection.py
+++ dbcollection/datasets/caltech/caltech_pedestrian/detection.py
@@ -22,16 +22,12 @@ class Detection(BaseTask):
 
     # metadata filename
     filename_h5 = 'detection'
-
     skip_step = 30
-
     classes = ['person', 'person-fa', 'people', 'person?']
-
     sets = {
-        "train" : ['set00', 'set01', 'set02', 'set03', 'set04', 'set05'],
-        "test" : ['set06', 'set07', 'set08', 'set09', 'set10']
+        "train": ['set00', 'set01', 'set02', 'set03', 'set04', 'set05'],
+        "test": ['set06', 'set07', 'set08', 'set09', 'set10']
     }
-
 
     def convert_extract_data(self):
         """
@@ -41,7 +37,6 @@ class Detection(BaseTask):
             sets = [val for l in self.sets.values() for val in l]
             sets.sort()
             extract_data(self.data_path, self.extracted_data_path, sets)
-
 
     def load_data(self):
         """
@@ -53,7 +48,7 @@ class Detection(BaseTask):
         self.convert_extract_data()
 
         for set_name in self.sets:
-            data = {set_name : {}}
+            data = {set_name: {}}
 
             if self.verbose:
                 print('\n> Loading data files for the set: {}'.format(set_name))
@@ -61,7 +56,6 @@ class Detection(BaseTask):
             # progressbar
             if self.verbose:
                 prgbar = progressbar.ProgressBar(max_value=len(self.sets[set_name]))
-
 
             for i, set_data in enumerate(self.sets[set_name]):
                 data[set_name][set_data] = {}
@@ -75,19 +69,26 @@ class Detection(BaseTask):
                 for video in folders:
                     # fetch all images filenames
                     img_fnames = os.listdir(os.path.join(extracted_data_dir, video, 'images'))
-                    img_fnames = [os.path.join(self.data_path, 'extracted_data', set_data, video, 'images', fname) for fname in img_fnames]
+                    img_fnames = [os.path.join(self.data_path, 'extracted_data', set_data,
+                                               video, 'images', fname)
+                                  for fname in img_fnames]
                     img_fnames.sort()
-                    img_fnames_list = [img_fnames[i] for i in range(self.skip_step-1, len(img_fnames), self.skip_step)]
+                    range_imgs = range(self.skip_step - 1, len(img_fnames), self.skip_step)
+                    img_fnames_list = [img_fnames[i] for i in range_imgs]
 
                     # fetch all annotations filenames
-                    annotation_fnames = os.listdir(os.path.join( extracted_data_dir, video, 'annotations'))
-                    annotation_fnames = [os.path.join(self.data_path, 'extracted_data', set_data, video, 'annotations', fname) for fname in annotation_fnames]
+                    annotation_fnames = os.listdir(os.path.join(extracted_data_dir, video,
+                                                                'annotations'))
+                    annotation_fnames = [os.path.join(self.data_path, 'extracted_data', set_data,
+                                                      video, 'annotations', fname)
+                                         for fname in annotation_fnames]
                     annotation_fnames.sort()
-                    annotation_fnames_list = [annotation_fnames[i] for i in range(self.skip_step-1, len(annotation_fnames), self.skip_step)]
+                    range_annot = range(self.skip_step - 1, len(annotation_fnames), self.skip_step)
+                    annotation_fnames_list = [annotation_fnames[i] for i in range_annot]
 
                     data[set_name][set_data][video] = {
-                        "images" : img_fnames_list,
-                        "annotations" : annotation_fnames_list
+                        "images": img_fnames_list,
+                        "annotations": annotation_fnames_list
                     }
 
                 # update progressbar
@@ -99,7 +100,6 @@ class Detection(BaseTask):
                 prgbar.finish()
 
             yield data
-
 
     def add_data_to_source(self, hdf5_handler, data, set_name):
         """
@@ -117,9 +117,10 @@ class Detection(BaseTask):
             for video in sorted(data[set_data]):
                 video_grp = set_grp.create_group(video)
                 for j in range(len(data[set_data][video]['images'])):
+                    set_data_video = data[set_data][video]
                     file_grp = video_grp.create_group(str(j))
-                    file_grp['image_filenames'] = str2ascii(data[set_data][video]['images'][j])
-                    file_grp['annotation_filenames'] = str2ascii(data[set_data][video]['annotations'][j])
+                    file_grp['image_filenames'] = str2ascii(set_data_video['images'][j])
+                    file_grp['annotation_filenames'] = str2ascii(set_data_video['annotations'][j])
 
             # update progressbar
             if self.verbose:
@@ -128,7 +129,6 @@ class Detection(BaseTask):
         # update progressbar
         if self.verbose:
             prgbar.finish()
-
 
     def add_data_to_default(self, hdf5_handler, data, set_name):
         """
@@ -147,8 +147,8 @@ class Detection(BaseTask):
         list_boxesv_per_image = []
         list_object_ids_per_image = []
         list_objects_ids_per_class = []
-        #list_objects_ids_per_id = []
-        #list_objects_ids_per_occlusion= []
+        # list_objects_ids_per_id = []
+        # list_objects_ids_per_occlusion= []
 
         if self.verbose:
             print('> Adding data to default file...')
@@ -167,25 +167,25 @@ class Detection(BaseTask):
                     image_filenames.append(img_fnames[j])
 
                     # load annotation file
-                    #annotation = load_json(os.path.join(self.data_path, annot_fnames[j]))
                     annotation = load_json(annot_fnames[j])
 
                     obj_per_img = []
                     if any(annotation):
                         for obj in annotation:
                             # convert [x,y,w,h] to [xmin,ymin,xmax,ymax]
-                            if obj['pos'][2] >= 5 and obj['pos'][3] >= 5: # discard any bbox smaller than 5px wide/high
+                            # discard any bbox smaller than 5px wide/high
+                            if obj['pos'][2] >= 5 and obj['pos'][3] >= 5:
                                 bb_correct_format = [obj['pos'][0],
                                                      obj['pos'][1],
-                                                     obj['pos'][0] + obj['pos'][2]-1,
-                                                     obj['pos'][1] + obj['pos'][3]-1]
+                                                     obj['pos'][0] + obj['pos'][2] - 1,
+                                                     obj['pos'][1] + obj['pos'][3] - 1]
                                 bbox.append(bb_correct_format)
                                 if isinstance(obj['posv'], list):
                                     # convert [x,y,w,h] to [xmin,ymin,xmax,ymax]
                                     bbv_correct_format = [obj['posv'][0],
                                                           obj['posv'][1],
-                                                          obj['posv'][0] + obj['posv'][2]-1,
-                                                          obj['posv'][1] + obj['posv'][3]-1]
+                                                          obj['posv'][0] + obj['posv'][2] - 1,
+                                                          obj['posv'][1] + obj['posv'][3] - 1]
                                     bboxv.append(bbv_correct_format)
                                 else:
                                     bboxv.append([0, 0, 0, 0])
@@ -227,29 +227,38 @@ class Detection(BaseTask):
         # Process lists
         for i in range(len(self.classes)):
             imgs_per_class = [val[0] for j, val in enumerate(object_id) if val[1] == i]
-            imgs_per_class = list(set(imgs_per_class)) # get unique values
+            imgs_per_class = list(set(imgs_per_class))  # get unique values
             imgs_per_class.sort()
             list_image_filenames_per_class.append(imgs_per_class)
 
         for i in range(len(self.classes)):
             objs_per_class = [j for j, val in enumerate(object_id) if val[1] == i]
-            objs_per_class = list(set(objs_per_class)) # get unique values
+            objs_per_class = list(set(objs_per_class))  # get unique values
             objs_per_class.sort()
             list_objects_ids_per_class.append(objs_per_class)
 
         # add data to hdf5 file
-        hdf5_write_data(hdf5_handler, 'image_filenames', str2ascii(image_filenames), dtype=np.uint8, fillvalue=0)
-        hdf5_write_data(hdf5_handler, 'classes', str2ascii(self.classes), dtype=np.uint8, fillvalue=0)
-        hdf5_write_data(hdf5_handler, 'boxes', np.array(bbox, dtype=np.float), fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'boxesv', np.array(bboxv, dtype=np.float), fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'id', np.array(lbl_id, dtype=np.int32), fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'occlusion', np.array(occlusion, dtype=np.float), fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'object_ids', np.array(object_id, dtype=np.int32), fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'object_fields', str2ascii(object_fields), dtype=np.uint8, fillvalue=0)
+        hdf5_write_data(hdf5_handler, 'image_filenames', str2ascii(image_filenames),
+                        dtype=np.uint8, fillvalue=0)
+        hdf5_write_data(hdf5_handler, 'classes', str2ascii(self.classes),
+                        dtype=np.uint8, fillvalue=0)
+        hdf5_write_data(hdf5_handler, 'boxes', np.array(bbox, dtype=np.float),
+                        fillvalue=-1)
+        hdf5_write_data(hdf5_handler, 'boxesv', np.array(bboxv, dtype=np.float),
+                        fillvalue=-1)
+        hdf5_write_data(hdf5_handler, 'id', np.array(lbl_id, dtype=np.int32),
+                        fillvalue=-1)
+        hdf5_write_data(hdf5_handler, 'occlusion', np.array(occlusion, dtype=np.float),
+                        fillvalue=-1)
+        hdf5_write_data(hdf5_handler, 'object_ids', np.array(object_id, dtype=np.int32),
+                        fillvalue=-1)
+        hdf5_write_data(hdf5_handler, 'object_fields', str2ascii(object_fields),
+                        dtype=np.uint8, fillvalue=0)
 
         pad_value = -1
         hdf5_write_data(hdf5_handler, 'list_image_filenames_per_class',
-                        np.array(pad_list(list_image_filenames_per_class, pad_value), dtype=np.int32),
+                        np.array(pad_list(list_image_filenames_per_class, pad_value),
+                                 dtype=np.int32),
                         fillvalue=pad_value)
         hdf5_write_data(hdf5_handler, 'list_boxes_per_image',
                         np.array(pad_list(list_boxes_per_image, pad_value), dtype=np.int32),
