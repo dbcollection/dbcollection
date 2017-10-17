a2ff593eb0aa652e01963dada7eba7109d14db4d
farrajota <mglfarrajota@outlook.com>
2017-10-17 22:37:06 +0100

    Fix linting for leeds sports pose

--- dbcollection/datasets/leeds_sports_pose/leeds_sports_pose/keypoints.py
+++ dbcollection/datasets/leeds_sports_pose/leeds_sports_pose/keypoints.py
@@ -6,11 +6,9 @@ LSP Keypoints process functions.
 from __future__ import print_function, division
 import os
 import numpy as np
-import h5py
 import progressbar
 
 from dbcollection.datasets import BaseTask
-
 from dbcollection.utils.string_ascii import convert_str_to_ascii as str2ascii
 from dbcollection.utils.file_load import load_matlab
 from dbcollection.utils.hdf5 import hdf5_write_data
@@ -23,32 +21,30 @@ class Keypoints(BaseTask):
     filename_h5 = 'keypoint'
 
     keypoints_labels = [
-        'Right ankle',      #-- 1
-        'Right knee',       #-- 2
-        'Right hip',        #-- 3
-        'Left hip',         #-- 4
-        'Left knee',        #-- 5
-        'Left ankle',       #-- 6
-        'Right wrist',      #-- 7
-        'Right elbow',      #-- 8
-        'Right shoulder',   #-- 9
-        'Left shoulder',    #-- 10
-        'Left elbow',       #-- 11
-        'Left wrist',       #-- 12
-        'Neck',             #-- 13
-        'Head top'          #-- 14
+        'Right ankle',     # -- 1
+        'Right knee',      # -- 2
+        'Right hip',       # -- 3
+        'Left hip',        # -- 4
+        'Left knee',       # -- 5
+        'Left ankle',      # -- 6
+        'Right wrist',     # -- 7
+        'Right elbow',     # -- 8
+        'Right shoulder',  # -- 9
+        'Left shoulder',   # -- 10
+        'Left elbow',      # -- 11
+        'Left wrist',      # -- 12
+        'Neck',            # -- 13
+        'Head top'         # -- 14
     ]
-
 
     def fetch_annotations_images_paths(self):
         """
         Returns the paths of the annotation file and images folder.
         """
-        annot_filepath = annot_filepath = os.path.join(self.data_path, 'lsp_dataset', 'joints.mat')
+        annot_filepath = os.path.join(self.data_path, 'lsp_dataset', 'joints.mat')
         images_dir = os.path.join(self.data_path, 'lsp_dataset', 'images')
 
         return annot_filepath, images_dir
-
 
     def load_annotations(self):
         """
@@ -60,8 +56,8 @@ class Keypoints(BaseTask):
         annotations = load_matlab(annot_filepath)
 
         data = {
-            "train" : [],
-            "test" : []
+            "train": [],
+            "test": []
         }
 
         image_filenames = os.listdir(images_dir)
@@ -79,13 +75,12 @@ class Keypoints(BaseTask):
             for j in range(0, 14):
                 joints.append([annotations['joints'][0][j][i],  # x
                                annotations['joints'][1][j][i],  # y
-                               annotations['joints'][2][j][i]]) # is_visible (0 - visible,
-                                                                #             1 - hidden)
+                               annotations['joints'][2][j][i]])  # is_visible (0 - visible,
+                #             1 - hidden)
 
-            data[set_name].append({"filename" : filename, "joints" : joints})
+            data[set_name].append({"filename": filename, "joints": joints})
 
         return data
-
 
     def load_data(self):
         """
@@ -93,19 +88,15 @@ class Keypoints(BaseTask):
         """
         # load annotations
         annotations = self.load_annotations()
-
         for set_name in annotations:
             if self.verbose:
                 print('\n> Loading data files for the set: ' + set_name)
-
-            yield {set_name : annotations[set_name]}
-
+            yield {set_name: annotations[set_name]}
 
     def add_data_to_source(self, hdf5_handler, data, set_name):
         """
         Store classes + filenames as a nested tree.
         """
-
         if self.verbose:
             print('> Adding data to source group:')
             prgbar = progressbar.ProgressBar(max_value=len(data))
@@ -125,7 +116,6 @@ class Keypoints(BaseTask):
         # update progressbar
         if self.verbose:
             prgbar.finish()
-
 
     def add_data_to_default(self, hdf5_handler, data, set_name):
         """
@@ -154,11 +144,21 @@ class Keypoints(BaseTask):
         if self.verbose:
             prgbar.finish()
 
-        hdf5_write_data(hdf5_handler, 'image_filenames', str2ascii(image_filenames), dtype=np.uint8, fillvalue=0)
-        hdf5_write_data(hdf5_handler, 'keypoints', np.array(keypoints, dtype=np.float), fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'keypoint_names', str2ascii(self.keypoints_labels), dtype=np.uint8, fillvalue=0)
-        hdf5_write_data(hdf5_handler, 'object_ids', np.array(object_id, dtype=np.int32), fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'object_fields', str2ascii(object_fields), dtype=np.uint8, fillvalue=0)
+        hdf5_write_data(hdf5_handler, 'image_filenames',
+                        str2ascii(image_filenames),
+                        dtype=np.uint8, fillvalue=0)
+        hdf5_write_data(hdf5_handler, 'keypoints',
+                        np.array(keypoints, dtype=np.float),
+                        fillvalue=-1)
+        hdf5_write_data(hdf5_handler, 'keypoint_names',
+                        str2ascii(self.keypoints_labels),
+                        dtype=np.uint8, fillvalue=0)
+        hdf5_write_data(hdf5_handler, 'object_ids',
+                        np.array(object_id, dtype=np.int32),
+                        fillvalue=-1)
+        hdf5_write_data(hdf5_handler, 'object_fields',
+                        str2ascii(object_fields),
+                        dtype=np.uint8, fillvalue=0)
 
 
 class KeypointsOriginal(Keypoints):
--- dbcollection/datasets/leeds_sports_pose/leeds_sports_pose_extended/keypoints.py
+++ dbcollection/datasets/leeds_sports_pose/leeds_sports_pose_extended/keypoints.py
@@ -6,17 +6,15 @@ LSPe Keypoints process functions.
 from __future__ import print_function, division
 import os
 
-from dbcollection.datasets.leeds_sports_pose.leeds_sports_pose.keypoints import Keypoints as LspKeypoints
-
+from dbcollection.datasets.leeds_sports_pose.leeds_sports_pose.keypoints import Keypoints as LspKps
 from dbcollection.utils.file_load import load_matlab
 
 
-class Keypoints(LspKeypoints):
+class Keypoints(LspKps):
     """LSPe Keypoints preprocessing functions."""
 
     # metadata filename
     filename_h5 = 'detection'
-
 
     def load_annotations(self):
         """
@@ -30,13 +28,13 @@ class Keypoints(LspKeypoints):
         annotations_lspe = load_matlab(annot_filepath_lspe)
 
         data = {
-            "train" : [],
-            "test" : []
+            "train": [],
+            "test": []
         }
 
-        #---------------------------
-        # add lsp train + test data
-        #---------------------------
+        # ---------------------------
+        #  add lsp train + test data
+        # ---------------------------
 
         image_filenames_lsp = os.listdir(os.path.join(self.data_path, 'lsp_dataset', 'images'))
         image_filenames_lsp.sort()
@@ -47,21 +45,21 @@ class Keypoints(LspKeypoints):
             else:
                 set_name = 'train'
 
-            filename = os.path.join(self.data_path, 'lsp_dataset', 'images', image_filenames_lsp[i])
+            filename = os.path.join(self.data_path, 'lsp_dataset',
+                                    'images', image_filenames_lsp[i])
 
             joints = []
             for j in range(0, 14):
-                joints.append([annotations_lsp['joints'][0][j][i],  # x
-                               annotations_lsp['joints'][1][j][i],  # y
-                               annotations_lsp['joints'][2][j][i]]) # is_visible (0 - visible,
-                                                                    #             1 - hidden)
+                # is_visible => (0 - visible, 1 - hidden)
+                joints.append([annotations_lsp['joints'][0][j][i],   # x
+                               annotations_lsp['joints'][1][j][i],   # y
+                               annotations_lsp['joints'][2][j][i]])  # is_visible
 
-            data[set_name].append({"filename" : filename, "joints" : joints})
+            data[set_name].append({"filename": filename, "joints": joints})
 
-
-        #-----------------------
-        # add lspe train data
-        #-----------------------
+        # -----------------------
+        #  add lspe train data
+        # -----------------------
 
         image_filenames_lspe = os.listdir(os.path.join(self.data_path, 'images'))
         image_filenames_lspe.sort()
@@ -72,11 +70,11 @@ class Keypoints(LspKeypoints):
 
             joints = []
             for j in range(0, 14):
-                joints.append([annotations_lspe['joints'][j][0][i],  # x
-                               annotations_lspe['joints'][j][1][i],  # y
-                               annotations_lspe['joints'][j][2][i]]) # is_visible (0 - visible,
-                                                                     #             1 - hidden)
+                # is_visible => (0 - visible, 1 - hidden)
+                joints.append([annotations_lspe['joints'][j][0][i],   # x
+                               annotations_lspe['joints'][j][1][i],   # y
+                               annotations_lspe['joints'][j][2][i]])  # is_visible
 
-            data[set_name].append({"filename" : filename, "joints" : joints})
+            data[set_name].append({"filename": filename, "joints": joints})
 
         return data
