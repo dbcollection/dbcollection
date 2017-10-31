275725345a7fd96ccad21b561dc4065879d65bb0
farrajota <mglfarrajota@outlook.com>
2017-10-31 09:49:29 +0000

    Fix API docstring

--- dbcollection/core/api.py
+++ dbcollection/core/api.py
@@ -2,6 +2,7 @@
 API methods for managing datasets.
 
 This module contains several methods for easy management of datasets. These include methods for:
+
 - downloading datasets' data files from online sources (urls)
 - processing/parsing data files + annotations into a HDF5 file to store metadata information
 - loading dataset's metadata into a data loader object
@@ -10,7 +11,7 @@ This module contains several methods for easy management of datasets. These incl
 - querying the cache file for some dataset/keyword
 - displaying information about available datasets in cache or for download
 
-These methods consticomposetue the core API for dealing with dataset management.
+These methods compose the core API for dealing with dataset management.
 Users should be able to take advantage of most functionality by using only these
 functions to manage and query their datasets in a simple and easy way.
 """
