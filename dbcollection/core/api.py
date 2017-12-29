df535a9daffd20a8bc51d6d86806c6c579b5aad9
farrajota <mglfarrajota@outlook.com>
2017-12-29 21:58:23 +0000

    Refactor process API

--- dbcollection/core/api.py
+++ dbcollection/core/api.py
@@ -89,19 +89,6 @@ def download(name, data_dir=None, extract_data=True, verbose=True, is_test=False
         print('==> Dataset download complete.')
 
 
-def exists_task(available_datasets_list, name, task):
-    """Checks if a task exists for a dataset."""
-    if task == '':
-        task_ = available_datasets_list[name]['default_task']
-    elif task == 'default':
-        task_ = available_datasets_list[name]['default_task']
-    elif task.endswith('_s'):
-        task_ = task[:-2]
-    else:
-        task_ = task
-    return task_ in available_datasets_list[name]['tasks']
-
-
 def process(name, task='default', verbose=True, is_test=False):
     """Process a dataset's metadata and stores it to file.
 
@@ -132,40 +119,14 @@ def process(name, task='default', verbose=True, is_test=False):
 
     """
     assert name is not None, 'Must input a valid dataset name: {}'.format(name)
+    check_if_dataset_name_is_valid(name)
 
-    available_datasets_list = fetch_list_datasets()
+    process = ProcessAPI(name=name,
+                         task=task,
+                         verbose=verbose,
+                         is_test=is_test)
 
-    # check if the dataset name exists in the list of available dataset for download
-    assert name in available_datasets_list, 'Invalid dataset name: {}'.format(name)
-
-    # Load a cache manager object
-    cache_manager = CacheManager(is_test)
-
-    # get data + cache dir paths
-    dset_paths = cache_manager.get_dataset_storage_paths(name)
-    data_dir = dset_paths['data_dir']
-    cache_save_path = dset_paths['cache_dir']
-
-    # setup dataset class
-    constructor = available_datasets_list[name]['constructor']
-    db = constructor(data_path=data_dir,
-                     cache_path=cache_save_path,
-                     extract_data=False,
-                     verbose=verbose)
-
-    # check if task exists in the list
-    if not exists_task(available_datasets_list, name, task):
-        raise KeyError('The task \'{}\' does not exists for loading/processing.'.format(task))
-
-    if not os.path.exists(cache_save_path):
-        os.makedirs(cache_save_path)
-
-    # process metadata
-    task_info = db.process(task)
-
-    # update dbcollection.json file with the new data
-    keywords = available_datasets_list[name]['keywords']
-    cache_manager.update(name, data_dir, task_info, keywords)
+    process.run()
 
     if verbose:
         print('==> Processing complete.')
--- dbcollection/core/process.py
+++ dbcollection/core/process.py
@@ -4,17 +4,123 @@ Process API class.
 
 from __future__ import print_function
 import os
-import sys
-import shutil
-import pkgutil
-import json
 
-import dbcollection.datasets as datasets
 from dbcollection.core.cache import CacheManager
-from dbcollection.core.loader import DataLoader
 
 from .list_datasets import fetch_list_datasets
 
 
 class ProcessAPI(object):
-    pass
+    """Process a dataset's metadata and stores it to file.
+
+    The data is stored a a HSF5 file for each task composing the dataset's tasks.
+
+    Parameters
+    ----------
+    name : str
+        Name of the dataset.
+    task : str, optional
+        Name of the task to process.
+    verbose : bool, optional
+        Displays text information (if true).
+    is_test : bool, optional
+        Flag used for tests.
+
+    Raises
+    ------
+    KeyError
+        If a task does not exist for a dataset.
+
+    Examples
+    --------
+    Download the CIFAR10 dataset to disk.
+
+    >>> import dbcollection as dbc
+    >>> dbc.process('cifar10', task='classification', verbose=False)
+
+    """
+
+    def __init__(self, name, task='default', verbose=True, is_test=False):
+        """Initialize class."""
+        assert name, 'Must input a valid dataset name: {}'.format(name)
+
+        self.name = name
+        self.task = task
+        self.verbose = verbose
+        self.is_test = is_test
+        self.extract_data = False
+        self.cache_manager = CacheManager(self.is_test)
+        self.available_datasets_list = fetch_list_datasets()
+
+        self.check_if_task_exists()
+
+    def check_if_task_exists(self):
+        """Check if task exists in the list of available tasks for processing."""
+        if not self.exists_task():
+            raise KeyError('The task \'{}\' does not exists for loading/processing.'
+                           .format(self.task))
+
+    def exists_task(self):
+        """Checks if a task exists for a dataset."""
+        if self.task == '':
+            task_ = self.get_default_task()
+        elif self.task == 'default':
+            task_ = self.get_default_task()
+        elif self.task.endswith('_s'):
+            task_ = self.task[:-2]
+        else:
+            task_ = self.task
+        return task_ in self.available_datasets_list[self.name]['tasks']
+
+    def get_default_task(self):
+        """Returns the default task for this dataset."""
+        return self.available_datasets_list[self.name]['default_task']
+
+    def run(self):
+        """Process the metadata of the dataset into an HDF5 file.
+
+        ** Main method **
+
+        """
+        self.set_dataset_dirs()
+        self.process_dataset()
+        self.update_cache()
+
+    def set_dataset_dirs(self):
+        """Set the dataset's data + cache dirs in disk."""
+        if self.verbose:
+            print('==> Setup directories to store the data files.')
+
+        dset_paths = self.cache_manager.get_dataset_storage_paths(self.name)
+        self.save_data_dir = dset_paths['data_dir']
+        self.save_cache_dir = dset_paths['cache_dir']
+        self.create_dir(self.save_cache_dir)
+
+    def create_dir(self, path):
+        """Create a directory in the disk."""
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def process_dataset(self):
+        """Process the dataset's metadata."""
+        if self.verbose:
+            print('==> Process \'{}\' metadata to disk...'.format(self.name))
+
+        constructor = self.available_datasets_list[self.name]['constructor']
+        db = constructor(data_path=self.save_data_dir,
+                         cache_path=self.save_cache_dir,
+                         extract_data=self.extract_data,
+                         verbose=self.verbose)
+
+        self.task_info = db.process(self.task)
+
+    def update_cache(self):
+        """Update the cache manager information for this dataset."""
+        if self.verbose:
+            print('==> Updating the cache manager')
+
+        keywords = self.available_datasets_list[self.name]['keywords']
+        self.cache_manager.update(self.name,
+                                  self.save_data_dir,
+                                  self.task_info,
+                                  keywords)
