af700dbe0975f42c00dd8e85654fd53cad0f39f6
farrajota <mglfarrajota@outlook.com>
2018-01-19 22:09:15 +0000

    Update setup.py
    It now loads the README.rst content and uses it for the long description info.

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+"""
+Parts of this file were taken from the luigi project
+(https://github.com/spotify/luigi) as reference.
+"""
+
 
 import os
 from setuptools import setup, find_packages
@@ -10,15 +15,25 @@ VERSION = '0.2.0'
 
 
 def get_requirements():
-    """
-    Load contents from requirements.txt.
-    """
+    """Loads contents from requirements.txt."""
     requirements = []
     with open('requirements.txt') as f:
         data = f.read().splitlines()
     if any(data):
         requirements = [item.split(" ")[0] for item in data]
     return requirements
+
+
+LONG_DESCRIPTION = ''
+
+readme_note = """\
+.. note::
+   For the latest source, discussion, etc, please visit the
+   `GitHub repository <https://github.com/dbcollection/dbcollection>`_\n\n
+"""
+
+with open('README.rst') as fobj:
+    LONG_DESCRIPTION = readme_note + fobj.read()
 
 
 setup(
@@ -28,39 +43,7 @@ setup(
     url='https://github.com/dbcollection/dbcollection',
     download_url='https://github.com/dbcollection/dbcollection/archive/' + VERSION + '.tar.gz',
     description="A collection of popular datasets for deep learning.",
-    long_description="""
-        dbcollection is a library for downloading/parsing/managing datasets via simple methods.
-        It was built from the ground up to be cross-platform (Windows, Linux, MacOS) and
-        cross-language (Python, Lua, Matlab, etc.). This is achieved by using the popular HDF5
-        file format to store (meta)data of manually parsed datasets and Python for scripting.
-        By doing so, this library can target any platform that supports Python and any language
-        that has bindings for HDF5.
-
-        This package allows to easily manage and load datasets in an easy and simple
-        way by using HDF5 files as metadata storage. By storing all the necessary metadata
-        to disk, it allows for huge datasets to be used in systems with reduced
-        memory usage. Also, once a dataset is setup, it is setup forever! Users can reuse it
-        as many times as they want/need for a myriad of tasks without having to setup a
-        dataset each time they hack some code. This lets users focus on more important tasks
-        fast prototyping without having to spend time managing datasets or creating/modyfing
-        scripts to load/fetch data from disk.
-
-        Main features
-        -------------
-
-        Here are some of key features dbcollection provides:
-
-        - Simple API to load/download/setup/manage datasets
-        - Simple API to fetch data of a dataset
-        - All data is stored in disk, resulting in reduced RAM usage (useful for large datasets)
-        - Datasets only need to be setup once
-        - Cross-platform (Windows, Linux, MacOs).
-        - Easily extensible to other languages that have support for HDF5 files
-        - Concurrent/parallel data access is possible thanks to the HDF5 file format
-        - Diverse list of popular datasets are available for use
-        - All datasets were manually parsed by someone, meaning that some of the quirks were
-          already solved for you
-        """,
+    long_description=LONG_DESCRIPTION,
     license='MIT License',
     classifiers=[
         'Development Status :: 3 - Alpha',
