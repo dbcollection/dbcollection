8a361c913f5c6369730c794b8cd3c20a7a552515
M. Farrajota <farrajota@users.noreply.github.com>
2018-01-20 11:15:52 +0000

    Refactor file_load.py
    Remove try calls and raises the built-in errors instead. Also, input argument checks were added.

--- dbcollection/utils/file_load.py
+++ dbcollection/utils/file_load.py
@@ -14,7 +14,7 @@ else:
 
 
 def load_txt(fname, mode='r'):
-    """Load a .txt file to memory.
+    """Loads a .txt file to memory.
 
     Parameters
     ----------
@@ -28,16 +28,15 @@ def load_txt(fname, mode='r'):
     list of strings
 
     """
+    assert fname, 'Must input a valid file name.'
     with open(fname, mode=mode, encoding="utf-8") as f:
         data = f.read()
-
     split_lines = data.split('\n')
-
     return split_lines[:-1]
 
 
 def load_matlab(fname):
-    """Load a matlab file to memory.
+    """Loads a matlab file to memory.
 
     Parameters
     ----------
@@ -49,15 +48,9 @@ def load_matlab(fname):
     dict/list
         Data structure of the input matlab file.
 
-    Raises
-    ------
-    IOError, OSError
-        If the file cannot be opened.
     """
-    try:
-        return scipy.loadmat(fname)
-    except (IOError, OSError):
-        raise IOError('Error opening file: {}'.format(fname))
+    assert fname, 'Must input a valid file name.'
+    return scipy.loadmat(fname)
 
 
 def load_json(fname):
@@ -73,15 +66,9 @@ def load_json(fname):
     dict/list
         Data structure of the input json file.
 
-    Raises
-    ------
-    IOError
-        If the file cannot be opened.
     """
-    try:
-        return json.load(open(fname, mode='r'))
-    except (IOError, OSError):
-        raise IOError('Error opening file: {}'.format(fname))
+    assert fname, 'Must input a valid file name.'
+    return json.load(open(fname, mode='r'))
 
 
 def load_pickle(fname):
@@ -97,22 +84,16 @@ def load_pickle(fname):
     dict/list
         Data structure of the input file.
 
-    Raises
-    ------
-    IOError, OSError
-        If the file cannot be opened.
     """
-    try:
-        if sys.version_info[0] == 2:
-            return pickle.load(open(fname, mode='rb'))
-        else:
-            return pickle.load(open(fname, mode='rb'), encoding='latin1')
-    except (IOError, OSError):
-        raise IOError('Error opening file: {}'.format(fname))
+    assert fname, 'Must input a valid file name.'
+    if sys.version_info[0] == 2:
+        return pickle.load(open(fname, mode='rb'))
+    else:
+        return pickle.load(open(fname, mode='rb'), encoding='latin1')
 
-
+    
 def load_xml(fname):
-    """Load+parse a xml file to memory.
+    """Loads and parses a xml file to a dictionary.
 
     Parameters
     ----------
@@ -124,4 +105,5 @@ def load_xml(fname):
     dict
         Dictionary of the input file's data structure.
     """
+    assert fname, 'Must input a valid file name.'
     return xmltodict.parse(open(fname, mode='r').read())
