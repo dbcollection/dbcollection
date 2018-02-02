36fd24b186775621a0aab9d7e4d99191820ad02f
farrajota <mglfarrajota@outlook.com>
2018-02-02 17:17:10 +0000

    Add print_text_box() to utils

--- dbcollection/utils/__init__.py
+++ dbcollection/utils/__init__.py
@@ -4,3 +4,23 @@ data padding and parsing, testing, etc.
 
 Also, all third-party submodules are located under this module.
 """
+
+
+from __future__ import print_function
+
+
+def print_text_box(text):
+    """Prints a simple text box.
+
+    Example:
+        >>> print_text_box('Some text to display!')
+        >>>
+        '-----------------------------'
+        '--  Some text to display!  --'
+        '-----------------------------'
+
+    """
+    str_display = '--  {display_text}  --'.format(display_text=text)
+    str_separator = '-' * len(str_display)
+    print('\n{separator}\n{text}\n{separator}\n'
+          .format(separator=str_separator, text=str_display))
