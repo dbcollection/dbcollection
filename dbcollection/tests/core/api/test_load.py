8ce5accbcd3ea9985d92ae5f02cb03e31aa3891c
farrajota <mglfarrajota@outlook.com>
2018-02-19 22:45:22 +0000

    Refactor tests setup

--- dbcollection/tests/core/api/test_load.py
+++ dbcollection/tests/core/api/test_load.py
@@ -12,7 +12,8 @@ from dbcollection.core.api.load import load, LoadAPI
 def mocks_init_class(mocker):
     mock_parse = mocker.patch.object(LoadAPI, "parse_task_name", return_value=True)
     mock_cache = mocker.patch.object(LoadAPI, "get_cache_manager")
-    return [mock_parse, mock_cache]
+    mock_run = mocker.patch.object(LoadAPI, "run", return_value=True)
+    return [mock_parse, mock_cache, mock_run]
 
 
 def assert_mock_init_class(mocks):
@@ -20,46 +21,43 @@ def assert_mock_init_class(mocks):
         assert mock.called
 
 
+def generate_inputs_for_load():
+    return {
+        "dataset": 'mnist',
+        "task": 'some_task',
+        "data_dir": '/some/path/',
+        "verbose": True,
+    }
+
+
 class TestCallLoad:
     """Unit tests for the core api load method."""
 
     def test_call_with_dataset_name_only(self, mocker, mocks_init_class):
-        mock_run = mocker.patch.object(LoadAPI, "run", return_value=True)
-        dataset = 'mnist'
+        inputs = generate_inputs_for_load()
 
-        data_loader = load(dataset)
+        data_loader = load(inputs["dataset"])
 
         assert_mock_init_class(mocks_init_class)
-        assert mock_run.called
         assert data_loader == True
 
     def test_call_with_all_inputs(self, mocker, mocks_init_class):
-        mock_run = mocker.patch.object(LoadAPI, "run", return_value=True)
-        dataset = 'mnist'
-        task = 'some_task'
-        data_dir = '/some/path/'
-        verbose = True
+        inputs = generate_inputs_for_load()
 
-        data_loader = load(dataset, task, data_dir, verbose)
+        data_loader = load(inputs["dataset"], inputs["task"], inputs["data_dir"], inputs["verbose"])
 
         assert_mock_init_class(mocks_init_class)
-        assert mock_run.called
         assert data_loader == True
 
     def test_call_with_all_inputs_named_args(self, mocker, mocks_init_class):
-        mock_run = mocker.patch.object(LoadAPI, "run", return_value=True)
-        dataset = 'mnist'
-        task = 'some_task'
-        data_dir = '/some/path/'
-        verbose = True
+        inputs = generate_inputs_for_load()
 
-        data_loader = load(name=dataset,
-                           task=task,
-                           data_dir=data_dir,
-                           verbose=verbose)
+        data_loader = load(name=inputs["dataset"],
+                           task=inputs["task"],
+                           data_dir=inputs["data_dir"],
+                           verbose=inputs["verbose"])
 
         assert_mock_init_class(mocks_init_class)
-        assert mock_run.called
         assert data_loader == True
 
     def test_call__raises_error_no_inputs(self, mocker):
