d6428103157a2ff78c07003166ba0a752aef72bd
farrajota <mglfarrajota@outlook.com>
2018-02-21 22:33:17 +0000

    Add tests for the run() method

--- dbcollection/tests/core/api/test_load.py
+++ dbcollection/tests/core/api/test_load.py
@@ -9,14 +9,14 @@ from dbcollection.core.api.load import load, LoadAPI
 
 
 @pytest.fixture()
-def mocks_init_class(mocker):
+def mocks_api(mocker):
     mock_parse = mocker.patch.object(LoadAPI, "parse_task_name", return_value=True)
     mock_cache = mocker.patch.object(LoadAPI, "get_cache_manager")
     mock_run = mocker.patch.object(LoadAPI, "run", return_value=True)
     return [mock_parse, mock_cache, mock_run]
 
 
-def assert_mock_init_class(mocks):
+def assert_mock_api(mocks):
     for mock in mocks:
         assert mock.called
 
@@ -33,23 +33,23 @@ def generate_inputs_for_load():
 class TestCallLoad:
     """Unit tests for the core api load method."""
 
-    def test_call_with_dataset_name_only(self, mocker, mocks_init_class):
+    def test_call_with_dataset_name_only(self, mocker, mocks_api):
         inputs = generate_inputs_for_load()
 
         data_loader = load(inputs["dataset"])
 
-        assert_mock_init_class(mocks_init_class)
+        assert_mock_api(mocks_api)
         assert data_loader == True
 
-    def test_call_with_all_inputs(self, mocker, mocks_init_class):
+    def test_call_with_all_inputs(self, mocker, mocks_api):
         inputs = generate_inputs_for_load()
 
         data_loader = load(inputs["dataset"], inputs["task"], inputs["data_dir"], inputs["verbose"])
 
-        assert_mock_init_class(mocks_init_class)
+        assert_mock_api(mocks_api)
         assert data_loader == True
 
-    def test_call_with_all_inputs_named_args(self, mocker, mocks_init_class):
+    def test_call_with_all_inputs_named_args(self, mocker, mocks_api):
         inputs = generate_inputs_for_load()
 
         data_loader = load(name=inputs["dataset"],
@@ -57,7 +57,7 @@ class TestCallLoad:
                            data_dir=inputs["data_dir"],
                            verbose=inputs["verbose"])
 
-        assert_mock_init_class(mocks_init_class)
+        assert_mock_api(mocks_api)
         assert data_loader == True
 
     def test_call__raises_error_no_inputs(self, mocker):
@@ -68,36 +68,42 @@ class TestCallLoad:
         with pytest.raises(TypeError):
             load("some_dataset", "some_task", "some_dir", True, "extra_field")
 
+@pytest.fixture
+def mocks_init_class(mocker):
+    inputs = generate_inputs_for_load()
+    mock_parse = mocker.patch.object(LoadAPI, "parse_task_name", return_value=inputs["task"])
+    mock_cache = mocker.patch.object(LoadAPI, "get_cache_manager")
+    return [mock_parse, mock_cache]
+
+
+def assert_mock_init_class(mocks):
+    for mock in mocks:
+        assert mock.called
+
 
 class TestClassLoadAPI:
     """Unit tests for the LoadAPI class."""
 
-    def test_init_with_all_inputs(self, mocker):
+    def test_init_with_all_inputs(self, mocker, mocks_init_class):
         inputs = generate_inputs_for_load()
-        mock_parse = mocker.patch.object(LoadAPI, "parse_task_name", return_value=inputs["task"])
-        mock_cache = mocker.patch.object(LoadAPI, "get_cache_manager")
 
         load_api = LoadAPI(inputs["dataset"], inputs["task"], inputs["data_dir"], inputs["verbose"])
 
-        assert mock_parse.called
-        assert mock_cache.called
+        assert_mock_init_class(mocks_init_class)
         assert load_api.name == inputs["dataset"]
         assert load_api.task == inputs["task"]
         assert load_api.data_dir == inputs["data_dir"]
         assert load_api.verbose == inputs["verbose"]
 
-    def test_init_with_all_inputs_named_args(self, mocker):
+    def test_init_with_all_inputs_named_args(self, mocker, mocks_init_class):
         inputs = generate_inputs_for_load()
-        mock_parse = mocker.patch.object(LoadAPI, "parse_task_name", return_value=inputs["task"])
-        mock_cache = mocker.patch.object(LoadAPI, "get_cache_manager")
 
         load_api = LoadAPI(name=inputs["dataset"],
                            task=inputs["task"],
                            data_dir=inputs["data_dir"],
                            verbose=inputs["verbose"])
 
-        assert mock_parse.called
-        assert mock_cache.called
+        assert_mock_init_class(mocks_init_class)
         assert load_api.name == inputs["dataset"]
         assert load_api.task == inputs["task"]
         assert load_api.data_dir == inputs["data_dir"]
@@ -121,3 +127,71 @@ class TestClassLoadAPI:
     def test_init__raises_error_extra_inputs(self, mocker):
         with pytest.raises(TypeError):
             LoadAPI("some_dataset", "some_task", "some_dir", True, "extra_field")
+
+    def test_run_dataset_data_files_and_task_exist_in_cache(self, mocker, mocks_init_class):
+        inputs = generate_inputs_for_load()
+        mock_data_exists = mocker.patch.object(LoadAPI, "dataset_data_exists_in_cache", return_value=True)
+        mock_task_exists = mocker.patch.object(LoadAPI, "dataset_task_metadata_exists_in_cache", return_value=True)
+        mock_data_loader = mocker.patch.object(LoadAPI, "get_data_loader", return_value=["data_loader_dummy_data"])
+
+        load_api = LoadAPI(inputs["dataset"], inputs["task"], inputs["data_dir"], inputs["verbose"])
+        data_loader = load_api.run()
+
+        assert_mock_init_class(mocks_init_class)
+        assert mock_data_exists.called
+        assert mock_task_exists.called
+        assert mock_data_loader.called
+        assert data_loader == ["data_loader_dummy_data"]
+
+    def test_run_dataset_data_files_exist_task_missing_in_cache(self, mocker, mocks_init_class):
+        inputs = generate_inputs_for_load()
+        mock_data_exists = mocker.patch.object(LoadAPI, "dataset_data_exists_in_cache", return_value=True)
+        mock_task_exists = mocker.patch.object(LoadAPI, "dataset_task_metadata_exists_in_cache", return_value=False)
+        mock_process = mocker.patch.object(LoadAPI, "process_dataset_task_metadata")
+        mock_data_loader = mocker.patch.object(LoadAPI, "get_data_loader", return_value=["data_loader_dummy_data"])
+
+        load_api = LoadAPI(inputs["dataset"], inputs["task"], inputs["data_dir"], inputs["verbose"])
+        data_loader = load_api.run()
+
+        assert_mock_init_class(mocks_init_class)
+        assert mock_data_exists.called
+        assert mock_task_exists.called
+        assert mock_process.called
+        assert mock_data_loader.called
+        assert data_loader == ["data_loader_dummy_data"]
+
+    def test_run_dataset_data_files_missing_task_exists_in_cache(self, mocker, mocks_init_class):
+        inputs = generate_inputs_for_load()
+        mock_data_exists = mocker.patch.object(LoadAPI, "dataset_data_exists_in_cache", return_value=False)
+        mock_download = mocker.patch.object(LoadAPI, "download_dataset_data")
+        mock_task_exists = mocker.patch.object(LoadAPI, "dataset_task_metadata_exists_in_cache", return_value=True)
+        mock_data_loader = mocker.patch.object(LoadAPI, "get_data_loader", return_value=["data_loader_dummy_data"])
+
+        load_api = LoadAPI(inputs["dataset"], inputs["task"], inputs["data_dir"], inputs["verbose"])
+        data_loader = load_api.run()
+
+        assert_mock_init_class(mocks_init_class)
+        assert mock_data_exists.called
+        assert mock_task_exists.called
+        assert mock_download.called
+        assert mock_data_loader.called
+        assert data_loader == ["data_loader_dummy_data"]
+
+    def test_run_dataset_files_and_task_missing_in_cache(self, mocker, mocks_init_class):
+        inputs = generate_inputs_for_load()
+        mock_data_exists = mocker.patch.object(LoadAPI, "dataset_data_exists_in_cache", return_value=False)
+        mock_download = mocker.patch.object(LoadAPI, "download_dataset_data")
+        mock_task_exists = mocker.patch.object(LoadAPI, "dataset_task_metadata_exists_in_cache", return_value=False)
+        mock_process = mocker.patch.object(LoadAPI, "process_dataset_task_metadata")
+        mock_data_loader = mocker.patch.object(LoadAPI, "get_data_loader", return_value=["data_loader_dummy_data"])
+
+        load_api = LoadAPI(inputs["dataset"], inputs["task"], inputs["data_dir"], inputs["verbose"])
+        data_loader = load_api.run()
+
+        assert_mock_init_class(mocks_init_class)
+        assert mock_data_exists.called
+        assert mock_task_exists.called
+        assert mock_download.called
+        assert mock_data_loader.called
+        assert mock_process.called
+        assert data_loader == ["data_loader_dummy_data"]
