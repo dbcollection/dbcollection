160ea5f6a7e0ad1aec7892d5e525e2c7e09bd464
farrajota <mglfarrajota@outlook.com>
2018-02-26 20:41:43 +0000

    Add initial test struct for testing the DatasetConstructor class

---
+++ dbcollection/tests/core/api/test_list_datasets.py
@@ -0,0 +1,15 @@
+"""
+Test dbcollection's metadata handling methods/classes.
+"""
+
+
+import pytest
+
+from dbcollection.core.api.list_datasets import DatasetConstructor
+
+
+class TestDatasetConstructor:
+    """Unit tests for the DatasetConstructor class."""
+
+    def test_init(self, mocker):
+        pass
