9f1af55820ba94c241ec42a1f2d99b55438cb49d
farrajota <mglfarrajota@outlook.com>
2018-02-27 22:42:11 +0000

    Refactor add() method call tests

--- dbcollection/tests/core/api/test_add.py
+++ dbcollection/tests/core/api/test_add.py
@@ -8,43 +8,61 @@ import pytest
 from dbcollection.core.api.add import add, AddAPI
 
 
+@pytest.fixture()
+def mocks_init_class(mocker):
+    mock_cache = mocker.patch.object(AddAPI, "get_cache_manager", return_value=True)
+    return [mock_cache]
+
+
+def assert_mock_call(mocks):
+    for mock in mocks:
+        assert mock.called
+
+@pytest.fixture()
+def test_data():
+    return {
+        "dataset": 'some_db',
+        "task": "taskA",
+        "data_dir": '/some/dir/data',
+        "hdf5_filename": '/some/dir/db/hdf5_file.h5',
+        "categories": ['categoryA', 'categoryB', 'categoryC'],
+        "verbose": True
+    }
+
+
 class TestCallAdd:
     """Unit tests for the core api add method."""
 
-    def test_call_add(self, mocker):
-        mock_cache = mocker.patch.object(AddAPI, "get_cache_manager", return_value=True)
+    def test_call_add(self, mocker, mocks_init_class, test_data):
         mock_run = mocker.patch.object(AddAPI, "run")
-        dataset = 'some_db'
-        task = 'taskA'
-        data_dir = '/some/dir/data'
-        hdf5_filename = '/some/dir/db/hdf5_file.h5'
-        categories = ['categoryA', 'categoryB', 'categoryC']
-        verbose = True
 
-        add(dataset, task, data_dir, hdf5_filename, categories, verbose)
+        add(test_data['dataset'], test_data['task'], test_data['data_dir'],
+            test_data['hdf5_filename'], test_data['categories'], test_data['verbose'])
 
-        assert mock_cache.called
+        assert_mock_call(mocks_init_class)
         assert mock_run.called
 
-    def test_call_add_with_named_args(self, mocker):
-        mock_cache = mocker.patch.object(AddAPI, "get_cache_manager", return_value=True)
+    def test_call_add_with_named_args(self, mocker, mocks_init_class, test_data):
         mock_run = mocker.patch.object(AddAPI, "run")
-        dataset = 'some_db'
-        task = 'taskA'
-        data_dir = '/some/dir/data'
-        hdf5_filename = '/some/dir/db/hdf5_file.h5'
-        categories = ['categoryA', 'categoryB', 'categoryC']
-        verbose = True
 
-        add(name=dataset,
-            task=task,
-            data_dir=data_dir,
-            hdf5_filename=hdf5_filename,
-            categories=categories,
-            verbose=verbose)
+        add(name=test_data['dataset'],
+            task=test_data['task'],
+            data_dir=test_data['data_dir'],
+            hdf5_filename=test_data['hdf5_filename'],
+            categories=test_data['categories'],
+            verbose=test_data['verbose'])
 
-        assert mock_cache.called
+        assert_mock_call(mocks_init_class)
         assert mock_run.called
+
+    def test_call_add_without_optional_inputs(self, mocker, mocks_init_class, test_data):
+        mock_run = mocker.patch.object(AddAPI, "run")
+
+        add(test_data['dataset'], test_data['task'], test_data['data_dir'], test_data['hdf5_filename'])
+
+        assert_mock_call(mocks_init_class)
+        assert mock_run.called
+
 
 class TestClassAddAPI:
     """Unit tests for the AddAPI class."""
