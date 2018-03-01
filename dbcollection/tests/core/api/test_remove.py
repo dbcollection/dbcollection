1d2077aa4ff7e4b09a1cf952d2d55111c43e5fb7
farrajota <mglfarrajota@outlook.com>
2018-03-01 22:46:02 +0000

    Rename tests

--- dbcollection/tests/core/api/test_remove.py
+++ dbcollection/tests/core/api/test_remove.py
@@ -32,7 +32,7 @@ def test_data():
 class TestCallRemove:
     """Unit tests for the core api remove method."""
 
-    def test_call(self, mocker, mocks_init_class, test_data):
+    def test_call_with_all_input_args(self, mocker, mocks_init_class, test_data):
         mock_run = mocker.patch.object(RemoveAPI, 'run')
 
         remove(test_data['dataset'], test_data['task'], test_data['delete_data'], test_data['verbose'])
@@ -40,7 +40,7 @@ class TestCallRemove:
         assert_mock_call(mocks_init_class)
         assert mock_run.called
 
-    def test_call_with_named_args(self, mocker, mocks_init_class, test_data):
+    def test_call_with_named_input_args(self, mocker, mocks_init_class, test_data):
         mock_run = mocker.patch.object(RemoveAPI, 'run')
 
         remove(name=test_data['dataset'],
@@ -51,7 +51,7 @@ class TestCallRemove:
         assert_mock_call(mocks_init_class)
         assert mock_run.called
 
-    def test_call_without_optional_args(self, mocker, mocks_init_class, test_data):
+    def test_call_without_optional_input_args(self, mocker, mocks_init_class, test_data):
         mock_run = mocker.patch.object(RemoveAPI, 'run')
 
         remove(test_data['dataset'])
@@ -59,7 +59,7 @@ class TestCallRemove:
         assert_mock_call(mocks_init_class)
         assert mock_run.called
 
-    def test_call__raises_error_missing_args(self, mocker):
+    def test_call__raises_error_no_input_args(self, mocker):
         with pytest.raises(TypeError):
             remove()
 
