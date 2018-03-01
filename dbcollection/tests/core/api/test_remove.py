d07a6845351b9629fe0cdcfded85a363a81d736d
farrajota <mglfarrajota@outlook.com>
2018-03-01 22:45:53 +0000

    Add tests for RemoveAPI init()

--- dbcollection/tests/core/api/test_remove.py
+++ dbcollection/tests/core/api/test_remove.py
@@ -67,5 +67,30 @@ class TestCallRemove:
         with pytest.raises(TypeError):
             remove(test_data['dataset'], test_data['task'], test_data['delete_data'], test_data['verbose'], 'extra_input')
 
+
 class TestClassRemoveAPI:
     """Unit tests for the RemoveAPI class."""
+
+    def test_init_with_all_input_agrs(self, mocker, mocks_init_class, test_data):
+        remove_api = RemoveAPI(name=test_data['dataset'],
+                               task=test_data['task'],
+                               delete_data=test_data['delete_data'],
+                               verbose=test_data['verbose'])
+
+        assert_mock_call(mocks_init_class)
+        assert remove_api.name == test_data['dataset']
+        assert remove_api.task == test_data['task']
+        assert remove_api.delete_data == test_data['delete_data']
+        assert remove_api.verbose == test_data['verbose']
+
+    def test_init__raises_error_no_input_args(self, mocker, mocks_init_class, test_data):
+        with pytest.raises(TypeError):
+            RemoveAPI()
+
+    def test_init__raises_error_too_many_input_args(self, mocker, mocks_init_class, test_data):
+        with pytest.raises(TypeError):
+            RemoveAPI(test_data['dataset'], test_data['task'], test_data['delete_data'], test_data['verbose'], 'extra_input')
+
+    def test_init__raises_error_missing_one_input_arg(self, mocker, mocks_init_class, test_data):
+        with pytest.raises(TypeError):
+            RemoveAPI(test_data['dataset'], test_data['task'], test_data['delete_data'])
