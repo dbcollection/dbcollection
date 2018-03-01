18554af57b8c2d6426653c5f9fb129fe67bfc131
M. Farrajota <farrajota@users.noreply.github.com>
2018-03-01 23:35:16 +0000

    [Fix] core api remove (#109)
    * Remove is_left input arg
    
    * Add initial struct for the tests
    
    * Fix asserts
    
    * Replace default value for the input arg task
    
    * Refactor RemoveAPI init
    
    * Add tests for remove() method call
    
    * Add tests for RemoveAPI init()
    
    * Rename tests
    
    * Refactor run() in RemoveAPI
    
    * Add tests for the run() method
    
    * Update the docstring for remove()

--- dbcollection/core/api/remove.py
+++ dbcollection/core/api/remove.py
@@ -4,21 +4,23 @@ Remove API class.
 
 
 from __future__ import print_function
+import shutil
 
 from dbcollection.core.cache import CacheManager
 
 
-def remove(name, task=None, delete_data=False, verbose=True, is_test=False):
+def remove(name, task='', delete_data=False, verbose=True):
     """Remove/delete a dataset and/or task from the cache.
 
-    Removes the datasets cache information from the dbcollection.json file.
-    The dataset's data files remain in disk if 'delete_data' is set to False,
-    otherwise it removes also the data files.
+    Removes the dataset's information registry from the dbcollection.json cache file.
+    The dataset's data files remain in disk if 'delete_data' is not enabled.
+    If it kintended to remove the data files as well, the 'delete_data' input arg
+    must be set to 'True' in order to also remove the data files.
 
-    Also, instead of deleting the entire dataset, removing a specific task
-    from disk is also possible by specifying which task to delete. This removes
-    the task entry for the dataset and removes the corresponding hdf5 file from
-    disk.
+    Moreover, if the intended action is to remove a task of the dataset from the cache
+    registry, this can be achieved by specifying the task name to be deleted. This
+    effectively removes only that task entry for the dataset. Note that deleting a task
+    results in removing the associated HDF5 metadata file from disk.
 
     Parameters
     ----------
@@ -30,8 +32,6 @@ def remove(name, task=None, delete_data=False, verbose=True, is_test=False):
         Delete all data files from disk for this dataset if True.
     verbose : bool, optional
         Displays text information (if true).
-    is_test : bool, optional
-        Flag used for tests.
 
     Examples
     --------
@@ -49,18 +49,14 @@ def remove(name, task=None, delete_data=False, verbose=True, is_test=False):
     {}
 
     """
-    assert name is not None, 'Must input a valid dataset name: {}'.format(name)
+    assert name, 'Must input a valid dataset name.'
 
     db_remover = RemoveAPI(name=name,
                            task=task,
                            delete_data=delete_data,
-                           verbose=verbose,
-                           is_test=is_test)
+                           verbose=verbose)
 
     db_remover.run()
-
-    if verbose:
-        print('==> Dataset registry removed.')
 
 
 class RemoveAPI(object):
@@ -79,8 +75,6 @@ class RemoveAPI(object):
         Name of the task to delete.
     delete_data : bool
         Delete all data files from disk for this dataset if True.
-    is_test : bool
-        Flag used for tests.
 
     Attributes
     ----------
@@ -90,62 +84,80 @@ class RemoveAPI(object):
         Name of the task to delete.
     delete_data : bool
         Delete all data files from disk for this dataset if True.
-    is_test : bool
-        Flag used for tests.
     cache_manager : CacheManager
         Cache manager object.
 
     """
 
-    def __init__(self, name, task, delete_data, verbose, is_test):
+    def __init__(self, name, task, delete_data, verbose):
         """Initialize class."""
-        assert name, 'Must input a valid dataset name: {}'.format(name)
-        assert delete_data is not None, 'delete_data cannot be empty'
-        assert verbose is not None, 'verbose cannot be empty'
-        assert is_test is not None, 'is_test cannot be empty'
+        assert name, 'Must input a valid dataset name.'
+        assert isinstance(task, str), 'Must input a valid task name.'
+        assert isinstance(delete_data, bool), "Must input a valid boolean for delete_data."
+        assert isinstance(verbose, bool), "Must input a valid boolean for verbose."
 
         self.name = name
         self.task = task
         self.delete_data = delete_data
         self.verbose = verbose
-        self.is_test = is_test
-        self.cache_manager = CacheManager(self.is_test)
+        self.cache_manager = self.get_cache_manager()
+
+    def get_cache_manager(self):
+        return CacheManager()
 
     def run(self):
         """Main method."""
+        self.remove_dataset()
+
+        if self.verbose:
+            self.print_msg_registry_removal()
+
+    def remove_dataset(self):
+        """Removes the dataset from cache (and disk if selected)."""
         if self.exists_dataset():
             self.remove_registry_from_cache()
         else:
-            raise Exception('Dataset \'{}\' does not exist.'.format(self.name))
+            raise Exception('Dataset \'{}\' does not exist in the cache.'.format(self.name))
 
     def exists_dataset(self):
         """Return True if a dataset name exists in the cache."""
-        return self.cache_manager.exists_dataset(self.name)
+        return self.cache_manager.dataset.exists(self.name)
 
     def remove_registry_from_cache(self):
         """Remove the dataset or task from cache."""
-        if self.task is None:
-            self.remove_dataset_registry()
-        else:
+        if any(self.task):
             self.remove_task_registry()
+        else:
+            self.remove_dataset_registry()
 
     def remove_dataset_registry(self):
-        """Remove the dataset registry from cache."""
+        """Removes the dataset registry from cache."""
         if self.delete_data:
-            self.cache_manager.delete_dataset(self.name, True)
-        else:
-            self.cache_manager.delete_dataset(self.name, False)
-        if self.verbose:
-            print('Removed \'{}\' dataset: cache=True, disk={}'
-                  .format(self.name, self.delete_data))
+            self.remove_dataset_data_files_from_disk()
+        self.remove_dataset_entry_from_cache()
+
+    def remove_dataset_data_files_from_disk(self):
+        """Removes the directory containing the data files from disk."""
+        data_dir = self.get_dataset_data_dir()
+        shutil.rmtree(data_dir)
+
+    def get_dataset_data_dir(self):
+        dataset_metadata = self.cache_manager.dataset.get(self.name)
+        return dataset_metadata["data_dir"]
+
+    def remove_dataset_entry_from_cache(self):
+        """Removes the dataset registry from cache."""
+        self.cache_manager.dataset.delete(self.name)
 
     def remove_task_registry(self):
         """Remove the task registry for this dataset from cache."""
-        if self.cache_manager.delete_task(self.name, self.task):
-            if self.verbose:
-                print('Removed the task \'{}\' from the \'{}\' dataset: cache=True'
-                      .format(self.task, self.name))
+        self.cache_manager.task.delete(self.name, self.task)
+
+    def print_msg_registry_removal(self):
+        """Prints to screen the success message."""
+        if any(self.task):
+            print('Removed the task \'{}\' from the \'{}\' dataset: cache=True'
+                  .format(self.task, self.name))
         else:
-            if self.verbose:
-                raise Exception('The task \'{}\' does not exists for \'{}\' dataset'
-                                .format(self.task, self.name))
+            print('Removed \'{}\' dataset: cache=True, disk={}'
+                  .format(self.name, self.delete_data))
---
+++ dbcollection/tests/core/api/test_remove.py
@@ -0,0 +1,178 @@
+"""
+Test dbcollection core API: remove.
+"""
+
+
+import pytest
+
+from dbcollection.core.api.remove import remove, RemoveAPI
+
+
+@pytest.fixture()
+def mocks_init_class(mocker):
+    mock_cache = mocker.patch.object(RemoveAPI, "get_cache_manager", return_value=True)
+    return [mock_cache]
+
+
+def assert_mock_call(mocks):
+    for mock in mocks:
+        assert mock.called
+
+
+@pytest.fixture()
+def test_data():
+    return {
+        "dataset": 'some_db',
+        "task": "taskA",
+        "delete_data": False,
+        "verbose": True,
+    }
+
+
+class TestCallRemove:
+    """Unit tests for the core api remove method."""
+
+    def test_call_with_all_input_args(self, mocker, mocks_init_class, test_data):
+        mock_run = mocker.patch.object(RemoveAPI, 'run')
+
+        remove(test_data['dataset'], test_data['task'], test_data['delete_data'], test_data['verbose'])
+
+        assert_mock_call(mocks_init_class)
+        assert mock_run.called
+
+    def test_call_with_named_input_args(self, mocker, mocks_init_class, test_data):
+        mock_run = mocker.patch.object(RemoveAPI, 'run')
+
+        remove(name=test_data['dataset'],
+               task=test_data['task'],
+               delete_data=test_data['delete_data'],
+               verbose=test_data['verbose'])
+
+        assert_mock_call(mocks_init_class)
+        assert mock_run.called
+
+    def test_call_without_optional_input_args(self, mocker, mocks_init_class, test_data):
+        mock_run = mocker.patch.object(RemoveAPI, 'run')
+
+        remove(test_data['dataset'])
+
+        assert_mock_call(mocks_init_class)
+        assert mock_run.called
+
+    def test_call__raises_error_no_input_args(self, mocker):
+        with pytest.raises(TypeError):
+            remove()
+
+    def test_call__raises_error_too_many_args(self, mocker, test_data):
+        with pytest.raises(TypeError):
+            remove(test_data['dataset'], test_data['task'], test_data['delete_data'], test_data['verbose'], 'extra_input')
+
+
+@pytest.fixture()
+def remove_api_cls(mocker, mocks_init_class, test_data):
+    return RemoveAPI(
+        name=test_data['dataset'],
+        task=test_data['task'],
+        delete_data=test_data['delete_data'],
+        verbose=test_data['verbose']
+    )
+
+
+class TestClassRemoveAPI:
+    """Unit tests for the RemoveAPI class."""
+
+    def test_init_with_all_input_agrs(self, mocker, mocks_init_class, test_data):
+        remove_api = RemoveAPI(name=test_data['dataset'],
+                               task=test_data['task'],
+                               delete_data=test_data['delete_data'],
+                               verbose=test_data['verbose'])
+
+        assert_mock_call(mocks_init_class)
+        assert remove_api.name == test_data['dataset']
+        assert remove_api.task == test_data['task']
+        assert remove_api.delete_data == test_data['delete_data']
+        assert remove_api.verbose == test_data['verbose']
+
+    def test_init__raises_error_no_input_args(self, mocker, mocks_init_class, test_data):
+        with pytest.raises(TypeError):
+            RemoveAPI()
+
+    def test_init__raises_error_too_many_input_args(self, mocker, mocks_init_class, test_data):
+        with pytest.raises(TypeError):
+            RemoveAPI(test_data['dataset'], test_data['task'], test_data['delete_data'], test_data['verbose'], 'extra_input')
+
+    def test_init__raises_error_missing_one_input_arg(self, mocker, mocks_init_class, test_data):
+        with pytest.raises(TypeError):
+            RemoveAPI(test_data['dataset'], test_data['task'], test_data['delete_data'])
+
+    def test_run(self, mocker, remove_api_cls):
+        mock_remove_db = mocker.patch.object(RemoveAPI, 'remove_dataset')
+
+        remove_api_cls.run()
+
+        assert mock_remove_db.called
+
+    def test_remove_dataset_exists_dataset(self, mocker, remove_api_cls):
+        mock_exists_db = mocker.patch.object(RemoveAPI, 'exists_dataset', return_value=True)
+        mock_remove_registry = mocker.patch.object(RemoveAPI, 'remove_registry_from_cache')
+
+        remove_api_cls.remove_dataset()
+
+        assert mock_exists_db.called
+        assert mock_remove_registry.called
+
+    def test_remove_dataset_does_not_exist_dataset(self, mocker, remove_api_cls):
+        mock_exists_db = mocker.patch.object(RemoveAPI, 'exists_dataset', return_value=True)
+
+        with pytest.raises(Exception):
+            remove_api_cls.remove_dataset()
+
+        assert mock_exists_db.called
+
+    def test_remove_registry_from_cache_exists_task(self, mocker, remove_api_cls):
+        mock_remove_task = mocker.patch.object(RemoveAPI, 'remove_task_registry')
+        mock_remove_db = mocker.patch.object(RemoveAPI, 'remove_dataset_registry')
+
+        remove_api_cls.remove_registry_from_cache()
+
+        assert mock_remove_task.called
+        assert not mock_remove_db.called
+
+    def test_remove_registry_from_cache_task_is_empty(self, mocker, remove_api_cls):
+        mock_remove_task = mocker.patch.object(RemoveAPI, 'remove_task_registry')
+        mock_remove_db = mocker.patch.object(RemoveAPI, 'remove_dataset_registry')
+
+        remove_api_cls.task = ''
+        remove_api_cls.remove_registry_from_cache()
+
+        assert not mock_remove_task.called
+        assert mock_remove_db.called
+
+    def test_remove_dataset_registry(self, mocker, remove_api_cls):
+        mock_remove_files = mocker.patch.object(RemoveAPI, 'remove_dataset_data_files_from_disk')
+        mock_remove_cache = mocker.patch.object(RemoveAPI, 'remove_dataset_entry_from_cache')
+
+        remove_api_cls.delete_data = False
+        remove_api_cls.remove_dataset_registry()
+
+        assert not mock_remove_files.called
+        assert mock_remove_cache.called
+
+    def test_remove_dataset_registry_and_data_files(self, mocker, remove_api_cls):
+        mock_remove_files = mocker.patch.object(RemoveAPI, 'remove_dataset_data_files_from_disk')
+        mock_remove_cache = mocker.patch.object(RemoveAPI, 'remove_dataset_entry_from_cache')
+
+        remove_api_cls.delete_data = True
+        remove_api_cls.remove_dataset_registry()
+
+        assert mock_remove_files.called
+        assert mock_remove_cache.called
+
+    def test_remove_dataset_data_files_from_disk(self, mocker, remove_api_cls):
+        mock_get_dir = mocker.patch.object(RemoveAPI, 'get_dataset_data_dir', return_value='/some/dir/path')
+        mock_rmtree = mocker.patch('shutil.rmtree')
+
+        remove_api_cls.remove_dataset_data_files_from_disk()
+
+        assert mock_get_dir.called
+        assert mock_rmtree.called
