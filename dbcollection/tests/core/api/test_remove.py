e222f64bbe29ba08268db682207202420e1e3cdd
farrajota <mglfarrajota@outlook.com>
2018-03-01 22:06:43 +0000

    Add initial struct for the tests

---
+++ dbcollection/tests/core/api/test_remove.py
@@ -0,0 +1,16 @@
+"""
+Test dbcollection core API: remove.
+"""
+
+
+import pytest
+
+from dbcollection.core.api.remove import remove, RemoveAPI
+
+
+class TestCallRemove:
+    """Unit tests for the core api remove method."""
+
+
+class TestClassRemoveAPI:
+    """Unit tests for the RemoveAPI class."""
