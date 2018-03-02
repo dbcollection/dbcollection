d853da93edb7fe9d32b36ebd39c0e25ea3ca6fa7
farrajota <mglfarrajota@outlook.com>
2018-03-02 22:30:56 +0000

    Add initial code for the new core api method cache()

---
+++ dbcollection/core/api/cache.py
@@ -0,0 +1,22 @@
+"""
+Cache API class.
+"""
+
+
+from __future__ import print_function
+import os
+import shutil
+
+from dbcollection.core.cache import CacheManager
+
+
+def cache():
+    """
+    """
+    pass
+
+
+class CacheAPI(object):
+    """
+    """
+    pass
