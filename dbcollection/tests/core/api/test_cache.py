de7e5473613b7408ecd9c794a33ce0d65d526ad6
farrajota <mglfarrajota@outlook.com>
2018-03-02 22:31:19 +0000

    Add tests struct file

---
+++ dbcollection/tests/core/api/test_cache.py
@@ -0,0 +1,46 @@
+"""
+Test dbcollection core API: cacbe.
+"""
+
+
+import pytest
+
+from dbcollection.core.api.cache import cache, CacheAPI
+
+
+class TestCallCache:
+    """Unit tests for the core api cache method."""
+
+    def test_call_with_all_input_args(self, mocker):
+        pass
+
+    def test_call_with_named_input_args(self, mocker):
+        pass
+
+    def test_call_without_optional_input_args(self, mocker):
+        pass
+
+    def test_call__raises_error_no_input_args(self, mocker):
+        pass
+
+    def test_call__raises_error_too_many_args(self, mocker):
+        pass
+
+
+class TestClassCacheAPI:
+    """Unit tests for the CacheAPI class."""
+
+    def test_init_with_all_input_agrstest_init_with_all_input_agrs(self, mocker):
+        pass
+
+    def test_init__raises_error_no_input_args(self, mocker):
+        pass
+
+    def test_init__raises_error_too_many_input_args(self, mocker):
+        pass
+
+    def test_init__raises_error_missing_one_input_arg(self, mocker):
+        pass
+
+    def test_run(self, mocker):
+        pass
