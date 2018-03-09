2dab5a89834481bf0ce6314d7fc34cd4345a7da4
farrajota <mglfarrajota@outlook.com>
2018-03-09 22:21:44 +0000

    Add initial test structure in test_info.py

---
+++ dbcollection/tests/core/api/test_info.py
@@ -0,0 +1,43 @@
+"""
+Test dbcollection core API: info.
+"""
+
+
+import pytest
+
+from dbcollection.core.api.info import info, InfoAPI
+
+
+class TestCallInfo:
+    """Unit tests for the core api info method."""
+
+    def test_call(self, mocker):
+        pass
+
+    def test_call_with_named_args(self, mocker):
+        pass
+
+    def test_call_without_optional_args(self, mocker):
+        pass
+
+    def test_call__raises_error_too_many_input_args(self, mocker):
+        pass
+
+
+class TestClassInfoAPI:
+    """Unit tests for the InfoAPI class."""
+
+    def test_init_with_all_input_args(self, mocker):
+        pass
+
+    def test_init__raises_error_no_input_args(self, mocker):
+        pass
+
+    def test_init__raises_error_too_many_input_args(self, mocker):
+        pass
+
+    def test_init__raises_error_missing_one_input_arg(self, mocker):
+        pass
+
+    def test_run(self, mocker):
+        pass
