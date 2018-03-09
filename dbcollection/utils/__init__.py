57c8e0fc5ca0e34353a759129d06e82fe5a24b13
farrajota <mglfarrajota@outlook.com>
2018-03-09 00:56:47 +0000

    Add a dictionary nested lookup method to utils

--- dbcollection/utils/__init__.py
+++ dbcollection/utils/__init__.py
@@ -24,3 +24,23 @@ def print_text_box(text):
     str_separator = '-' * len(str_display)
     print('\n{separator}\n{text}\n{separator}\n'
           .format(separator=str_separator, text=str_display))
+
+
+def nested_lookup(key, document, wild=False):
+    """Lookup a key in a nested document recursively and yields a value."""
+    if isinstance(document, list):
+        for d in document:
+            for result in nested_lookup(key, d, wild=wild):
+                yield result
+
+    if isinstance(document, dict):
+        for k, v in document:
+            if key == k or (wild and key.lower() in k.lower()):
+                yield v
+            elif isinstance(v, dict):
+                for result in nested_lookup(key, v, wild=wild):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    for result in nested_lookup(key, d, wild=wild):
+                        yield result
