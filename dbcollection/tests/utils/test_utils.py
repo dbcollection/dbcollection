5cb6b7c2a34a5d7cfbb23a8b4baef6e97924dd41
farrajota <mglfarrajota@outlook.com>
2018-03-12 22:30:27 +0000

    Add method to merge dictionaries recursively

--- dbcollection/tests/utils/test_utils.py
+++ dbcollection/tests/utils/test_utils.py
@@ -5,7 +5,10 @@ Test dbcollection utils.
 
 import pytest
 
-from dbcollection.utils import nested_lookup
+from dbcollection.utils import (
+    nested_lookup,
+    merge_dicts
+)
 
 
 class TestNestedLookup:
@@ -36,3 +39,28 @@ class TestNestedLookup:
     def test_raises_error_missing_one_input_args(self):
         with pytest.raises(TypeError):
             nested_lookup('some_key')
+
+
+class TestMergeDicts:
+    """Unit tests for the merge_dicts method."""
+
+    def test_merge_two_dicts(self):
+        dict1 = {4: ['some', 'data', 'here'], 1:{"a":"A"},2:{"b":"B"},3:{'d': {'Y':2, 'U':(3,2,1), 4: ('stuff')}}}
+        dict2 = {2:{"c":"C"},3:{"d":"D"}}
+
+        result = merge_dicts(dict1, dict2)
+
+        assert sorted(dict(result)) == sorted({1: {'a': 'A'}, 2: {'b': 'B', 'c': 'C'}, 3: {'d': {'Y': 2, 4: 'stuff', 'X': 1, 'U': (3, 2, 1)}}, 4: ['some', 'data', 'here']})
+
+    def test_raises_error_missing_input_args(self):
+        with pytest.raises(TypeError):
+            merge_dicts()
+
+    def test_raises_error_missing_one_input_args(self):
+        with pytest.raises(TypeError):
+            merge_dicts({1:1})
+
+    def test_raises_error_missing_too_many_input_args(self):
+        with pytest.raises(TypeError):
+            merge_dicts({1:1}, {2:2}, {3:3})
+
