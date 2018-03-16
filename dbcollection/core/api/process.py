2b932b0899ce8bfe31173ea82f11812bcfe58fa7
farrajota <mglfarrajota@outlook.com>
2018-03-16 21:22:56 +0000

    Refactor process()

--- dbcollection/core/api/process.py
+++ dbcollection/core/api/process.py
@@ -32,13 +32,14 @@ def process(name, task='default', verbose=True):
 
     Examples
     --------
+    >>> import dbcollection as dbc
+
     Download the CIFAR10 dataset to disk.
 
-    >>> import dbcollection as dbc
     >>> dbc.process('cifar10', task='classification', verbose=False)
 
     """
-    assert name, 'Must input a valid dataset name: {}'.format(name)
+    assert name, 'Must input a valid dataset name.'
 
     processer = ProcessAPI(name=name,
                            task=task,
@@ -75,8 +76,6 @@ class ProcessAPI(object):
         Flag to extract data (if True).
     cache_manager : CacheManager
         Cache manager object.
-    db_metadata : MetadataConstructor
-        Dataset metadata/constructor manager.
 
     Raises
     ------
@@ -87,70 +86,24 @@ class ProcessAPI(object):
 
     def __init__(self, name, task, verbose):
         """Initialize class."""
-        assert name, 'Must input a valid dataset name: {}'.format(name)
-        assert task is not None, 'Must input a valid task name: {}'.format(task)
-        assert verbose is not None, 'verbose cannot be empty'
+        assert name, 'Must input a valid dataset name.'
+        assert isinstance(task, str), 'Must input a valid task name.'
+        assert isinstance(verbose, bool), "Must input a valid boolean for verbose."
 
         self.name = name
+        self.task = task
         self.verbose = verbose
         self.extract_data = False
         self.cache_manager = self.get_cache_manager()
-        self.db_metadata = self.get_dataset_metadata_obj(name)
-        self.task = self.parse_task_name(task)
-        self.check_if_task_exists_in_database()
-
-        self.save_data_dir = self.get_dataset_data_dir_path()
-        self.save_cache_dir = self.get_dataset_cache_dir_path()
 
     def get_cache_manager(self):
         return CacheManager()
-
-    def get_dataset_metadata_obj(self, name):
-        return MetadataConstructor(name)
-
-    def parse_task_name(self, task):
-        """Parse the input task string."""
-        if task == '':
-            task_parsed = self.get_default_task()
-        elif task == 'default':
-            task_parsed = self.get_default_task()
-        else:
-            task_parsed = task
-        return task_parsed
-
-    def get_default_task(self):
-        """Returns the default task for this dataset."""
-        return self.db_metadata.get_default_task()
-
-    def check_if_task_exists_in_database(self):
-        """Check if task exists in the list of available tasks for processing."""
-        if not self.exists_task():
-            raise KeyError('The task \'{}\' does not exists for loading/processing.'
-                           .format(self.task))
-
-    def exists_task(self):
-        """Checks if a task exists for a dataset."""
-        return self.task in self.db_metadata.get_tasks()
-
-    def get_dataset_data_dir_path(self):
-        dataset_data_cache = self.get_dataset_metadata_from_cache()
-        return dataset_data_cache['data_dir']
-
-    def get_dataset_metadata_from_cache(self):
-        return self.cache_manager.dataset.get(self.name)
-
-    def get_dataset_cache_dir_path(self):
-        cache_dir = self.get_cache_dir_path_from_cache()
-        return os.path.join(cache_dir, self.name)
-
-    def get_cache_dir_path_from_cache(self):
-        return self.cache_manager.info.cache_dir
 
     def run(self):
         """Main method."""
         if self.verbose:
             print('==> Setup directories to store the data files.')
-        self.create_dir(self.save_cache_dir)
+        self.set_dirs_processed_metadata()
 
         if self.verbose:
             print('==> Process \'{}\' metadata to disk...'.format(self.name))
@@ -163,6 +116,17 @@ class ProcessAPI(object):
         if self.verbose:
             print('==> Dataset processing complete.')
 
+    def set_dirs_processed_metadata(self):
+        cache_dir = self.get_dataset_cache_dir_path()
+        self.create_dir(cache_dir)
+
+    def get_dataset_cache_dir_path(self):
+        cache_dir = self.get_cache_dir_path_from_cache()
+        return os.path.join(cache_dir, self.name)
+
+    def get_cache_dir_path_from_cache(self):
+        return self.cache_manager.info.cache_dir
+
     def create_dir(self, path):
         """Create a directory in the disk."""
         if not os.path.exists(path):
@@ -171,18 +135,60 @@ class ProcessAPI(object):
     def process_dataset(self):
         """Process the dataset's metadata."""
         constructor = self.get_dataset_constructor()
-        db = constructor(data_path=self.save_data_dir,
-                         cache_path=self.save_cache_dir,
+        save_data_dir = self.get_dataset_data_dir_path()
+        save_cache_dir = self.get_dataset_cache_dir_path()
+        db = constructor(data_path=save_data_dir,
+                         cache_path=save_cache_dir,
                          extract_data=self.extract_data,
                          verbose=self.verbose)
-        task_info = db.process(self.task)
+        task = self.parse_task_name(self.task)
+        self.check_if_task_exists_in_database(task)
+        task_info = db.process(task)
         return task_info
 
     def get_dataset_constructor(self):
-        return self.db_metadata.get_constructor()
+        db_metadata = self.get_dataset_metadata_obj(self.name)
+        constructor = db_metadata.get_constructor()
+        return constructor
+
+    def get_dataset_metadata_obj(self, name):
+        return MetadataConstructor(name)
+
+    def get_dataset_data_dir_path(self):
+        dataset_metadata = self.get_dataset_metadata_from_cache()
+        return dataset_metadata['data_dir']
+
+    def get_dataset_metadata_from_cache(self):
+        return self.cache_manager.dataset.get(self.name)
+
+    def parse_task_name(self, task):
+        """Parse the input task string."""
+        if task == '' or task == 'default':
+            task_parsed = self.get_default_task()
+        else:
+            task_parsed = task
+        return task_parsed
+
+    def get_default_task(self):
+        """Returns the default task for this dataset."""
+        db_metadata = self.get_dataset_metadata_obj(self.name)
+        default_task = db_metadata.get_default_task()
+        return default_task
+
+    def check_if_task_exists_in_database(self, task):
+        """Check if task exists in the list of available tasks for processing."""
+        if not self.exists_task(task):
+            raise KeyError('The task \'{}\' does not exists for loading/processing.'
+                           .format(self.task))
+
+    def exists_task(self, task):
+        """Checks if a task exists for a dataset."""
+        db_metadata = self.get_dataset_metadata_obj(self.name)
+        return task in db_metadata.get_tasks()
 
     def update_cache(self, task_info):
         """Update the cache manager information for this dataset."""
+        cache_dir = self.get_dataset_cache_dir_path()
         self.cache_manager.dataset.update(name=self.name,
-                                          cache_dir=self.save_cache_dir,
+                                          cache_dir=cache_dir,
                                           tasks=task_info)
