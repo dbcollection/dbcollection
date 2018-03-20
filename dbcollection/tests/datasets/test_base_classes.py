b3d0fdc44323c77bd5186838ef9aabfed7390817
farrajota <mglfarrajota@outlook.com>
2018-03-20 21:42:00 +0000

    Add test structure for testing the BaseDataset class

---
+++ dbcollection/tests/datasets/test_base_classes.py
@@ -0,0 +1,12 @@
+"""
+Test the base classes for managing datasets and tasks.
+"""
+
+
+import pytest
+
+from dbcollection.datasets import BaseDataset
+
+
+class TestBaseDataset:
+    """Unit tests for the BaseDataset class."""
