e2e843dac9ad94cb4fbbaa1aecc140523e2212a8
farrajota <mglfarrajota@outlook.com>
2018-03-27 23:44:35 +0100

    Add add_field_to_group() to HDF5Manager

--- dbcollection/utils/hdf5.py
+++ dbcollection/utils/hdf5.py
@@ -81,5 +81,70 @@ class HDF5Manager(object):
         self.file.close()
 
     def exists_group(self, group):
+        """Checks if a group exists in the file."""
         assert group, "Must input a valid group name."
         return group in self.file
+
+    def create_group(self, name):
+        """Creates a group in the file."""
+        assert name, "Must input a name for the group."
+        self.file.create_group(name)
+
+    def add_field_to_group(self, group, field, data, dtype, fillvalue,
+                           chunks, compression, compression_opts):
+        """Writes the data of a field into an HDF5 file.
+
+        Parameters
+        ----------
+        group : str
+            Name of the group.
+        field : str
+            Name of the field (h5 dataset).
+        data : np.ndarray
+            Data array.
+        dtype : np.dtype
+            Data type.
+        chunks : bool
+            Stores the data as chunks if True.
+        compression : str
+            Compression algorithm type.
+        compression_opts : int
+            Compression option (range: [1,10])
+        fillvalue : int/float
+            Value to pad the data array.
+
+        Returns
+        -------
+        h5py._hl.dataset.Dataset
+            Object handler of the created HDF5 dataset.
+
+        """
+        assert group, "Must input a valid group name."
+        assert field, "Must input a valid field name."
+        assert isinstance(data, np.ndarray), "Must input a valid numpy data array."
+        assert dtype, "Must input a valid numpy data type."
+        assert fillvalue is not None, "Must input a valid fill value to pad the array."
+        assert chunks is not None, "Must input a valid chunk size."
+        assert compression, "Must input a valid compression algorithm"
+        assert compression_opts, "Must input a valid compression value."
+
+        h5_group = self.get_group(group)
+
+        h5_field = h5_group.create_dataset(
+            name=field,
+            data=data,
+            shape=data.shape,
+            dtype=dtype,
+            chunks=chunks,
+            compression=compression,
+            compression_opts=compression_opts,
+            fillvalue=fillvalue
+        )
+
+        return h5_field
+
+    def get_group(self, group):
+        if self.exists_group(group):
+            return self.file[group]
+        else:
+            return self.create_group(group)
