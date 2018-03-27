bd3a5428aaa3c82ee32353f5778dafb58ede691e
farrajota <mglfarrajota@outlook.com>
2018-03-27 22:23:03 +0100

    Add tests for exists_group() of HDF5Manager

--- dbcollection/tests/utils/test_hdf5.py
+++ dbcollection/tests/utils/test_hdf5.py
@@ -9,6 +9,21 @@ import pytest
 from dbcollection.utils.hdf5 import HDF5Manager
 
 
+@pytest.fixture()
+def test_data():
+    return {
+        "filename": '/path/to/filename.h5'
+    }
+
+
+@pytest.fixture()
+def mock_hdf5manager(mocker, test_data):
+    mock_open_file = mocker.patch.object(HDF5Manager, "open_file", return_value={'test_group': 'dummy_data'})
+    return HDF5Manager(
+        filename=test_data["filename"]
+    )
+
+
 class TestHDF5Manager:
     """Unit tests for the HDF5Manager class."""
 
@@ -28,4 +43,22 @@ class TestHDF5Manager:
     def test_init__raises_error_too_many_input_args(self, mocker):
         with pytest.raises(TypeError):
             HDF5Manager('/some/path/filename.h5', 'extra_field')
+
+    def test_exists_group_and_group_exists(self, mocker, mock_hdf5manager):
+        group = 'test_group'
+
+        result = mock_hdf5manager.exists_group(group)
+
+        assert result == True
+
+    def test_exists_group_and_group_does_not_exist(self, mocker, mock_hdf5manager):
+        group = 'invalid_group'
+
+        result = mock_hdf5manager.exists_group(group)
+
+        assert result == False
+
+    def test_exists_group__raises_error_no_input_args(self, mocker, mock_hdf5manager):
+        with pytest.raises(TypeError):
+            mock_hdf5manager.exists_group()
 
