3f06877c18e0cf9e5058cfda3abf7b04f61eb0f9
farrajota <mglfarrajota@outlook.com>
2018-03-27 21:55:37 +0100

    Update tests

--- dbcollection/tests/utils/test_hdf5.py
+++ dbcollection/tests/utils/test_hdf5.py
@@ -13,10 +13,12 @@ class TestHDF5Manager:
     """Unit tests for the HDF5Manager class."""
 
     def test_init_with_all_input_args(self, mocker):
+        mock_open = mocker.patch.object(HDF5Manager, "open_file", return_value={})
         filename = '/some/path/filename.h5'
 
-        hdf5_manager = HDF5Manager(filename)
+        hdf5_manager = HDF5Manager(filename=filename)
 
+        assert mock_open.called
         assert hdf5_manager.filename == filename
 
     def test_init__raises_error_no_input_args(self, mocker):
@@ -26,3 +28,4 @@ class TestHDF5Manager:
     def test_init__raises_error_too_many_input_args(self, mocker):
         with pytest.raises(TypeError):
             HDF5Manager('/some/path/filename.h5', 'extra_field')
+
