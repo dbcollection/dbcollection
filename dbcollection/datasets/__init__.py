1dd184ab3b9adc050a63da70ac49109e910efb24
M. Farrajota <farrajota@users.noreply.github.com>
2018-05-06 21:37:55 +0100

    [Refactor] modify BaseTask to simplify processing tasks of dataset (#130)
    * Refactor BaseTask class
    
    * Add initial test structure for the BaseTask
    
    * Add tests for BaseTask init
    
    * Add test for get_hdf5_save_filename()
    
    * Improve BaseTask's parameter docstrings
    
    * Add test to run()
    
    * Refactor setup_manager_hdf5() and create a temporary stub
    
    * Add a stub test for setup_manager_hdf5()
    
    * Test load_data()
    
    * Add test for process_metadata()
    
    * Add dummy test for hdf5_create_group()
    
    * Add tests for set_data_fields_to_save()
    
    * Add tests for save_raw_metadata_to_hdf5()
    
    * Add dummy test for save_data_to_disk()
    
    * Rename BaseTask to BaseTaskNew
    
    * Restore old BaseTask class
    
    * Refactor BaseTaskNew to simplify the metadata processing/saving scheme
    
    * Refactor tests
    
    * Add hdf5 manager creation to BaseTaskNew
    
    * Update method names
    
    * Update the base classes tests
    
    * Add save_fields_to_hdf5() to BaseTaskNew
    
    This method iterates over all fields contained in a dict to store their data into an HDF5 file.
    
    * Add tests to save_fields_to_hdf5() and add_field_data_to_hdf5()
    
    * Remove save raw data and refactor the metadata process method for a set
    
    * Add method to save data of a field into the hdf5 metadata file
    
    * Fix unit tests and add tests for save_field_to_hdf5()
    
    * Add test for teardown_hdf5_manager()

--- dbcollection/datasets/__init__.py
+++ dbcollection/datasets/__init__.py
@@ -11,6 +11,7 @@ from __future__ import print_function
 import os
 import h5py
 
+from dbcollection.utils.hdf5 import HDF5Manager
 from dbcollection.utils.url import download_extract_all
 
 
@@ -413,3 +414,137 @@ class BaseTask(object):
         """Run task processing."""
         filename = self.process_metadata()
         return filename
+
+
+class BaseTaskNew(object):
+    """Base class for processing the metadata of a task of a dataset.
+
+    Parameters
+    ----------
+    data_path : str
+        Path to the data directory.
+    cache_path : str
+        Path to the cache file
+    verbose : bool, optional
+        Displays text information to the screen (if true).
+
+    Attributes
+    ----------
+    data_path : str
+        Path to the data directory of the dataset.
+    cache_path : str
+        Path to store the HDF5 metadata file of a dataset in the cache directory.
+    verbose : bool
+        Displays text information to the screen (if true).
+    filename_h5 : str
+        Name of the HDF5 file.
+    hdf5_filepath : str
+        File name + path of the HDF5 metadata file in disk.
+
+    """
+
+    filename_h5 = ''  # name of the task file
+
+    def __init__(self, data_path, cache_path, verbose=True):
+        """Initialize class."""
+        assert data_path, "Must insert a valid data path"
+        assert cache_path, "Must insert a valid cache path"
+        self.cache_path = cache_path
+        self.data_path = data_path
+        self.verbose = verbose
+        self.hdf5_filepath = self.get_hdf5_save_filename()
+        self.hdf5_manager = None
+
+    def get_hdf5_save_filename(self):
+        """Builds the HDF5 file name + path on disk."""
+        return os.path.join(self.cache_path, self.filename_h5 + '.h5')
+
+    def run(self):
+        """Main Method. Runs the task metadata processing.
+
+        It creates an HDF5 file in disk to store the resulting
+        subgroups of the dataset's set partitions (e.g., train/val/test/etc.).
+        Then, it loads the dataset's raw metadata from disk into memory as a
+        generator, retrieves the data fields obtained in the processing stage
+        and saves them into an HDF5 file in disk.
+
+        Returns
+        -------
+        str
+            File name + path of the task's HDF5 metadata file.
+        """
+        self.setup_hdf5_manager()
+        data_generator = self.load_data()
+        self.process_metadata(data_generator)
+        self.teardown_hdf5_manager()
+        return self.hdf5_filepath
+
+    def setup_hdf5_manager(self):
+        """Sets up the metadata manager to store the processed data to disk."""
+        if self.verbose:
+            print('\n==> Storing metadata to file: {}'.format(self.hdf5_filepath))
+        self.hdf5_manager = HDF5Manager(filename=self.hdf5_filepath)
+
+    def load_data(self):
+        """Loads the dataset's (meta)data from disk (create a generator).
+
+        Load data from annnotations and split it to corresponding
+        sets (train, val, test, etc.)
+
+        Returns
+        -------
+        generator
+            A sequence of dictionary objects with a key-value pair
+            with the name of the set split and the data.
+
+        """
+        pass  # stub
+
+    def process_metadata(self, data_generator):
+        """Processes the dataset's (meta)data and stores it into an HDF5 file."""
+        for data in data_generator:
+            for set_name in data:
+                if self.verbose:
+                    print('\nSaving set metadata: {}'.format(set_name))
+                self.process_set_metadata(data[set_name], set_name)
+
+    def process_set_metadata(self, data, set_name):
+        """Sets up the set's data fields to be stored in the HDF5 metadata file.
+
+        All fields set in this method are organized as a single big matrix.
+        This results in much faster data retrieval than by transversing nested
+        groups + datasets in an HDF5 file.
+
+        Parameters
+        ----------
+        data : dict
+            Dictionary containing the data annotations of a set split.
+        set_name : str
+            Name of the set split.
+
+        """
+        pass
+
+    def save_field_to_hdf5(self, set_name, field, data, **kwargs):
+        """Saves data of a field into the HDF% metadata file.
+
+        Parameters
+        ----------
+        set_name: str
+            Name of the set split.
+        field : str
+            Name of the data field.
+        data : np.ndarray
+            Numpy ndarray of the field's data.
+
+        """
+        self.hdf5_manager.add_field_to_group(
+            group=set_name,
+            field=field,
+            data=data,
+            **kwargs
+        )
+
+    def teardown_hdf5_manager(self):
+        """Sets up the MetadataManager object to manage the metadata save process to disk."""
+        self.hdf5_manager.close()
--- dbcollection/tests/datasets/test_base_classes.py
+++ dbcollection/tests/datasets/test_base_classes.py
@@ -3,9 +3,11 @@ Test the base classes for managing datasets and tasks.
 """
 
 
+import os
 import pytest
+import numpy as np
 
-from dbcollection.datasets import BaseDatasetNew, BaseTask
+from dbcollection.datasets import BaseDatasetNew, BaseTaskNew
 
 
 @pytest.fixture()
@@ -53,7 +55,7 @@ class TestBaseDatasetNew:
         assert db_manager.tasks == {}
         assert db_manager.default_task == ''
 
-    def test_init_withouth_optional_input_args(self, mocker):
+    def test_init_without_optional_input_args(self, mocker):
         data_path = '/path/to/data'
         cache_path = '/path/to/cache'
 
@@ -138,3 +140,146 @@ class TestBaseDatasetNew:
         result = mock_dataset_class.get_task_constructor(task)
 
         assert result == 'some_data'
+
+
+@pytest.fixture()
+def mock_task_class(test_data):
+    return BaseTaskNew(
+        data_path=test_data["data_path"],
+        cache_path=test_data["cache_path"],
+        verbose=test_data["verbose"]
+    )
+
+
+class TestBaseTaskNew:
+    """Unit tests for the BaseTaskNew class."""
+
+    def test_init_with_all_input_args(self, mocker):
+        mock_get_filename = mocker.patch.object(BaseTaskNew, "get_hdf5_save_filename", return_value='/path/to/hdf5/file.h5')
+        data_path = '/path/to/data'
+        cache_path = '/path/to/cache'
+        verbose = True
+
+        task_manager = BaseTaskNew(data_path=data_path,
+                                cache_path=cache_path,
+                                verbose=verbose)
+
+        assert mock_get_filename.called
+        assert task_manager.data_path == '/path/to/data'
+        assert task_manager.cache_path == '/path/to/cache'
+        assert task_manager.verbose == True
+        assert task_manager.hdf5_filepath == '/path/to/hdf5/file.h5'
+        assert task_manager.filename_h5 == ''
+        assert task_manager.hdf5_manager == None
+
+    def test_init_without_optional_input_args(self, mocker):
+        mock_get_filename = mocker.patch.object(BaseTaskNew, "get_hdf5_save_filename", return_value='/path/to/hdf5/file.h5')
+        data_path = '/path/to/data'
+        cache_path = '/path/to/cache'
+
+        task_manager = BaseTaskNew(data_path=data_path,
+                                cache_path=cache_path)
+
+        assert mock_get_filename.called
+        assert task_manager.data_path == '/path/to/data'
+        assert task_manager.cache_path == '/path/to/cache'
+        assert task_manager.verbose == True
+        assert task_manager.hdf5_filepath == '/path/to/hdf5/file.h5'
+        assert task_manager.filename_h5 == ''
+        assert task_manager.hdf5_manager == None
+
+    def test_init__raises_error_no_input_args(self, mocker):
+        with pytest.raises(TypeError):
+            BaseTaskNew()
+
+    def test_init__raises_error_too_many_input_args(self, mocker):
+        with pytest.raises(TypeError):
+            BaseTaskNew('/path/to/data', '/path/to/cache', False, 'extra_input')
+
+    def test_get_hdf5_save_filename(self, mocker, mock_task_class):
+        mock_task_class.filename_h5 = 'classification'
+
+        filepath = mock_task_class.get_hdf5_save_filename()
+
+        assert filepath == os.path.join('/path/to/cache', 'classification.h5')
+
+    def test_run(self, mocker, mock_task_class):
+        mock_setup_manager = mocker.patch.object(BaseTaskNew, "setup_hdf5_manager")
+        mock_load_data = mocker.patch.object(BaseTaskNew, "load_data", return_value={})
+        mock_process = mocker.patch.object(BaseTaskNew, "process_metadata")
+        mock_teardown_manager = mocker.patch.object(BaseTaskNew, "teardown_hdf5_manager")
+
+        filename = mock_task_class.run()
+
+        assert mock_setup_manager.called
+        assert mock_load_data.called
+        assert mock_process.called
+        assert mock_teardown_manager.called
+        assert filename == mock_task_class.hdf5_filepath
+
+    def test_load_data(self, mocker, mock_task_class):
+        mock_task_class.load_data()
+
+    def test_process_metadata(self, mocker, mock_task_class):
+        mock_process_metadata = mocker.patch.object(BaseTaskNew, "process_set_metadata")
+
+        def sample_generator():
+            yield {'train': ['dummy', 'data']}
+            yield {'test': ['dummy', 'data']}
+        generator = sample_generator()
+
+        mock_task_class.process_metadata(generator)
+
+        assert mock_process_metadata.called
+
+    def test_hdf5_create_group(self, mocker, mock_task_class):
+        pass  # Todo
+
+    def test_save_field_to_hdf5(self, mocker, mock_task_class):
+        mock_add_field = mocker.Mock()
+        mock_task_class.hdf5_manager = mock_add_field
+
+        group = 'train'
+        field = 'fieldA'
+        data = np.array(range(10))
+        mock_task_class.save_field_to_hdf5(group, field, data)
+
+        mock_add_field.add_field_to_group.assert_called_once_with(
+            group='train',
+            field='fieldA',
+            data=data
+        )
+
+    def test_save_field_to_hdf5_all_args(self, mocker, mock_task_class):
+        mock_add_field = mocker.Mock()
+        mock_task_class.hdf5_manager = mock_add_field
+
+        data = np.array(range(10))
+        mock_task_class.save_field_to_hdf5(
+            set_name='train',
+            field='fieldA',
+            data=data,
+            dtype=np.uint8,
+            fillvalue=0,
+            chunks=True,
+            compression="gzip",
+            compression_opts=4)
+
+        mock_add_field.add_field_to_group.assert_called_once_with(
+            group='train',
+            field='fieldA',
+            data=data,
+            dtype=np.uint8,
+            fillvalue=0,
+            chunks=True,
+            compression="gzip",
+            compression_opts=4
+        )
+
+    def test_teardown_hdf5_manager(self, mocker, mock_task_class):
+        mock_add_field = mocker.Mock()
+        mock_task_class.hdf5_manager = mock_add_field
+
+        mock_task_class.teardown_hdf5_manager()
+
+        mock_add_field.close.assert_called_once_with()
