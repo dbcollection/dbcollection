b9fffa88110ed5ac62b8c83281840e66b96b9a2b
farrajota <mglfarrajota@outlook.com>
2018-05-08 22:29:20 +0100

    Add files for testing the cifar10 dataset

---
+++ tests/datasets/cifar/test_cifar10.py
@@ -0,0 +1,15 @@
+"""
+Test the base classes for managing datasets and tasks.
+"""
+
+
+import os
+import pytest
+import numpy as np
+
+from dbcollection.datasets.cifar.cifar10 import Classification
+
+
+class TestClassification:
+    """Unit tests for the BaseDatasetNew class."""
+    pass
