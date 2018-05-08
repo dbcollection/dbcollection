25d872636641065727cbc2bc4351b546d12ccb76
farrajota <mglfarrajota@outlook.com>
2018-05-08 21:36:17 +0100

    Replace BaseDataset with BaseDatasetNew in cifar10 __init__.py

--- dbcollection/datasets/cifar/cifar10/__init__.py
+++ dbcollection/datasets/cifar/cifar10/__init__.py
@@ -3,18 +3,18 @@ Cifar10 download/process functions.
 """
 
 
-from dbcollection.datasets import BaseDataset
+from dbcollection.datasets import BaseDatasetNew
 from .classification import Classification
 
 
-urls = ({'url': 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
-         'md5hash': 'c58f30108f718f92721af3b95e74349a'},)
+urls = ({"url": 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
+         "md5hash": 'c58f30108f718f92721af3b95e74349a'},)
 keywords = ('image_processing', 'classification')
 tasks = {"classification": Classification}
 default_task = 'classification'
 
 
-class Dataset(BaseDataset):
+class Dataset(BaseDatasetNew):
     """ Cifar10 preprocessing/downloading functions """
     urls = urls
     keywords = keywords
