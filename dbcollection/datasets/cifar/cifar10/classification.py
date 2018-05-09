ba2f80397421509861c75a12c6b6c36cb5420e15
M. Farrajota <farrajota@users.noreply.github.com>
2018-05-09 23:57:47 +0100

    [Refactor] cifar10 (#139)
    * Replace BaseDataset with BaseDatasetNew in cifar10 __init__.py
    
    * Replace BaseTask with BaseTaskNew in classification.py
    
    * Remove unnecessary import
    
    * Move load_data() to the top of the class
    
    * Refactor load_data()
    
    * Refactor process_set_metadata()
    
    * Refactor cifar10 README
    
    * Add files for testing the cifar10 dataset
    
    * Add test for load_data()
    
    * Add test for load_data_set()
    
    * Add test for load_data_annotations()
    
    * Move setting the filename from out of the method call in get_class_name()
    
    * Add test for get_object_list()
    
    * Fix padding value in get_images_per_class() to -1
    
    * Add test for get_images_per_class()
    
    * Add test for process_set_metadata()
    
    * Add test for the class attributes
    
    * Rename test class
    
    * Fix test to support python 2.7 generator syntax

--- dbcollection/datasets/cifar/cifar10/classification.py
+++ dbcollection/datasets/cifar/cifar10/classification.py
@@ -7,15 +7,13 @@ from __future__ import print_function, division
 import os
 import numpy as np
 
-from dbcollection.datasets import BaseTask
-
+from dbcollection.datasets import BaseTaskNew
 from dbcollection.utils.file_load import load_pickle
 from dbcollection.utils.string_ascii import convert_str_to_ascii as str2ascii
 from dbcollection.utils.pad import pad_list
-from dbcollection.utils.hdf5 import hdf5_write_data
 
 
-class Classification(BaseTask):
+class Classification(BaseTaskNew):
     """ Cifar10 Classification preprocessing functions """
 
     # metadata filename
@@ -32,77 +30,22 @@ class Classification(BaseTask):
         "test_batch"
     ]
 
-    def get_object_list(self, data, labels):
+    def load_data(self):
         """
-        Groups the data + labels info in a 'list' of indexes.
+        Loads data from annotation files.
         """
-        # object_id = np.ndarray((data.shape[0], 2), dtype=np.uint16)
-        object_id = np.ndarray((data.shape[0], 2), dtype=int)
-        for i in range(data.shape[0]):
-            object_id[i][0] = i
-            object_id[i][1] = labels[i]
-        return object_id
-
-    def get_class_names(self, path):
-        """
-        Returns the class names/labels.
-        """
-        return load_pickle(os.path.join(path, self.data_files[0]))
+        yield {"train": self.load_data_set(is_test=False)}
+        yield {"test": self.load_data_set(is_test=True)}
 
     def load_data_set(self, is_test):
         """
-        Fetch train/test data.
+        Fetches the train/test data.
         """
-        # merge the path with the extracted folder name
-        data_path_ = os.path.join(self.data_path, 'cifar-10-batches-py')
+        assert isinstance(is_test, bool), "Must input a valid boolean input."
+        data, labels, class_names = self.load_data_annotations(is_test)
 
-        class_names = self.get_class_names(data_path_)
-
-        # load test data file
-        if is_test:
-            batch = load_pickle(os.path.join(data_path_, self.data_files[6]))
-            data = batch['data'].reshape(10000, 3, 32, 32)
-            labels = np.array(batch['labels'], dtype=np.uint8)
-        else:
-            batch1 = load_pickle(os.path.join(data_path_, self.data_files[1]))
-            batch2 = load_pickle(os.path.join(data_path_, self.data_files[2]))
-            batch3 = load_pickle(os.path.join(data_path_, self.data_files[3]))
-            batch4 = load_pickle(os.path.join(data_path_, self.data_files[4]))
-            batch5 = load_pickle(os.path.join(data_path_, self.data_files[5]))
-
-            # concatenate data
-            data = np.concatenate(
-                (
-                    batch1['data'],
-                    batch2['data'],
-                    batch3['data'],
-                    batch4['data'],
-                    batch5['data']
-                ),
-                axis=0,
-            )
-            data = data.reshape((50000, 3, 32, 32))
-
-            labels = np.concatenate(
-                (
-                    batch1['labels'],
-                    batch2['labels'],
-                    batch3['labels'],
-                    batch4['labels'],
-                    batch5['labels']
-                ),
-                axis=0,
-            )
-
-        data = np.transpose(data, (0, 2, 3, 1))  # NxHxWxC
         object_list = self.get_object_list(data, labels)
-
-        # organize list of image indexes per class
-        images_per_class = []
-        unique_labels = np.unique(labels)
-        for label in unique_labels:
-            images_idx = np.where(labels == label)[0].tolist()
-            images_per_class.append(images_idx)
+        images_per_class = self.get_images_per_class(labels)
 
         return {
             "object_fields": str2ascii(['images', 'classes']),
@@ -110,41 +53,82 @@ class Classification(BaseTask):
             "data": data,
             "labels": labels,
             "object_ids": object_list,
-            "list_images_per_class": np.array(pad_list(images_per_class, 1), dtype=np.int32)
+            "list_images_per_class": images_per_class
         }
 
-    def load_data(self):
-        """
-        Load the data from the files.
-        """
-        # train set
-        yield {"train": self.load_data_set(False)}
+    def load_data_annotations(self, is_test):
+        """Loads the data from the annotations' files."""
+        assert isinstance(is_test, bool), "Must input a valid boolean input."
+        data_path = os.path.join(self.data_path, 'cifar-10-batches-py')
+        class_names = self.get_class_names(data_path)
+        if is_test:
+            data, labels = self.get_data_test(data_path)
+        else:
+            data, labels = self.get_data_train(data_path)
+        data = np.transpose(data, (0, 2, 3, 1))  # NxHxWxC
+        return data, labels, class_names
 
-        # test set
-        yield {"test": self.load_data_set(True)}
+    def get_class_names(self, path):
+        """Returns the class names/labels."""
+        assert path, "Must input a valid path."
+        filename = os.path.join(path, self.data_files[0])
+        return load_pickle(filename)
 
-    # def add_data_to_source(self, hdf5_handler, data, set_name=None):
-    #    """
-    #    Store data annotations in a nested tree fashion.
-    #
-    #    It closely follows the tree structure of the data.
-    #    """
-    #    hdf5_write_data(hdf5_handler, 'classes', data["class_name"], dtype=np.uint8, fillvalue=0)
-    #    hdf5_write_data(hdf5_handler, 'images', data["data"], dtype=np.uint8, fillvalue=-1)
-    #    hdf5_write_data(hdf5_handler, 'labels', data["labels"], dtype=np.uint8, fillvalue=1)
+    def get_data_train(self, path):
+        batch1 = load_pickle(os.path.join(path, self.data_files[1]))
+        batch2 = load_pickle(os.path.join(path, self.data_files[2]))
+        batch3 = load_pickle(os.path.join(path, self.data_files[3]))
+        batch4 = load_pickle(os.path.join(path, self.data_files[4]))
+        batch5 = load_pickle(os.path.join(path, self.data_files[5]))
 
-    def add_data_to_default(self, hdf5_handler, data, set_name=None):
+        data = np.concatenate(
+            (batch1['data'], batch2['data'], batch3['data'], batch4['data'], batch5['data']),
+            axis=0
+        )
+        data = data.reshape((50000, 3, 32, 32))
+        labels = np.concatenate(
+            (batch1['labels'], batch2['labels'], batch3['labels'], batch4['labels'], batch5['labels']),
+            axis=0
+        )
+
+        return data, labels
+
+    def get_data_test(self, path):
+        batch = load_pickle(os.path.join(path, self.data_files[6]))
+        data = batch['data'].reshape(10000, 3, 32, 32)
+        labels = np.array(batch['labels'], dtype=np.uint8)
+        return data, labels
+
+    def get_object_list(self, data, labels):
+        """Groups the data + labels to a list of indexes."""
+        object_id = np.ndarray((data.shape[0], 2), dtype=np.int32)
+        for i in range(data.shape[0]):
+            object_id[i][0] = i
+            object_id[i][1] = labels[i]
+        return object_id
+
+    def get_images_per_class(self, labels):
+        """Returns a list of image indexes per class."""
+        images_per_class = []
+        unique_labels = np.unique(labels)
+        for label in unique_labels:
+            images_idx = np.where(labels == label)[0].tolist()
+            images_per_class.append(images_idx)
+        return np.array(pad_list(images_per_class, -1), dtype=np.int32)
+
+    def process_set_metadata(self, data, set_name):
         """
-        Add data of a set to the default group.
-
-        For each field, the data is organized into a single big matrix.
+        Saves the metadata of a set.
         """
-        hdf5_write_data(hdf5_handler, 'classes', data["class_name"], dtype=np.uint8, fillvalue=0)
-        hdf5_write_data(hdf5_handler, 'labels', data["labels"], dtype=np.uint8, fillvalue=1)
-        hdf5_write_data(hdf5_handler, 'images', data["data"], dtype=np.uint8, fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'object_ids',
-                        data["object_ids"], dtype=np.int32, fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'object_fields',
-                        data["object_fields"], dtype=np.uint8, fillvalue=-1)
-        hdf5_write_data(hdf5_handler, 'list_images_per_class',
-                        data["list_images_per_class"], dtype=np.int32, fillvalue=-1)
+        self.save_field_to_hdf5(set_name, 'classes', data["classes"],
+                                dtype=np.uint8, fillvalue=0)
+        self.save_field_to_hdf5(set_name, 'images', data["images"],
+                                dtype=np.uint8, fillvalue=-1)
+        self.save_field_to_hdf5(set_name, 'labels', data["labels"],
+                                dtype=np.uint8, fillvalue=0)
+        self.save_field_to_hdf5(set_name, 'object_fields', data["object_fields"],
+                                dtype=np.uint8, fillvalue=0)
+        self.save_field_to_hdf5(set_name, 'object_ids', data["object_ids"],
+                                dtype=np.int32, fillvalue=-1)
+        self.save_field_to_hdf5(set_name, 'list_images_per_class', data["list_images_per_class"],
+                                dtype=np.int32, fillvalue=-1)
---
+++ tests/datasets/cifar/test_cifar10.py
@@ -0,0 +1,113 @@
+"""
+Test the base classes for managing datasets and tasks.
+"""
+
+
+import os
+import sys
+import pytest
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from dbcollection.datasets.cifar.cifar10 import Classification
+from dbcollection.utils.string_ascii import convert_str_to_ascii as str2ascii
+
+
+@pytest.fixture()
+def mock_classification_class():
+    return Classification(data_path='/some/path/data', cache_path='/some/path/cache')
+
+
+class TestClassificationTask:
+    """Unit tests for the BaseDatasetNew class."""
+
+    def test_task_attributes(self, mocker, mock_classification_class):
+        assert mock_classification_class.filename_h5 == 'classification'
+        assert mock_classification_class.data_files == ["batches.meta",
+                                                        "data_batch_1",
+                                                        "data_batch_2",
+                                                        "data_batch_3",
+                                                        "data_batch_4",
+                                                        "data_batch_5",
+                                                        "test_batch"]
+
+    def test_load_data(self, mocker, mock_classification_class):
+        mock_load_data = mocker.patch.object(Classification, "load_data_set", return_value=['some_data'])
+
+        load_data_generator = mock_classification_class.load_data()
+        if sys.version[0] == '3':
+            train_data = load_data_generator.__next__()
+            test_data = load_data_generator.__next__()
+        else:
+            train_data = load_data_generator.next()
+            test_data = load_data_generator.next()
+
+        assert mock_load_data.called
+        assert train_data == {"train": ['some_data']}
+        assert test_data == {"test": ['some_data']}
+
+    def test_load_data_set(self, mocker, mock_classification_class):
+        mock_load_annotations = mocker.patch.object(Classification, "load_data_annotations", return_value=('data', 'labels', {"label_names": 'some_class'}))
+        mock_get_object_list = mocker.patch.object(Classification, "get_object_list", return_value=list(range(10)))
+        mock_get_list = mocker.patch.object(Classification, "get_images_per_class", return_value=list(range(5)))
+
+        set_data = mock_classification_class.load_data_set(False)
+
+        mock_load_annotations.assert_called_once_with(False)
+        mock_get_object_list.assert_called_once_with('data', 'labels')
+        mock_get_list.assert_called_once_with('labels')
+        assert_array_equal(set_data['object_fields'], str2ascii(['images', 'classes']))
+        assert_array_equal(set_data['class_name'], str2ascii(['some_class']))
+        assert set_data['data'] == 'data'
+        assert set_data['labels'] == 'labels'
+        assert set_data['object_ids'] == list(range(10))
+        assert set_data['list_images_per_class'] == list(range(5))
+
+    @pytest.mark.parametrize('is_test', [False, True])
+    def test_load_data_annotations_for_train_set(self, mocker, mock_classification_class, is_test):
+        random_data = np.random.rand(20,3,32,32)
+        mock_get_names = mocker.patch.object(Classification, "get_class_names", return_value=['some', 'class', 'names'])
+        mock_get_data_test = mocker.patch.object(Classification, "get_data_test", return_value=(random_data, np.array(range(10))))
+        mock_get_data_train = mocker.patch.object(Classification, "get_data_train", return_value=(random_data, np.array(range(10))))
+
+        data_path = os.path.join(mock_classification_class.data_path, 'cifar-10-batches-py')
+        data, labels, class_names = mock_classification_class.load_data_annotations(is_test=is_test)
+
+        mock_get_names.assert_called_once_with(data_path)
+        if is_test:
+            mock_get_data_test.assert_called_once_with(data_path)
+            assert not mock_get_data_train.called
+        else:
+            assert not mock_get_data_test.called
+            mock_get_data_train.assert_called_once_with(data_path)
+        assert_array_equal(data, np.transpose(random_data, (0, 2, 3, 1)))
+        assert_array_equal(labels, np.array(range(10)))
+        assert class_names == ['some', 'class', 'names']
+
+    def test_get_object_list(self, mocker, mock_classification_class):
+        data = np.random.rand(20,2,32,32)
+        labels = np.array(range(20))
+
+        object_ids = mock_classification_class.get_object_list(data, labels)
+
+        assert_array_equal(object_ids, np.array([[i, labels[i]] for i in range(20)]))
+
+    def test_get_images_per_class(self, mocker, mock_classification_class):
+        labels = np.array([1,1,1,1,0,1,1,0])
+
+        images_per_class = mock_classification_class.get_images_per_class(labels)
+
+        expected = np.array([[4, 7, -1, -1, -1, -1],
+                             [0, 1, 2, 3, 5, 6]],
+                             dtype=np.int32)
+        assert_array_equal(images_per_class, expected)
+
+    def test_process_set_metadata(self, mocker, mock_classification_class):
+        mock_save_hdf5 = mocker.patch.object(Classification, "save_field_to_hdf5")
+
+        data = {"classes": 1, "images": 1, "labels": 1,
+                "object_fields": 1, "object_ids": 1, "list_images_per_class": 1}
+        mock_classification_class.process_set_metadata(data, 'train')
+
+        assert mock_save_hdf5.called
+        assert mock_save_hdf5.call_count == 6
