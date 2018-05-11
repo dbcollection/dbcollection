567d13d81cffc94cb7c2edf97f11c89101202d35
farrajota <mglfarrajota@outlook.com>
2018-05-11 20:48:52 +0100

    Add test for the class attributes

--- tests/datasets/test_mnist.py
+++ tests/datasets/test_mnist.py
@@ -10,7 +10,15 @@ import numpy as np
 from dbcollection.datasets.mnist import Classification
 
 
+@pytest.fixture()
+def mock_classification_class():
+    return Classification(data_path='/some/path/data', cache_path='/some/path/cache')
+
+
 class TestClassificationTask:
     """Unit tests for the mnist Classification task."""
 
-    pass
+    def test_task_attributes(self, mocker, mock_classification_class):
+        assert mock_classification_class.filename_h5 == 'classification'
+        classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        assert mock_classification_class.classes == classes
