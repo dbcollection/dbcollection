5cb3bfbee725c8b5c8568e4c76359f60305a7aff
farrajota <mglfarrajota@outlook.com>
2018-05-11 20:51:54 +0100

    Add test for load_data()

--- tests/datasets/test_mnist.py
+++ tests/datasets/test_mnist.py
@@ -4,6 +4,7 @@ Test the base classes for managing datasets and tasks.
 
 
 import os
+import sys
 import pytest
 import numpy as np
 
@@ -22,3 +23,20 @@ class TestClassificationTask:
         assert mock_classification_class.filename_h5 == 'classification'
         classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         assert mock_classification_class.classes == classes
+
+    def test_load_data(self, mocker, mock_classification_class):
+        mock_load_data_train = mocker.patch.object(Classification, "load_data_train", return_value=['some_train_data'])
+        mock_load_data_test = mocker.patch.object(Classification, "load_data_test", return_value=['some_test_data'])
+
+        load_data_generator = mock_classification_class.load_data()
+        if sys.version[0] == '3':
+            train_data = load_data_generator.__next__()
+            test_data = load_data_generator.__next__()
+        else:
+            train_data = load_data_generator.next()
+            test_data = load_data_generator.next()
+
+        mock_load_data_train.assert_called_once_with()
+        mock_load_data_test.assert_called_once_with()
+        assert train_data == {"train": ['some_train_data']}
+        assert test_data == {"test": ['some_test_data']}
