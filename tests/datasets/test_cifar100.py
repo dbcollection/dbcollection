66429201caf073c04dd591f8cd3d06c86a318800
farrajota <mglfarrajota@outlook.com>
2018-05-11 22:30:57 +0100

    Add initial test code for cifar100

---
+++ tests/datasets/test_cifar100.py
@@ -0,0 +1,14 @@
+"""
+Test the base classes for managing datasets and tasks.
+"""
+
+
+import os
+import sys
+import pytest
+import numpy as np
+
+
+class TestClassificationTask:
+    """Unit tests for the cifar100 Classification task."""
+    pass
