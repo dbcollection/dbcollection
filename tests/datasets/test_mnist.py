52199aff1804df33744a522706ae5a5ee316bf81
farrajota <mglfarrajota@outlook.com>
2018-05-11 20:44:34 +0100

    Add initial test code for the mnist dataset

---
+++ tests/datasets/test_mnist.py
@@ -0,0 +1,16 @@
+"""
+Test the base classes for managing datasets and tasks.
+"""
+
+
+import os
+import pytest
+import numpy as np
+
+from dbcollection.datasets.mnist import Classification
+
+
+class TestClassificationTask:
+    """Unit tests for the mnist Classification task."""
+
+    pass
