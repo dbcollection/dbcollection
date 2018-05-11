b9dccc7b1cfc7ce5824a5c5fa7325b8a12b3f064
farrajota <mglfarrajota@outlook.com>
2018-05-11 22:40:04 +0100

    Add test for load_data()

--- tests/datasets/test_cifar100.py
+++ tests/datasets/test_cifar100.py
@@ -67,3 +67,17 @@ class TestClassificationTask:
             'lawn-mower', 'rocket', 'streetcar', 'tank', 'tractor'
         ]
 
+    def test_load_data(self, mocker, mock_classification_class):
+        mock_load_data = mocker.patch.object(Classification, "load_data_set", return_value=['some_data'])
+
+        load_data_generator = mock_classification_class.load_data()
+        if sys.version[0] == '3':
+            train_data = load_data_generator.__next__()
+            test_data = load_data_generator.__next__()
+        else:
+            train_data = load_data_generator.next()
+            test_data = load_data_generator.next()
+
+        assert mock_load_data.called
+        assert train_data == {"train": ['some_data']}
+        assert test_data == {"test": ['some_data']}
