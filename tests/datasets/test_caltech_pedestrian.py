ce758bf263c46c8ae71840330532b1a371d842fb
farrajota <mglfarrajota@outlook.com>
2018-05-16 23:09:51 +0100

    Add initial test code

---
+++ tests/datasets/test_caltech_pedestrian.py
@@ -0,0 +1,18 @@
+"""
+Test the base classes for managing datasets and tasks.
+
+Dataset: Caltech Pedestrian
+
+Tasks: Detection, Detection10x, Detection30x
+"""
+
+
+import os
+import sys
+import pytest
+import numpy as np
+
+
+class TestDetectionTask:
+    """Unit tests for the caltech pedestrian Detection task."""
+    pass
