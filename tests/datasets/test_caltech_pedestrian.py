08709775d84b937e285f749ca48efd4d7b42738f
farrajota <mglfarrajota@outlook.com>
2018-05-18 22:45:37 +0100

    Add test for Detection class attributes

--- tests/datasets/test_caltech_pedestrian.py
+++ tests/datasets/test_caltech_pedestrian.py
@@ -13,6 +13,23 @@ import pytest
 import numpy as np
 
 
+from dbcollection.datasets.caltech.caltech_pedestrian import Detection
+
+
+@pytest.fixture()
+def mock_detection_class():
+    return Detection(data_path='/some/path/data', cache_path='/some/path/cache')
+
+
 class TestDetectionTask:
     """Unit tests for the caltech pedestrian Detection task."""
-    pass
+
+    def test_task_attributes(self, mocker, mock_detection_class):
+        assert mock_detection_class.filename_h5 == 'detection'
+        assert mock_detection_class.skip_step == 30
+        assert mock_detection_class.classes == ('person', 'person-fa', 'people', 'person?')
+        assert mock_detection_class.sets == {
+            "train": ('set00', 'set01', 'set02', 'set03', 'set04', 'set05'),
+            "test": ('set06', 'set07', 'set08', 'set09', 'set10')
+        }
+
