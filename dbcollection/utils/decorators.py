f51c206d3ba769c3e69d531c0d87453d9fc5d101
farrajota <mglfarrajota@outlook.com>
2018-06-01 23:50:06 +0100

    Fix linting issues

--- dbcollection/utils/decorators.py
+++ dbcollection/utils/decorators.py
@@ -9,10 +9,10 @@ import sys
 def display_message_processing(label):
     """Displays a message about processing a data field  metadata."""
     def decorator(fn):
-        def decorated(*args,**kwargs):
+        def decorated(*args, **kwargs):
             if args[0].verbose:
                 sys.stdout.write("> Processing the '{label}' metadata...".format(label=label))
-            data = fn(*args,**kwargs)
+            data = fn(*args, **kwargs)
             if args[0].verbose:
                 sys.stdout.write(" Done.\n")
             return data
