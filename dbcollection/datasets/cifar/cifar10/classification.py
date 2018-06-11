5a6020d4e93095e379be0a9d189916e9587afcd2
farrajota <mglfarrajota@outlook.com>
2018-06-11 23:21:47 +0100

    Copy data loading related code to the data loader class

--- dbcollection/datasets/cifar/cifar10/classification.py
+++ dbcollection/datasets/cifar/cifar10/classification.py
@@ -164,12 +164,82 @@ class DatasetAnnotationLoader:
     def load_train_data(self):
         """Loads the train set annotation data from disk
         and returns it as a dictionary."""
-        pass
+        return self.load_data_set(is_test=False)
 
     def load_test_data(self):
         """Loads the test set annotation data from disk
         and returns it as a dictionary."""
-        pass
+        return self.load_data_set(is_test=True)
+
+    def load_data_set(self, is_test):
+        """Fetches the train/test data."""
+        assert isinstance(is_test, bool), "Must input a valid boolean input."
+        images, labels, class_names = self.load_data_annotations(is_test)
+
+        object_list = self.get_object_list(images, labels)
+        images_per_class = self.get_images_per_class(labels)
+
+        return {
+            "object_fields": str2ascii(['images', 'classes']),
+            "classes": str2ascii(class_names['label_names']),
+            "images": images,
+            "labels": labels,
+            "object_ids": object_list,
+            "list_images_per_class": images_per_class
+        }
+
+    def load_data_annotations(self, is_test):
+        """Loads the data from the annotations' files."""
+        assert isinstance(is_test, bool), "Must input a valid boolean input."
+        data_path = os.path.join(self.data_path, 'cifar-10-batches-py')
+        class_names = self.get_class_names(data_path)
+        if is_test:
+            data, labels = self.get_data_test(data_path)
+        else:
+            data, labels = self.get_data_train(data_path)
+        data = np.transpose(data, (0, 2, 3, 1))  # NxHxWxC
+        return data, labels, class_names
+
+    def get_class_names(self, path):
+        """Returns the class names/labels."""
+        assert path, "Must input a valid path."
+        filename = os.path.join(path, self.data_files[0])
+        return self.load_annotation_file(filename)
+
+    def load_annotation_file(self, path):
+        """Reads the data from annotation file from disk."""
+        return load_pickle(path)
+
+    def get_data_train(self, path):
+        assert path, "Must input a valid path."
+        batch1 = self.load_annotation_file(os.path.join(path, self.data_files[1]))
+        batch2 = self.load_annotation_file(os.path.join(path, self.data_files[2]))
+        batch3 = self.load_annotation_file(os.path.join(path, self.data_files[3]))
+        batch4 = self.load_annotation_file(os.path.join(path, self.data_files[4]))
+        batch5 = self.load_annotation_file(os.path.join(path, self.data_files[5]))
+
+        data = np.concatenate(
+            (batch1['data'], batch2['data'], batch3['data'], batch4['data'], batch5['data']),
+            axis=0
+        )
+        data = self.reshape_array(data, 50000)
+        labels = np.concatenate(
+            (batch1['labels'], batch2['labels'], batch3['labels'], batch4['labels'], batch5['labels']),
+            axis=0
+        )
+
+        return data, labels
+
+    def reshape_array(self, data, size_array):
+        """Reshapes the numpy array to a fixed format."""
+        return data.reshape(size_array, 3, 32, 32)
+
+    def get_data_test(self, path):
+        assert path, "Must input a valid path."
+        batch = self.load_annotation_file(os.path.join(path, self.data_files[6]))
+        data = self.reshape_array(batch['data'], 10000)
+        labels = np.array(batch['labels'], dtype=np.uint8)
+        return data, labels
 
 
 # -----------------------------------------------------------
