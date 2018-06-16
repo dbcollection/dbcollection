6e730b78f93b4ca5260a887c781433f15cf8854e
farrajota <mglfarrajota@outlook.com>
2018-06-16 17:27:23 +0100

    Remove tests of deprecated code

--- tests/datasets/test_mnist.py
+++ tests/datasets/test_mnist.py
@@ -59,66 +59,8 @@ class TestClassificationTask:
         assert train_data == {"train": ['some_data']}
         assert test_data == {"test": ['some_data']}
 
-    @pytest.mark.parametrize('is_test', [False, True])
-    def test_load_data_set(self, mocker, mock_classification_class, classes_classification, is_test):
-        images, labels, size_set = (np.random.rand(10, 784), np.array(range(10)), 10)
-        mock_get_data_train= mocker.patch.object(Classification, "get_train_data", return_value=(images, labels, size_set))
-        mock_get_data_test= mocker.patch.object(Classification, "get_test_data", return_value=(images, labels, size_set))
-        mock_get_list = mocker.patch.object(Classification, "get_list_images_per_class", return_value=list(range(10)))
-
-        set_data = mock_classification_class.load_data_set(is_test=is_test)
-
-        if is_test:
-            mock_get_data_test.assert_called_once_with()
-            assert not mock_get_data_train.called
-        else:
-            mock_get_data_train.assert_called_once_with()
-            assert not mock_get_data_test.called
-        mock_get_list.assert_called_once_with(labels)
-        assert_array_equal(set_data['classes'], str2ascii(classes_classification))
-        assert_array_equal(set_data['images'], images.reshape(size_set, 28, 28))
-        assert_array_equal(set_data['labels'], labels)
-        assert_array_equal(set_data['object_fields'], str2ascii(['images', 'labels']))
-        assert_array_equal(set_data['object_ids'], np.array([[i, labels[i]] for i in range(size_set)]))
-        assert set_data['list_images_per_class'] == list(range(10))
-
-    def test_get_train_data(self, mocker, mock_classification_class):
-        mock_load_images = mocker.patch.object(Classification, "load_images_numpy", return_value=np.zeros((5,768)))
-        mock_load_labels = mocker.patch.object(Classification, "load_labels_numpy", return_value=np.ones(5))
-
-        train_images, train_labels, size_train = mock_classification_class.get_train_data()
-
-        mock_load_images.assert_called_once_with(os.path.join('/some/path/data', 'train-images.idx3-ubyte'))
-        mock_load_labels.assert_called_once_with(os.path.join('/some/path/data', 'train-labels.idx1-ubyte'))
-        assert_array_equal(train_images, np.zeros((5,768)))
-        assert_array_equal(train_labels, np.ones(5))
-        assert size_train == 60000
-
-    def test_get_list_images_per_class(self, mocker, mock_classification_class):
-        labels = np.array([1,1,1,1,0,1,1,0])
-
-        images_per_class = mock_classification_class.get_list_images_per_class(labels)
-
-        expected = np.array([[4, 7, -1, -1, -1, -1],
-                             [0, 1, 2, 3, 5, 6]],
-                             dtype=np.int32)
-        assert_array_equal(images_per_class, expected)
-
-    def test_get_test_data(self, mocker, mock_classification_class):
-        mock_load_images = mocker.patch.object(Classification, "load_images_numpy", return_value=np.zeros((5,768)))
-        mock_load_labels = mocker.patch.object(Classification, "load_labels_numpy", return_value=np.ones(5))
-
-        test_images, test_labels, size_test = mock_classification_class.get_test_data()
-
-        mock_load_images.assert_called_once_with(os.path.join('/some/path/data', 't10k-images.idx3-ubyte'))
-        mock_load_labels.assert_called_once_with(os.path.join('/some/path/data', 't10k-labels.idx1-ubyte'))
-        assert_array_equal(test_images, np.zeros((5,768)))
-        assert_array_equal(test_labels, np.ones(5))
-        assert size_test == 10000
-
     def test_process_set_metadata(self, mocker, mock_classification_class):
         dummy_ids = [0, 1, 2, 3, 4, 5]
-        mock_save_hdf5 = mocker.patch.object(Classification, "save_field_to_hdf5")
         mock_class_field = mocker.patch.object(ClassLabelField, "process")
         mock_image_field = mocker.patch.object(ImageField, "process", return_value=dummy_ids)
         mock_label_field = mocker.patch.object(LabelIdField, "process", return_value=dummy_ids)
@@ -136,8 +78,6 @@ class TestClassificationTask:
         mock_objfield_field.assert_called_once_with()
         mock_objids_field.assert_called_once_with(dummy_ids, dummy_ids)
         mock_images_per_class_list.assert_called_once_with()
-        assert mock_save_hdf5.called
-        assert mock_save_hdf5.call_count == 6
 
 
 class TestDatasetAnnotationLoader:
