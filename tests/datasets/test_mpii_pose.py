0f619436a3e18124fe51c8720593e77d7f0a3c3b
farrajota <mglfarrajota@outlook.com>
2018-06-20 22:49:33 +0100

    Add test for the Keypoints class attributes

--- tests/datasets/test_mpii_pose.py
+++ tests/datasets/test_mpii_pose.py
@@ -15,4 +15,35 @@ from dbcollection.datasets.mpii_pose.keypoints import Keypoints
 class TestKeypointsTask:
     """Unit tests for the mpii Keypoints task."""
 
+    @staticmethod
+    @pytest.fixture()
+    def mock_keypoints_class():
+        return Keypoints(data_path='/some/path/data', cache_path='/some/path/cache')
+
+    def test_task_attributes(self, mocker, mock_keypoints_class):
+        assert mock_keypoints_class.filename_h5 == 'keypoint'
+        assert mock_keypoints_class.is_full == False
+        assert mock_keypoints_class.keypoints_labels == [
+            'right ankle',  # -- 1
+            'right knee',  # -- 2
+            'right hip',  # -- 3
+            'left hip',  # -- 4
+            'left knee',  # -- 5
+            'left ankle',  # -- 6
+            'pelvis',  # -- 7
+            'thorax',  # -- 8
+            'upper neck',  # -- 9
+            'head top',  # -- 10
+            'right wrist',  # -- 11
+            'right elbow',  # -- 12
+            'right shoulder',  # -- 13
+            'left shoulder',  # -- 14
+            'left elbow',  # -- 15
+            'left wrist'  # -- 16
+        ]
+
+
+class TestDatasetAnnotationLoader:
+    """Unit tests for DatasetAnnotationLoader class."""
+
     pass
