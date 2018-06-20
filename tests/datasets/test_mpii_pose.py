4f5d198e22cbc5f862604b46502edd0467563221
farrajota <mglfarrajota@outlook.com>
2018-06-20 22:14:41 +0100

    Add initial test code structure

---
+++ tests/datasets/test_mpii_pose.py
@@ -0,0 +1,18 @@
+"""
+Test the base classes for managing datasets and tasks.
+"""
+
+
+import os
+import sys
+import pytest
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from dbcollection.datasets.mpii_pose.keypoints import Keypoints
+
+
+class TestKeypointsTask:
+    """Unit tests for the mpii Keypoints task."""
+
+    pass
