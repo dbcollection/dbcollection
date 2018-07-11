caf5ce62fed8f70693c0fe9b8af9280d8e0c0a0c
farrajota <mglfarrajota@outlook.com>
2018-07-11 21:17:16 +0100

    Remove unused code path in ImageFilenamesField.get_image_filenames()

--- dbcollection/datasets/mpii_pose/keypoints.py
+++ dbcollection/datasets/mpii_pose/keypoints.py
@@ -604,11 +604,7 @@ class ImageFilenamesField(BaseField):
         pose_annotations = self.get_pose_annotations()
         for i, image_filename in enumerate(image_fnames):
             image_pose_annotations = pose_annotations[i]
-            if any(image_pose_annotations):
-                for j, _ in enumerate(image_pose_annotations):
-                    image_filenames.append(image_filename)
-                    image_filenames_ids.append(i)
-            else:
+            for j, _ in enumerate(image_pose_annotations):
                 image_filenames.append(image_filename)
                 image_filenames_ids.append(i)
         return image_filenames, image_filenames_ids
