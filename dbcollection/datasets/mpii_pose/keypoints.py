3d90868a9966610aa0d20253f40c86a26a08a247
farrajota <mglfarrajota@outlook.com>
2018-07-21 14:20:20 +0100

    Remove unnecessary import

--- dbcollection/datasets/mpii_pose/keypoints.py
+++ dbcollection/datasets/mpii_pose/keypoints.py
@@ -12,7 +12,6 @@ from dbcollection.utils.decorators import display_message_processing
 from dbcollection.utils.string_ascii import convert_str_to_ascii as str2ascii
 from dbcollection.utils.pad import pad_list
 from dbcollection.utils.file_load import load_matlab
-from dbcollection.utils.hdf5 import hdf5_write_data
 
 
 class Keypoints(BaseTaskNew):
