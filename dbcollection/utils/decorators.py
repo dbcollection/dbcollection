99d74c07b2b8dc17ba17378ae2f32e0c7eeb20be
farrajota <mglfarrajota@outlook.com>
2018-07-21 23:05:45 +0100

    Modify the display message string of the decorator withouth the word 'the'

--- dbcollection/utils/decorators.py
+++ dbcollection/utils/decorators.py
@@ -11,7 +11,7 @@ def display_message_processing(label):
     def decorator(fn):
         def decorated(*args, **kwargs):
             if args[0].verbose:
-                sys.stdout.write("> Processing the '{label}' metadata...".format(label=label))
+                sys.stdout.write("> Processing '{label}' metadata...".format(label=label))
             data = fn(*args, **kwargs)
             if args[0].verbose:
                 sys.stdout.write(" Done.\n")
