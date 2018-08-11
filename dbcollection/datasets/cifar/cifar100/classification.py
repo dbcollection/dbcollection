5df054191cbb8bf3b142a7b84af7c8b73aab178a
miguel <mglfarrajota@outlook.com>
2018-08-11 18:13:47 +0100

    Remove object_fields and object_ids

--- dbcollection/datasets/cifar/cifar100/classification.py
+++ dbcollection/datasets/cifar/cifar100/classification.py
@@ -108,8 +108,6 @@ class Classification(BaseTask):
         image_ids = ImageField(**args).process()
         label_ids = LabelIdField(**args).process()
         super_label_ids = SuperLabelIdField(**args).process()
-        ObjectFieldNamesField(**args).process()
-        ObjectIdsField(**args).process(image_ids, label_ids, super_label_ids)
 
         # Lists
         if self.verbose:
@@ -303,36 +301,6 @@ class SuperLabelIdField(BaseField):
         super_labels = self.data['coarse_labels']
         super_label_ids = list(range(len(super_labels)))
         return super_labels, super_label_ids
-
-
-class ObjectFieldNamesField(BaseField):
-    """Object field names metadata process/save class."""
-
-    def process(self):
-        """Processes and saves the labels metadata to hdf5."""
-        self.save_field_to_hdf5(
-            set_name=self.set_name,
-            field='object_fields',
-            data=str2ascii(['images', 'labels', 'superlabels']),
-            dtype=np.uint8,
-            fillvalue=0
-        )
-
-
-class ObjectIdsField(BaseField):
-    """Object ids' field metadata process/save class."""
-
-    def process(self, image_ids, label_ids, super_label_ids):
-        """Processes and saves the object ids metadata to hdf5."""
-        # images, labels, superlabels
-        object_ids = [[image_ids[i], label_ids[i], super_label_ids[i]] for i, _ in enumerate(label_ids)]
-        self.save_field_to_hdf5(
-            set_name=self.set_name,
-            field='object_ids',
-            data=np.array(object_ids, dtype=np.int32),
-            dtype=np.int32,
-            fillvalue=-1
-        )
 
 
 # -----------------------------------------------------------
