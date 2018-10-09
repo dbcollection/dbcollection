3cb4622b926fc7631140bba6a6ff97e51de0b899
miguel <mglfarrajota@outlook.com>
2018-10-09 22:27:36 +0100

    Move metadata into the Dataset class

--- dbcollection/datasets/cifar/cifar100/__init__.py
+++ dbcollection/datasets/cifar/cifar100/__init__.py
@@ -7,17 +7,15 @@ from dbcollection.datasets import BaseDataset
 from .classification import Classification
 
 
-urls = (
-    {"url": 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz', "md5hash": 'eb9058c3a382ffc7106e4002c42a8d85'},
-)
-keywords = ('image_processing', 'classification')
-tasks = {"classification": Classification}
-default_task = 'classification'
-
-
 class Dataset(BaseDataset):
     """Cifar100 preprocessing/downloading functions."""
-    urls = urls
-    keywords = keywords
-    tasks = tasks
-    default_task = default_task
+
+    urls = (
+        {
+            "url": 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz',
+            "md5hash": 'eb9058c3a382ffc7106e4002c42a8d85'
+        },
+    )
+    keywords = ('image_processing', 'classification')
+    tasks = {"classification": Classification}
+    default_task = 'classification'
