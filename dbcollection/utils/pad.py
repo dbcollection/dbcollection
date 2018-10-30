344cd91ec78762c51c9fc95b3b82200c2544cde2
miguel <mglfarrajota@outlook.com>
2018-10-30 22:21:50 +0000

    Remove comments and whitespace

--- dbcollection/utils/pad.py
+++ dbcollection/utils/pad.py
@@ -8,21 +8,22 @@ import itertools
 
 
 def pad_list(listA, val=-1, length=None):
-    """Pad list of lists with 'val' such that all lists have the same length.
+    """Pads list of lists with a value such that all lists have
+    the same length.
 
     Parameters
     ----------
     listA : list
         List of lists of different sizes.
     val : number, optional
-        Value to pad the lists.
+        Value to pad the lists. Default: -1.
     length : number, optional
-        Total length of the list.
+        Total length of the list. Default: None.
 
     Returns
     -------
     list
-        A list of lists with the same same.
+        A list of lists with the same length.
 
     Examples
     --------
@@ -37,31 +38,25 @@ def pad_list(listA, val=-1, length=None):
     [[0, 0, 0], [1, 0, 0], [3, 4, 5]]
     >>> pad_list([[],[1],[3,4,5]], 0, 6)  # pad lists with 0 of size 6
     [[0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0], [3, 4, 5, 0, 0, 0]]
-
     """
-    # pad list with zeros in order to have all lists of the same size
     assert isinstance(listA, list), 'Input must be a list. Got {}, expected {}' \
                                     .format(type(listA), type(list))
-
-    # get size of the biggest list
     if length:
         max_size = length
     else:
         max_size = len(max(listA, key=len))
-
-    # pad all lists with the a padding value
     return [l + [val] * int(max_size - len(l)) for l in listA]
 
 
 def unpad_list(listA, val=-1):
-    """Unpad list of lists with which has values equal to 'val'.
+    """Unpads a list of lists contiain a value 'val' as padding.
 
     Parameters
     ----------
     listA : list
         List of lists of equal sizes.
     val : number, optional
-        Value to unpad the lists.
+        Value to unpad the lists. Default: -1.
 
     Returns
     -------
@@ -77,12 +72,10 @@ def unpad_list(listA, val=-1):
     [[1, 2, 3], [5, 6]]
     >>> unpad_list([[5,0,-1],[1,2,3,4,5]], 5)
     [[0, -1], [1, 2, 3, 4]]
-
     """
     # pad list with zeros in order to have all lists of the same size
     assert isinstance(listA, list), 'Input must be a list. Got {}, expected {}' \
                                     .format(type(listA), type(list))
-
     if isinstance(listA[0], list):
         return [list(filter(lambda x: x != val, l)) for i, l in enumerate(listA)]
     else:
@@ -90,9 +83,9 @@ def unpad_list(listA, val=-1):
 
 
 def squeeze_list(listA, val=-1):
-    """ Compact a list of lists into a single list.
+    """Compacts a list of lists into a single list.
 
-    Squeezes (spaghettify) a list of lists into a single list.
+    Squeezes (spaghettifies) a list of lists into a single list.
     The lists are concatenated into a single one, and to separate
     them it is used a separating value to mark the split location
     when unsqueezing the list.
@@ -102,7 +95,7 @@ def squeeze_list(listA, val=-1):
     listA : list
         List of lists.
     val : number, optional
-        Value to separate the lists.
+        Value to separate the lists. Default: -1.
 
     Returns
     -------
@@ -116,7 +109,6 @@ def squeeze_list(listA, val=-1):
     >>> from dbcollection.utils.pad import squeeze_list
     >>> squeeze_list([[1,2], [3], [4,5,6]], -1)
     [1, 2, -1, 3, -1, 4, 5, 6]
-
     """
     concatA = [l + [val] for l in listA]
     out = [li for l in concatA for li in l]
@@ -124,7 +116,7 @@ def squeeze_list(listA, val=-1):
 
 
 def unsqueeze_list(listA, val=-1):
-    """ Unpacks a list into a list of lists.
+    """Unpacks a list into a list of lists.
 
     Returns a list of lists by splitting the input list
     into 'N' lists when encounters an element equal to
@@ -138,7 +130,7 @@ def unsqueeze_list(listA, val=-1):
     listA : list
         A list.
     val : int/float, optional
-        Value to separate the lists.
+        Value to separate the lists. Default: -1.
 
     Returns
     -------
@@ -152,6 +144,5 @@ def unsqueeze_list(listA, val=-1):
     >>> from dbcollection.utils.pad import unsqueeze_list
     >>> unsqueeze_list([1, 2, -1, 3, -1, 4, 5, 6], -1)
     [[1, 2], [3], [4, 5, 6]]
-
     """
     return [list(g) for k, g in itertools.groupby(listA, lambda x:x in (val, )) if not k]
