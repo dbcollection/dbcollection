ef3300c7b048debd367094e5dd84c69c7db26ca5
miguel <mglfarrajota@outlook.com>
2018-11-07 22:21:41 +0000

    Refactor flic's Dataset class

--- dbcollection/datasets/flic/__init__.py
+++ dbcollection/datasets/flic/__init__.py
@@ -6,15 +6,12 @@ Frames Labeled In Cinema (FLIC) Dataset download/process functions.
 from dbcollection.datasets import BaseDataset
 from .keypoints import Keypoints
 
-urls = ({'googledrive': '0B4K3PZp8xXDJN0Fpb0piVjQ3Y3M', 'save_name': 'flic.zip'},)
-keywords = ('image_processing', 'detection', 'human_pose', 'keypoints')
-tasks = {"keypoints": Keypoints}
-default_task = 'keypoints'
-
 
 class Dataset(BaseDataset):
     """Frames Labeled In Cinema (FLIC) Dataset preprocessing/downloading functions."""
-    urls = urls
-    keywords = keywords
-    tasks = tasks
-    default_task = default_task
+    urls = ({"url": '0B4K3PZp8xXDJN0Fpb0piVjQ3Y3M',
+             "save_name": 'flic.zip',
+             "source": 'googledrive'},)
+    keywords = ('image_processing', 'detection', 'human_pose', 'keypoints')
+    tasks = {"keypoints": Keypoints}
+    default_task = 'keypoints'
